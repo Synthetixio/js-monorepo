cc01f2cde67e53415000895acdbc89ae8410e727
David <dbschwartz17@gmail.com>
2021-03-22 16:13:16 -0700

    add a query for getting rates to the new data pacakge

---
+++ packages/data/queries/rateUpdates.ts
@@ -0,0 +1,30 @@
+import { gql } from 'graphql-request';
+import { RateUpdateQueryParams } from '../src/types';
+import { createGQLWhereString } from '../src/utils';
+
+export const createRateUpdatesQuery = ({ synth, minTimestamp }: RateUpdateQueryParams): string => {
+	const whereString = createGQLWhereString(
+		Object.entries({
+			synth: synth != null ? 'synth' : null,
+			timestamp_gte: minTimestamp != null ? 'minTimestamp' : null,
+		})
+	);
+
+	return gql`
+		query rateUpdates($max: Int, $synth: String, $minTimestamp: Int) {
+			rateUpdates(
+				first: $max
+				where: ${whereString}
+				orderBy: timestamp
+				orderDirection: desc
+			) {
+				id
+				currencyKey
+				synth
+				rate
+				block
+				timestamp
+			}
+		}
+	`;
+};
