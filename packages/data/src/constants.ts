827aac22fbbed06548176ab9b6c957f4a47c7dfc
Dave <dbschwartz17@gmail.com>
2021-04-05 16:45:56 -0700

    update the subgraph urls (#19)

--- packages/data/src/constants.ts
+++ packages/data/src/constants.ts
@@ -12,8 +12,8 @@ export const l1Endpoints = {
 };
 
 export const l2Endpoints = {
-	snx: 'https://api.thegraph.com/subgraphs/name/dvd-schwrtz/test',
-	snxKovanOvm: 'https://api.thegraph.com/subgraphs/name/dvd-schwrtz/synthetix',
+	snx: 'https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix-ovm',
+	snxKovanOvm: 'https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix-kovan-ovm',
 };
 
 export const timeSeriesEntityMap = { '1d': 'dailySNXPrices', '15m': 'fifteenMinuteSNXPrices' };
