66c3f483a13f0b9e837210d5d4a2c3efcff7174d
Clement Balestrat <clement.balestrat@gmail.com>
2021-04-07 13:56:46 +1000

    build

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -7,8 +7,7 @@ import { ProviderConfig, SynthetixProvider, OvmProvider } from './types';
 
 const loadProvider = ({ networkId = 1, infuraId, provider }: ProviderConfig): SynthetixProvider => {
 	if (!provider && !infuraId) throw new Error(ERRORS.noWeb3Provider);
-	if (provider && provider.isMetaMask) return new ethersProviders.Web3Provider(provider);
-	if (provider) return provider;
+	if (provider) return new ethersProviders.Web3Provider(provider);
 	if (infuraId) return new ethersProviders.InfuraProvider(networkId, infuraId);
 };
 
