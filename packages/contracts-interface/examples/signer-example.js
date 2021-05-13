be4dcbacbb36c13e4fca5802311668124a1b6283
Justin J. Moses <justinjmoses@gmail.com>
2021-05-13 13:22:57 -0600

    Updating example

--- packages/contracts-interface/examples/signer-example.js
+++ packages/contracts-interface/examples/signer-example.js
@@ -6,12 +6,24 @@ const { synthetix } = require('../src/index.ts');
 	const provider = ethers.providers.getDefaultProvider('kovan');
 	const signer = new ethers.Wallet(
 		// just a dummy kovan wallet with a little keth from the faucet
-		'0xa0d951c494421559c63089093b020cf2f7aac003c916967dc36e989bc695222e'
+		'0xa0d951c494421559c63089093b020cf2f7aac003c916967dc36e989bc695222e',
+		provider
 	);
 	const snxjs = synthetix({ network: 'kovan', signer, provider });
+	const { formatEther, parseEther, parseUnits } = snxjs.utils;
 
 	console.log(
-		'Total Issued Synths:',
-		await snxjs.contracts.Synthetix.totalIssuedSynths(snxjs.toBytes32('sUSD'), {})
+		'Remaining ETH capacity in the EtherWrapper (kovan):',
+		formatEther(await snxjs.contracts.EtherWrapper.capacity())
 	);
+
+	console.log('Engaging the native ether wrapper to mint sETH');
+	const response = await snxjs.contracts.NativeEtherWrapper.mint({
+		value: parseEther('0.01'),
+		gasPrice: parseUnits('2', 'gwei'),
+		gasLimit: 500e3,
+	});
+	console.log('Submitted', response.hash);
+	await response.wait();
+	console.log('Mined', `https://kovan.etherscan.io/tx/${response.hash}`);
 })();
