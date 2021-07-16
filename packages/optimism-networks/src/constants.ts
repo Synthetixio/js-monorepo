09eb00aa18527442e0c47eb9920baa8823835e02
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-07-16 11:06:56 -0400

    feat: update blockExplorerUrls to optimistic etherscan

--- packages/optimism-networks/src/constants.ts
+++ packages/optimism-networks/src/constants.ts
@@ -15,7 +15,7 @@ export const OPTIMISM_NETWORKS: Record<number, OptimismNetwork> = {
 		chainId: '0xA',
 		chainName: 'Optimism Mainnet',
 		rpcUrls: ['https://mainnet.optimism.io'],
-		blockExplorerUrls: ['https://explorer.optimism.io/'],
+		blockExplorerUrls: ['https://optimistic.etherscan.io/'],
 		iconUrls: [
 			'https://optimism.io/images/metamask_icon.svg',
 			'https://optimism.io/images/metamask_icon.png',
@@ -25,7 +25,7 @@ export const OPTIMISM_NETWORKS: Record<number, OptimismNetwork> = {
 		chainId: '0x45',
 		chainName: 'Optimism Kovan',
 		rpcUrls: ['https://kovan.optimism.io'],
-		blockExplorerUrls: ['https://kovan-explorer.optimism.io/'],
+		blockExplorerUrls: ['https://kovan-optimistic.etherscan.io/'],
 		iconUrls: [
 			'https://optimism.io/images/metamask_icon.svg',
 			'https://optimism.io/images/metamask_icon.png',
