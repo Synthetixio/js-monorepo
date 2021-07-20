6d0dd48db941f9fc25e658eba00aa85248ae1700
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-07-19 21:46:19 -0400

    refactor: added formatGwei utils and error handling

--- packages/queries/__tests__/network.test.ts
+++ packages/queries/__tests__/network.test.ts
@@ -29,4 +29,30 @@ describe('@synthetixio/queries network useEthGasPriceQuery', () => {
 			average: 0.015,
 		});
 	});
+
+	test('test ovm gas error', async () => {
+		const wrapper = getWrapper({
+			defaultOptions: {
+				queries: {
+					// ✅ turns retries off
+					retry: false,
+				},
+			},
+		});
+
+		const useOvm = true;
+
+		delete ctx.provider.getGasPrice;
+
+		// eslint-disable-next-line
+		console.error = () => {}; //suppress error
+
+		const { result, waitFor } = renderHook(() => useEthGasPriceQuery(ctx, useOvm), { wrapper });
+
+		await waitFor(() => result.current.isError);
+
+		expect(result.current.error.message).toContain(
+			'Cannot retrieve optimistic gas price from provider'
+		);
+	});
 });
