8bec68cd5669c53b2cda1b9fb8e34a5b354aa54a
David <dbschwartz17@gmail.com>
2021-07-21 15:17:13 -0700

    lint fixes

--- packages/queries/babel.config.js
+++ packages/queries/babel.config.js
@@ -2,4 +2,3 @@ module.exports = {
 	presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
 	plugins: ['@babel/transform-runtime'],
 };
-
--- packages/queries/webpack-react-test-app.config.js
+++ packages/queries/webpack-react-test-app.config.js
@@ -1,32 +1,35 @@
+'use strict';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
-  entry: './react-test-app/src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            configFile: "tsconfig.react-test-app.json"
-          }
-        },
-        exclude: /node_modules/,
-      },
-    ]
-  },
-  resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-  output: {
-    path: path.resolve(__dirname, '/react-test-app/dist/'),
-    publicPath: '/react-test-app/dist/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, '/react-test-app/public/'),
-    port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
-    hotOnly: true
-  },
-  plugins: []
+	entry: './react-test-app/src/index.tsx',
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)?$/,
+				use: {
+					loader: 'ts-loader',
+					options: {
+						configFile: 'tsconfig.react-test-app.json',
+					},
+				},
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+	output: {
+		path: path.resolve(__dirname, '/react-test-app/dist/'),
+		publicPath: '/react-test-app/dist/',
+		filename: 'bundle.js',
+	},
+	devServer: {
+		contentBase: path.join(__dirname, '/react-test-app/public/'),
+		port: 3000,
+		publicPath: 'http://localhost:3000/dist/',
+		hotOnly: true,
+	},
+	plugins: [],
 };
