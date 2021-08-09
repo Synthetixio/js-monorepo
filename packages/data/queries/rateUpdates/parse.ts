082f5581910247ae9a8409c0d0f563ab9c5f9095
vbstreetz <vbstreetz@gmail.com>
2021-08-09 12:11:45 +0300

    try fix mixed rates precision

--- packages/data/queries/rateUpdates/parse.ts
+++ packages/data/queries/rateUpdates/parse.ts
@@ -1,14 +1,17 @@
+import { ethers } from 'ethers';
+import { NetworkId } from '@synthetixio/contracts-interface';
+
 import { RateUpdate } from '../../generated/graphql';
 import { formatTimestamp } from '../../src/utils';
-import { NetworkId } from '@synthetixio/contracts-interface';
 
 export const parseRates = (rate: RateUpdate, networkId?: number): RateUpdate => {
 	const { block, currencyKey, id, rate: rateValue, synth, timestamp } = rate;
-	const parsedRate =
-		networkId && (networkId === NetworkId['Kovan-Ovm'] || networkId === NetworkId['Mainnet-Ovm'])
-			? rateValue
-			: // todo: I have no idea what this code is supposed to do
-			  (rateValue / 1e18).toString();
+	let parsedRate: string = '';
+	try {
+		parsedRate = ethers.utils.formatEther(rateValue).toString();
+	} catch {
+		parsedRate = rateValue.toString();
+	}
 
 	return {
 		block: Number(block),
