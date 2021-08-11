7e8bfb1754a03e5242757c610791d53ea41d9e32
vbstreetz <vbstreetz@gmail.com>
2021-08-11 12:23:57 +0300

    safe formatEther

--- packages/data/queries/rateUpdates/parse.ts
+++ packages/data/queries/rateUpdates/parse.ts
@@ -1,23 +1,14 @@
-import { ethers } from 'ethers';
-import { NetworkId } from '@synthetixio/contracts-interface';
-
 import { RateUpdate } from '../../generated/graphql';
-import { formatTimestamp } from '../../src/utils';
+import { formatTimestamp, formatEther } from '../../src/utils';
 
 export const parseRates = (rate: RateUpdate, networkId?: number): RateUpdate => {
 	const { block, currencyKey, id, rate: rateValue, synth, timestamp } = rate;
-	let parsedRate: string = '';
-	try {
-		parsedRate = ethers.utils.formatEther(rateValue).toString();
-	} catch {
-		parsedRate = rateValue.toString();
-	}
 
 	return {
 		block: Number(block),
 		currencyKey,
 		id,
-		rate: parsedRate,
+		rate: formatEther(rateValue),
 		synth,
 		timestamp: formatTimestamp(timestamp),
 	};
