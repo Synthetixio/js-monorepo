b4880da597188e9bb4372f5af19852ec5b8a19be
Mitchel Kelonye <vbstreetz@gmail.com>
2021-08-23 22:08:36 +0300

    data pkg version

--- packages/queries/src/queries/staking/useDailyStakingActivityQuery.ts
+++ packages/queries/src/queries/staking/useDailyStakingActivityQuery.ts
@@ -1,7 +1,7 @@
 import { useQuery, UseQueryOptions } from 'react-query';
 
 import { QueryContext } from '../../context';
-import { DailyIssued, DailyBurned } from '@synthetixio/data/generated/graphql';
+import { DailyIssued, DailyBurned } from '@synthetixio/data/build/data/generated/graphql';
 
 const useDailyBurnedQuery = (
 	ctx: QueryContext,
