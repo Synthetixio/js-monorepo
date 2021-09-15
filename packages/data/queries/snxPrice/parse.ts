ceae8232cc67007b1193262d96a089b510f3e720
Mitchel Kelonye <vbstreetz@gmail.com>
2021-09-15 18:13:53 +0300

    wip

--- packages/data/queries/snxPrice/parse.ts
+++ packages/data/queries/snxPrice/parse.ts
@@ -1,9 +1,10 @@
 import { ethers } from 'ethers';
 import { DailySnxPrice, FifteenMinuteSnxPrice } from '../../generated/graphql';
+import { formatEther } from '../../src/utils';
 
 type SnxPriceSansCount = Omit<DailySnxPrice, 'count'> | Omit<FifteenMinuteSnxPrice, 'count'>;
 
 export const parseSnxPrice = ({ id, averagePrice }: SnxPriceSansCount): SnxPriceSansCount => ({
 	id,
-	averagePrice: ethers.utils.formatEther(averagePrice),
+	averagePrice: formatEther(averagePrice),
 });
