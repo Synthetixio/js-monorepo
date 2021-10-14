3a31d9106febdff33cf9ed6e0103a9c2af1f8dad
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-10-14 18:42:29 -0400

    fix: unreverse historical rates

--- packages/queries/src/queries/rates/useHistoricalRatesQuery.ts
+++ packages/queries/src/queries/rates/useHistoricalRatesQuery.ts
@@ -43,7 +43,7 @@ const useHistoricalRatesQuery = (
 				const change = calculateRateChange(rates);
 
 				return {
-					rates: rates.slice().reverse(),
+					rates: rates,
 					low,
 					high,
 					change,
