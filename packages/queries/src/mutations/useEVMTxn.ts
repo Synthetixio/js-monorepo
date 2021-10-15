09c80bcfc6ddaf304be2bcaed67e81bc9ab2b588
Clement Balestrat <clement.balestrat@gmail.com>
2021-10-15 15:53:59 +0200

    fix

--- packages/queries/src/mutations/useEVMTxn.ts
+++ packages/queries/src/mutations/useEVMTxn.ts
@@ -65,8 +65,8 @@ const useEVMTxn = (
 			estimateGas()
 				.then((gl) => {
 					/* eslint-disable no-console */
-					console.log('gas estimate', gl?.toString(), gl);
-					console.log('test', wei(gl?.toString() ?? 0).mul(wei(1 + options.gasLimitBuffer)));
+					console.log('gas estimate', gl?.toString(), gl, options.gasLimitBuffer);
+					console.log('test', wei(gl?.toString() ?? 0));
 					/* eslint-enable no-console */
 					if (gl) setGasLimit(wei(gl?.mul(1 + options.gasLimitBuffer)));
 				})
