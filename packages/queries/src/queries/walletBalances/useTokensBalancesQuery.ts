1ec49781f371e60c2cf8928459aaa4c602089c2d
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2021-10-18 15:44:45 +1100

    Remove `as`, tsignore,  lodash helpers and simplify mapping

--- packages/queries/src/queries/walletBalances/useTokensBalancesQuery.ts
+++ packages/queries/src/queries/walletBalances/useTokensBalancesQuery.ts
@@ -1,54 +1,42 @@
-import { useQuery, UseQueryOptions } from 'react-query';
+import { useQuery, UseQueryOptions, UseQueryResult } from 'react-query';
 import { wei } from '@synthetixio/wei';
 import erc20Abi from '../../abis/ERC20.json';
-import zipObject from 'lodash/zipObject';
-import omitBy from 'lodash/omitBy';
-import mapValues from 'lodash/mapValues';
-import keyBy from 'lodash/keyBy';
-import { ethers } from 'ethers';
-
+import { BigNumber, ethers } from 'ethers';
 import { CRYPTO_CURRENCY_MAP } from '../../currency';
-
 import { QueryContext } from '../../context';
 import { Token, TokenBalances } from '../../types';
 
+type UseTokensBalancesQueryReturn = UseQueryResult<TokenBalances>;
 const useTokensBalancesQuery = (
 	ctx: QueryContext,
 	tokens: Token[],
 	walletAddress: string | null,
 	options?: UseQueryOptions<TokenBalances>
-) => {
-	const symbols = tokens.map((token) => token.symbol);
-	const tokensMap = keyBy(tokens, 'symbol');
-
+): UseTokensBalancesQueryReturn => {
 	return useQuery<TokenBalances>(
 		['walletBalances', 'tokens', ctx.networkId, walletAddress, tokens.join()],
 		async () => {
-			// @ts-ignore
-			const calls = [];
-			for (const { address, symbol } of tokens) {
+			const getBalance = ({ address, symbol }: Token): Promise<ethers.BigNumber> => {
+				if (!ctx.provider || !walletAddress) return Promise.resolve(BigNumber.from(0));
 				if (symbol === CRYPTO_CURRENCY_MAP.ETH) {
-					calls.push(ctx.provider!.getBalance(walletAddress!));
+					return ctx.provider.getBalance(walletAddress);
 				} else {
-					const tokenContract = new ethers.Contract(address, erc20Abi, ctx.provider!);
-					calls.push(tokenContract.balanceOf(walletAddress));
+					const tokenContract = new ethers.Contract(address, erc20Abi, ctx.provider);
+					return tokenContract.balanceOf(walletAddress);
 				}
-			}
-
-			const data = (await Promise.all(calls)) as ethers.BigNumber[];
-
-			const balancesMap = zipObject(symbols, data);
-
-			const positiveBalances = omitBy(balancesMap, (entry: ethers.BigNumber) => entry.lte(0));
-
-			return mapValues(positiveBalances, (balance: ethers.BigNumber, symbol: string) => {
-				const token = tokensMap[symbol];
-
-				return {
-					balance: wei(balance, token.decimals ?? 18),
-					token,
-				};
+			};
+			const promises = tokens.map(async (token) => {
+				if (!ctx.provider || !walletAddress) return { balance: wei(0), token };
+				const balance = await getBalance(token);
+				return { balance: wei(balance, token.decimals ?? 18), token };
 			});
+
+			const data = await Promise.all(promises);
+			return data.reduce((acc: TokenBalances, val) => {
+				if (val.balance.lte(0)) return acc;
+				acc[val.token.symbol] = val;
+				return acc;
+			}, {});
 		},
 		{
 			enabled: !!ctx.provider && tokens.length > 0 && !!walletAddress,
