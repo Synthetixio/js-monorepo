da23f98c94eedbacc9969b3ffd437f7c1b1b0276
fritzschoff <max.j.f@gmx.de>
2021-10-29 17:33:40 +0200

    remove trailing slash

--- packages/optimism-networks/src/constants.ts
+++ packages/optimism-networks/src/constants.ts
@@ -17,7 +17,7 @@ export const OPTIMISM_NETWORKS: Record<number, OptimismNetwork> = {
 		chainId: '0xA',
 		chainName: 'Optimism Mainnet',
 		rpcUrls: ['https://mainnet.optimism.io'],
-		blockExplorerUrls: ['https://optimistic.etherscan.io/'],
+		blockExplorerUrls: ['https://optimistic.etherscan.io'],
 		iconUrls: [
 			'https://optimism.io/images/metamask_icon.svg',
 			'https://optimism.io/images/metamask_icon.png',
@@ -27,7 +27,7 @@ export const OPTIMISM_NETWORKS: Record<number, OptimismNetwork> = {
 		chainId: '0x45',
 		chainName: 'Optimism Kovan',
 		rpcUrls: ['https://kovan.optimism.io'],
-		blockExplorerUrls: ['https://kovan-optimistic.etherscan.io/'],
+		blockExplorerUrls: ['https://kovan-optimistic.etherscan.io'],
 		iconUrls: [
 			'https://optimism.io/images/metamask_icon.svg',
 			'https://optimism.io/images/metamask_icon.png',
