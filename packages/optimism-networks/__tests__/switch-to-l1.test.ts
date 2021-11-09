104b514067f4a5c2f465793907e7c27c25a182af
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2021-11-09 13:13:04 +1100

    Explicit any should be allowed in tests

--- packages/optimism-networks/__tests__/switch-to-l1.test.ts
+++ packages/optimism-networks/__tests__/switch-to-l1.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { switchToL1 } from '../src';
 
 describe('switchToL1', () => {
--- packages/optimism-networks/__tests__/switch-to-l2.test.ts
+++ packages/optimism-networks/__tests__/switch-to-l2.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { switchToL2 } from '../src';
 import { METAMASK_MISSING_NETWORK_ERROR_CODE } from '../src/constants';
 
