0006d7d0129315b57dab9bb53f662852d7c05ac8
Noah Litvin <noah.litvin@gmail.com>
2021-11-29 15:06:13 -0500

    linter fix

--- packages/queries/src/queries/debt/useGetDebtTimeseries.ts
+++ packages/queries/src/queries/debt/useGetDebtTimeseries.ts
@@ -6,9 +6,9 @@ import { useGetDebtStates, useGetDebtSnapshots } from '../../../generated/mainSu
 import { times, findIndex } from 'lodash';
 
 type WalletDebtTimeseriesData = {
-	timestamp: Wei,
-	debtPercentage: number,
-	debtAmount: number
+	timestamp: Wei;
+	debtPercentage: number;
+	debtAmount: number;
 }[];
 
 const useGetDebtTimeseries = (
@@ -19,36 +19,68 @@ const useGetDebtTimeseries = (
 	return useQuery<WalletDebtTimeseriesData>(
 		['debt', 'data', ctx.networkId, walletAddress],
 		async () => {
-			const debtStates = (await useGetDebtStates(ctx.subgraphEndpoints.main, { first: 100000, orderBy: 'timestamp', orderDirection: 'asc' }, { timestamp: true, debtRatio: true, totalIssuedSynths: true })).data
-			const debtSnapshots = (await useGetDebtSnapshots(ctx.subgraphEndpoints.main, { first: 100000, orderBy: 'timestamp', orderDirection: 'asc', where: { account: walletAddress }}, { timestamp: true, account: true, debtBalanceOf: true })).data
-			let timeseries:WalletDebtTimeseriesData = []
+			const debtStates = (
+				await useGetDebtStates(
+					ctx.subgraphEndpoints.main,
+					{ first: 100000, orderBy: 'timestamp', orderDirection: 'asc' },
+					{ timestamp: true, debtRatio: true, totalIssuedSynths: true }
+				)
+			).data;
+			const debtSnapshots = (
+				await useGetDebtSnapshots(
+					ctx.subgraphEndpoints.main,
+					{
+						first: 100000,
+						orderBy: 'timestamp',
+						orderDirection: 'asc',
+						where: { account: walletAddress },
+					},
+					{ timestamp: true, account: true, debtBalanceOf: true }
+				)
+			).data;
+			const timeseries: WalletDebtTimeseriesData = [];
 
-			if(debtStates && debtSnapshots) {
+			if (debtStates && debtSnapshots) {
 				times(debtSnapshots.length, () => {
-					let currentDebtSnapshotIndex = 0
-					let debtStateAsOfDebtSnapshotIndex = findIndex(debtStates, (ds) => ds.timestamp < debtSnapshots[currentDebtSnapshotIndex].timestamp)
-				
+					let currentDebtSnapshotIndex = 0;
+					let debtStateAsOfDebtSnapshotIndex = findIndex(
+						debtStates,
+						(ds) => ds.timestamp < debtSnapshots[currentDebtSnapshotIndex].timestamp
+					);
+
 					for (const debtState of debtStates.slice(debtStateAsOfDebtSnapshotIndex)) {
-						if (currentDebtSnapshotIndex < debtSnapshots.length - 1 && debtSnapshots[currentDebtSnapshotIndex + 1].timestamp < debtState.timestamp) {
+						if (
+							currentDebtSnapshotIndex < debtSnapshots.length - 1 &&
+							debtSnapshots[currentDebtSnapshotIndex + 1].timestamp < debtState.timestamp
+						) {
 							currentDebtSnapshotIndex++;
-							debtStateAsOfDebtSnapshotIndex = findIndex(debtStates, (ds) => ds.timestamp < debtSnapshots[currentDebtSnapshotIndex].timestamp)
+							debtStateAsOfDebtSnapshotIndex = findIndex(
+								debtStates,
+								(ds) => ds.timestamp < debtSnapshots[currentDebtSnapshotIndex].timestamp
+							);
 						}
-		
-						let currentDebtSnapshot = debtSnapshots[currentDebtSnapshotIndex]
-						currentDebtSnapshot.debtBalanceOf = currentDebtSnapshot.debtBalanceOf || wei(0)
-						const debtStateAsOfDebtSnapshot = debtStates[debtStateAsOfDebtSnapshotIndex]
+
+						const currentDebtSnapshot = debtSnapshots[currentDebtSnapshotIndex];
+						currentDebtSnapshot.debtBalanceOf = currentDebtSnapshot.debtBalanceOf || wei(0);
+						const debtStateAsOfDebtSnapshot = debtStates[debtStateAsOfDebtSnapshotIndex];
 
 						timeseries.push({
 							timestamp: wei(debtState.timestamp),
-							debtPercentage:(currentDebtSnapshot.debtBalanceOf.toNumber() / debtState.totalIssuedSynths.toNumber()) * debtStateAsOfDebtSnapshot.debtRatio.toNumber() / debtState.debtRatio.toNumber(),
-							debtAmount: currentDebtSnapshot.debtBalanceOf.toNumber() * debtStateAsOfDebtSnapshot.debtRatio.toNumber() / debtState.debtRatio.toNumber()
-						})
+							debtPercentage:
+								((currentDebtSnapshot.debtBalanceOf.toNumber() /
+									debtState.totalIssuedSynths.toNumber()) *
+									debtStateAsOfDebtSnapshot.debtRatio.toNumber()) /
+								debtState.debtRatio.toNumber(),
+							debtAmount:
+								(currentDebtSnapshot.debtBalanceOf.toNumber() *
+									debtStateAsOfDebtSnapshot.debtRatio.toNumber()) /
+								debtState.debtRatio.toNumber(),
+						});
 					}
-
-				})
+				});
 			}
 
-			return  timeseries;
+			return timeseries;
 		},
 		{
 			enabled: ctx.networkId != null && walletAddress != null,
