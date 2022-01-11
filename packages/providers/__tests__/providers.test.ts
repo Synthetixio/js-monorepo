3893b29eb6a45222e2c19980b02c515b29328d95
fritzschoff <max.j.f@gmx.de>
2022-01-11 12:34:20 +0000

    removed unused import

--- packages/providers/__tests__/providers.test.ts
+++ packages/providers/__tests__/providers.test.ts
@@ -1,8 +1,4 @@
 import { ERRORS } from '../src/constants';
-import { handleSwitchChain } from '../src/index';
-// @ts-ignore
-// eslint-disable-next-line import/no-unresolved
-import { NetworkId } from '@synthetixio/contracts-inferface';
 
 describe('@synthetixio/providers', () => {
 	it('needs tests', () => {
