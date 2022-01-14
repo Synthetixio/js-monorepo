7b4aad9449ba50be4d35eac10c1d5f29fdd109b4
AndyTCF <andy.c.f.98@gmail.com>
2022-01-15 09:40:47 +1100

    add numbers

--- packages/queries/src/queries/gov/constants.ts
+++ packages/queries/src/queries/gov/constants.ts
@@ -1,7 +1,9 @@
 export const electionAuthor = '0xAFe05574a3653cdE39c8Fb842f761F5326Aa424A';
+// @TODO: If numbers change - need to update these
 export const numOfGrantMembers = 5;
 export const numOfAmbassadorMembers = 3;
 export const numOfTreasuryMembers = 4;
+export const numOfCouncilMembers = 8;
 
 export enum SPACE_KEY {
 	COUNCIL = 'spartancouncil.eth',
