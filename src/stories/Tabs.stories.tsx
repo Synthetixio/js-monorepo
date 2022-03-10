e1a688373e151e9f55980318eb874f0a1863491d
fritzschoff <max.j.f@gmx.de>
2022-03-10 16:31:24 -0600

    feat(tabs): added icon to tabs

--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Tabs from '../components/Tabs';
+import CloseIcon from '../components/Icons/CloseIcon';
+
 export default {
   title: 'Tabs',
   component: Tabs,
@@ -9,8 +11,16 @@ export default {
 const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
 
 export const Primary = Template.bind({});
+export const WithoutIcon = Template.bind({});
 
 Primary.args = {
+  titles: ['first', 'second', 'third'],
+  onClick: () => {},
+  activeIndex: 0,
+  icon: <CloseIcon />,
+};
+
+WithoutIcon.args = {
   titles: ['first', 'second', 'third'],
   onClick: () => {},
   activeIndex: 0,
