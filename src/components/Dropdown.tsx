5dd10a681867b5edc38a2983e2c454cf11f0a5df
fritzschoff <max.j.f@gmx.de>
2022-03-14 16:22:08 +0100

    feat(dropdown): added new styling option

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -4,14 +4,18 @@ import colors from '../styles/colors';
 
 interface DropdownProps {
   elements: JSX.Element[];
-  parent: JSX.Element;
+  noBackground?: boolean;
 }
 
-export default function Dropdown({ elements }: DropdownProps) {
+export default function Dropdown({ elements, noBackground }: DropdownProps) {
   return (
-    <StyledUnorderedList>
+    <StyledUnorderedList noBackground={noBackground}>
       {elements.map((element, index) => (
-        <StyledListElement isEven={index % 2 === 0} key={index}>
+        <StyledListElement
+          isEven={index % 2 === 0}
+          key={index}
+          noBackground={noBackground}
+        >
           {element}
         </StyledListElement>
       ))}
@@ -19,18 +23,26 @@ export default function Dropdown({ elements }: DropdownProps) {
   );
 }
 
-const StyledUnorderedList = styled.ul`
+const StyledUnorderedList = styled.ul<{
+  noBackground?: DropdownProps['noBackground'];
+}>`
   display: flex;
   flex-direction: column;
   width: 100%;
-  background-color: ${colors.backgroundColor};
+  background-color: ${({ noBackground }) =>
+    !noBackground && colors.backgroundColor};
   padding: 0;
+  border-radius: 8px;
 `;
 
-const StyledListElement = styled.li<{ isEven: boolean }>`
+const StyledListElement = styled.li<{
+  isEven: boolean;
+  noBackground?: DropdownProps['noBackground'];
+}>`
   width: 100%;
   list-style-type: none;
-  background-color: ${({ isEven }) => isEven && 'rgba(0,0,0,0.2)'};
+  background-color: ${({ isEven, noBackground }) =>
+    isEven && !noBackground && 'rgba(0,0,0,0.2)'};
   cursor: pointer;
   :hover {
     background-color: ${colors.black};
