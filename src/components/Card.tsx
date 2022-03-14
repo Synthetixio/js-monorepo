2f6be8789bbc36c675f740b6b424ae20441b73b7
fritzschoff <max.j.f@gmx.de>
2022-03-14 17:18:52 +0100

    cealn up

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 import colors from '../styles/colors';
-import spacings from '../styles/spacings';
 
 interface CardProps {
   withGradientWrapper?: boolean;
@@ -22,13 +21,11 @@ export default function Card({
 }
 
 const StyledGradientWrapper = styled.div`
+  width: 100%;
+  height: 100%;
   border-radius: 5px;
   background: ${colors.gradients.rainbow};
-  box-shadow: 0px 0px 5px ${colors.lightBlue.primary};
   padding: 2px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   border-radius: 4px;
 `;
 
