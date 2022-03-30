02bab5ae772535e2f55b2f8c9df47fdb3967fe52
fritzschoff <max.j.f@gmx.de>
2022-03-30 14:03:49 +0200

    removed unused fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,40 +1,32 @@
 import { css } from 'styled-components';
 
 export default {
-  body: css`
-    font-family: Inter;
-    font-size: 1.16rem;
-  `,
-  h1: css`
-    font-family: Inter;
-    font-size: 3.33rem;
-  `,
-  h2: css`
-    font-family: Inter;
-    font-size: 2.66rem;
-  `,
-  h3: css`
-    font-family: Inter;
-    font-size: 2rem;
-  `,
-  h4: css`
-    font-family: Inter;
-    font-size: 1.5rem;
-  `,
-  h5: css`
-    font-family: Inter;
-    font-size: 1.16rem;
-  `,
-  h6: css`
-    font-family: Inter;
-    font-size: 1rem;
-  `,
-  regular: `Inter`,
-  interBold: `Inter Bold`,
-  interSemiBold: `Inter Semi Bold`,
-  mono: `GT America Mono`,
-  condensedBold: `GT America Condensed-Bold`,
-  condensedMedium: `GT America Condensed-Medium`,
-  extended: `GT America Extended Bold`,
-  expanded: `GT America Expanded Black`,
+	body: css`
+		font-family: Inter;
+		font-size: 1.16rem;
+	`,
+	h1: css`
+		font-family: Inter;
+		font-size: 3.33rem;
+	`,
+	h2: css`
+		font-family: Inter;
+		font-size: 2.66rem;
+	`,
+	h3: css`
+		font-family: Inter;
+		font-size: 2rem;
+	`,
+	h4: css`
+		font-family: Inter;
+		font-size: 1.5rem;
+	`,
+	h5: css`
+		font-family: Inter;
+		font-size: 1.16rem;
+	`,
+	h6: css`
+		font-family: Inter;
+		font-size: 1rem;
+	`,
 };
