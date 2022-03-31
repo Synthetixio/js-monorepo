ae2d88c857223581641c68818bbd8f371bfaae41
fritzschoff <max.j.f@gmx.de>
2022-03-31 13:49:36 +0200

    feat(webpack): added auto export lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const AutoExport = require('webpack-auto-export');
 
 module.exports = {
 	entry: './src/index.ts',
 	mode: 'production',
+	plugins: [
+		new AutoExport({
+			extension: '.js', // define extension of generated index file
+			exportType: 'named', // the default way to export. values can be: 'named' | 'default' | 'detect'
+			baseDir: './src', // base directory to observe the changes
+			paths: ['components'],
+		}),
+	],
 	module: {
 		rules: [
 			{
