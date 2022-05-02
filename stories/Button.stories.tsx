859ec418298259885604e1ad497d9f3b0c0e1122
fritzschoff <max.j.f@gmx.de>
2022-05-02 15:10:12 +0100

    feat(button): added new variant to button component

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -16,7 +16,7 @@ export const Template: ComponentStory<typeof Button> = (args) => (
 );
 
 Template.args = {
-	text: 'I am a Button',
+	children: 'I am a Button',
 	variant: 'primary',
 	type: 'button',
 	size: 'large',
@@ -28,4 +28,25 @@ Template.parameters = {
 		type: 'figma',
 		url: 'https://www.figma.com/file/zeCepPb3Bo6Fd92FdcolUT/v1.0?node-id=918%3A18443',
 	},
+};
+
+export const Quaternary: ComponentStory<typeof Button> = (args) => (
+	<div style={{ backgroundColor: 'red', padding: '10px' }}>
+		<Button {...args} />
+	</div>
+);
+
+Quaternary.args = {
+	children: (
+		<>
+			<span style={{ backgroundColor: 'green', minWidth: 8, minHeight: 8, borderRadius: '50%' }}>
+				.
+			</span>
+			I am a Button
+		</>
+	),
+	variant: 'primary',
+	type: 'button',
+	size: 'large',
+	disabled: false,
 };
