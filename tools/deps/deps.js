8eaae3e2324394d4287d00c58736d2ab35012b7e
nk <nikita@cc.snxdao.io>
2022-06-03 10:21:44 +0800

    Lint harded

--- tools/deps/deps.js
+++ tools/deps/deps.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
 
 const depcheck = require('depcheck');
 const cp = require('child_process');
--- tools/deps/mismatched.js
+++ tools/deps/mismatched.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
 
 const path = require('path');
 const cp = require('child_process');
