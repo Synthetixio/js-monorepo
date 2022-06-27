e39a78189b6302c6c4083e43414fbfdb7a152a06
Nikita <nikita@cc.snxdao.io>
2022-06-27 10:59:15 +0800

    Add @synthetixio/safe-import

---
+++ tools/safe-import/safeLazy.js
@@ -0,0 +1,10 @@
+const { lazy } = require('react');
+const { safeImport } = require('./safeImport');
+
+function safeLazy(importer) {
+	return lazy(() => safeImport(importer));
+}
+
+module.exports = {
+	safeLazy,
+};
