ad4f81709b894d508b08a1f1622f81a1fa195dad
joey <joey@cc.snxdao.io>
2022-07-21 12:54:38 +0200

    Add suppport for Goerli in contracts interface

--- packages/contracts-interface/__tests__/index.test.ts
+++ packages/contracts-interface/__tests__/index.test.ts
@@ -47,6 +47,7 @@ describe('@synthetixio/js tests', () => {
 	test('should include the supported networks', () => {
 		expect(snxjs.networkToChainId[NetworkNameById[1]]).toBe(NetworkIdByName.mainnet.toString());
 		expect(snxjs.networkToChainId[NetworkNameById[42]]).toBe(NetworkIdByName.kovan.toString());
+		expect(snxjs.networkToChainId[NetworkNameById[5]]).toBe(NetworkIdByName.goerli.toString());
 	});
 
 	test('should include the current network', () => {
