a10fc2212dcfc44328705bee09b16b8b993ff9b3
Sunny Vempati <sunnyvempati@gmail.com>
2022-08-03 18:27:50 -0500

    remove tabs

--- apps/v3/webpack.config.ts
+++ apps/v3/webpack.config.ts
@@ -4,93 +4,97 @@ import { Configuration } from 'webpack';
 import 'webpack-dev-server';
 
 const htmlPlugin = new HtmlWebpackPlugin({
-	template: path.join(__dirname, 'public', 'index.html'),
-	scriptLoading: 'defer',
-	minify: false,
-	hash: false,
-	xhtml: true,
+  template: path.join(__dirname, 'public', 'index.html'),
+  scriptLoading: 'defer',
+  minify: false,
+  hash: false,
+  xhtml: true,
 });
 
 const tsxRule = {
-	test: /\.(ts|js)x?$/,
-	exclude: /node_modules/,
-	use: {
-		loader: 'babel-loader',
-		options: {
-			presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-		},
-	},
+  test: /\.(ts|js)x?$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+    },
+  },
 };
 
 const svgRule = {
-	test: /\.svg$/,
-	use: '@svgr/webpack',
+  test: /\.svg$/,
+  use: '@svgr/webpack',
 };
 
 const imgRule = {
-	test: /\.(png|jpg|ico|gif|woff|woff2|ttf|eot|doc|pdf|zip|wav|avi|txt|webp)$/,
-	type: 'asset',
-	parser: {
-		dataUrlCondition: {
-			maxSize: 4 * 1024, // 4kb
-		},
-	},
+  test: /\.(png|jpg|ico|gif|woff|woff2|ttf|eot|doc|pdf|zip|wav|avi|txt|webp)$/,
+  type: 'asset',
+  parser: {
+    dataUrlCondition: {
+      maxSize: 4 * 1024, // 4kb
+    },
+  },
 };
 
 const cssRule = {
-	test: /\.css$/,
-	use: ['style-loader', 'css-loader'],
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader'],
 };
 
 const devServer = {
-	port: '3000',
+  port: '3000',
 
-	hot: true,
-	liveReload: false,
+  hot: true,
+  liveReload: false,
 
-	historyApiFallback: {
-		index: 'public/index.html',
-	},
+  historyApiFallback: {
+    index: 'public/index.html',
+  },
 
-	devMiddleware: {
-		writeToDisk: true,
-		publicPath: '/',
-	},
+  devMiddleware: {
+    writeToDisk: true,
+    publicPath: '/',
+  },
 
-	client: {
-		logging: 'log',
-		overlay: false,
-		progress: false,
-	},
+  client: {
+    logging: 'log',
+    overlay: false,
+    progress: false,
+  },
 
-	allowedHosts: 'all',
-	open: false,
-	compress: true,
+  allowedHosts: 'all',
+  open: false,
+  compress: true,
 };
 
 const config: Configuration = {
-	devtool: 'eval',
-	mode: 'development',
-	entry: './src/index.tsx',
-	// @ts-ignore
-	devServer,
+  devtool: 'eval',
+  mode: 'development',
+  entry: './src/index.tsx',
+  // @ts-ignore
+  devServer,
 
-	plugins: [htmlPlugin],
+  plugins: [htmlPlugin],
 
-	resolve: {
-		fallback: {
-			stream: false,
-			crypto: false,
-			http: false,
-			https: false,
-			os: false,
-		},
-		extensions: ['.ts', '.tsx', '.js', '.jsx'],
-	},
+  resolve: {
+    fallback: {
+      stream: false,
+      crypto: false,
+      http: false,
+      https: false,
+      os: false,
+    },
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
 
-	module: {
-		rules: [tsxRule, svgRule, imgRule, cssRule],
-	},
+  module: {
+    rules: [tsxRule, svgRule, imgRule, cssRule],
+  },
 };
 
 export default config;
