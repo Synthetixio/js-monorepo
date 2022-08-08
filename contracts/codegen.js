7fccc8e32a209035af1f25d623b101c9adb9d80a
jmzwar <james.zwar@protonmail.com>
2022-08-08 19:41:38 +1000

    hydrate codegen

--- contracts/codegen.js
+++ contracts/codegen.js
@@ -6,6 +6,7 @@ const prettier = require('prettier');
 const prettierOptions = JSON.parse(fs.readFileSync('../.prettierrc', 'utf8'));
 
 const synthetixPath = path.dirname(require.resolve('synthetix'));
+
 const deployed = path.join(synthetixPath, 'publish/deployed');
 const networks = fs
 	.readdirSync(deployed, { withFileTypes: true })
@@ -15,40 +16,29 @@ const networks = fs
 function generateTargets(network) {
 	const deployment = require(`synthetix/publish/deployed/${network}/deployment.json`);
 	fs.mkdirSync(`generated/${network}/deployment/targets`, { recursive: true });
-	if (!deployment.targets) {
+	const { targets, sources } = deployment;
+
+	if (!targets || !sources) {
 		return;
 	}
 
-	Object.entries(deployment.targets).forEach(([key, val]) => {
-		fs.writeFileSync(
-			`generated/${network}/deployment/targets/${key}.ts`,
-			prettier.format(
-				Object.entries(val)
-					.filter(([name]) => ['name', 'source', 'address'].includes(name))
-					.map(([name, value]) => `export const ${name} = ${JSON.stringify(value, null, 2)};`)
-					.join('\n'),
-				{ parser: 'typescript', ...prettierOptions }
-			),
-			'utf8'
-		);
-	});
-}
+	return Object.values(targets).map((target) => {
+		if (target.name === 'Synthetix') {
+			target.address = targets.ProxySynthetix.address;
+		} else if (target.name === 'FeePool') {
+			target.address = targets.ProxyFeePool.address;
+		} else if (target.name.match(/Synth(s|i)[a-zA-Z]+$/)) {
+			const newTarget = target.name.replace('Synth', 'Proxy');
+			target.address = targets[newTarget].address;
+		}
+		const iface = new ethers.utils.Interface(sources[target.source].abi);
+		target.abi = iface.format(ethers.utils.FormatTypes.full);
 
-function generateSources(network) {
-	const deployment = require(`synthetix/publish/deployed/${network}/deployment.json`);
-	fs.mkdirSync(`generated/${network}/deployment/sources`, { recursive: true });
-	if (!deployment.sources) {
-		return;
-	}
-
-	Object.entries(deployment.sources).forEach(([key, val]) => {
-		const iface = new ethers.utils.Interface(val.abi);
-		val.abi = iface.format(ethers.utils.FormatTypes.full);
 		fs.writeFileSync(
-			`generated/${network}/deployment/sources/${key}.ts`,
+			`generated/${network}/deployment/${target.name}.ts`,
 			prettier.format(
-				Object.entries(val)
-					.filter(([name]) => ['abi'].includes(name))
+				Object.entries(target)
+					.filter(([name]) => ['name', 'source', 'address', 'abi'].includes(name))
 					.map(([name, value]) => `export const ${name} = ${JSON.stringify(value, null, 2)};`)
 					.join('\n'),
 				{ parser: 'typescript', ...prettierOptions }
@@ -77,6 +67,5 @@ function generateSynths(network) {
 
 networks.forEach((network) => {
 	generateTargets(network);
-	generateSources(network);
 	generateSynths(network);
 });
