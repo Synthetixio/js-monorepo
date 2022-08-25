6e5029643bf46024738b0cacfd4e22471d12a0a3
Peiman <peymannaderi3@yahoo.com>
2022-08-25 17:41:23 +0430

    fix: v3 theme import issue

---
+++ v3/theme/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp ./src/style.css ./build
--- v3/ui/custom.d.ts
+++ v3/ui/custom.d.ts
@@ -1,5 +1,3 @@
 declare module '*.svg';
 declare module '*.png';
 declare module '*.css';
-// TODO: setup types for v3-theme
-declare module '@synthetixio/v3-theme';
