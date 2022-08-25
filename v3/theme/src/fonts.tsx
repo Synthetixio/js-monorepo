fdd7e2c761dcd34e03ce41025f58bceca3a0a1eb
fritzschoff <fritzschoff@icloud.com>
2022-08-25 11:08:02 +0200

    fixed global component with styles

--- v3/theme/src/fonts.tsx
+++ v3/theme/src/fonts.tsx
@@ -1,45 +1,66 @@
-import { Global } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import React from 'react';
 
 export const Fonts = () => (
   <Global
-    styles={`
-    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@200..700&display=swap");
-    @font-face {
-      font-family: 'GT America Condensed';
-      font-style: normal;
-      font-display: swap;
-      font-weight: 700;
-      src: local('GT America Condensed'),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Bold.woff2") format("woff2"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Bold.woff") format("woff"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Bold.otf") format("opentype");
-    }
-    @font-face {
-      font-family: 'GT America Condensed';
-      font-style: italic;
-      font-display: swap;
-      font-weight: 700;
-      src: local('GT America Condensed'),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Bold%20Italic.woff2") format("woff2"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Bold%20Italic.woff") format("woff"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Bold%20Italic.otf") format("opentype");
-    }
-    @font-face {
-      font-family: 'GT America Condensed';
-      font-style: normal;
-      font-display: swap;
-      font-weight: 100;
-      src: local('GT America Condensed'),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Thin.woff2") format("woff2"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Thin.woff") format("woff"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Thin.otf") format("opentype");
-    }
-    @font-face {
-      font-family: 'GT America Condensed';
-      font-style: italic;
-      font-display: swap;
-      font-weight: 100;
-      src: local('GT America Condensed'),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Thin%20Italic.woff2") format("woff2"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Thin%20Italic.woff") format("woff"),url("https://fonts.synthetix.io/GT%20America%20Condensed%20Thin%20Italic.otf") format("opentype");
-    }
-    @font-face {
-      font-family: 'GT America Mono';
-      font-style: normal;
-      font-display: swap;
-      font-weight: 700;
-      src: local('GT America Mono'),url("https://fonts.synthetix.io/GT%20America%20Mono%20Bold.woff2") format("woff2"),url("https://fonts.synthetix.io/GT%20America%20Mono%20Bold.woff") format("woff"),url("https://fonts.synthetix.io/GT%20America%20Mono%20Bold.otf") format("opentype");
-    }
-      `}
+    styles={css`
+      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200..700&display=swap');
+      @font-face {
+        font-family: 'GT America Condensed';
+        font-style: normal;
+        font-display: swap;
+        font-weight: 700;
+        src: local('GT America Condensed'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Bold.woff2') format('woff2'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Bold.woff') format('woff'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Bold.otf') format('opentype');
+      }
+      @font-face {
+        font-family: 'GT America Condensed';
+        font-style: italic;
+        font-display: swap;
+        font-weight: 700;
+        src: local('GT America Condensed'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Bold%20Italic.woff2')
+            format('woff2'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Bold%20Italic.woff')
+            format('woff'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Bold%20Italic.otf')
+            format('opentype');
+      }
+      @font-face {
+        font-family: 'GT America Condensed';
+        font-style: normal;
+        font-display: swap;
+        font-weight: 100;
+        src: local('GT America Condensed'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Thin.woff2') format('woff2'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Thin.woff') format('woff'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Thin.otf') format('opentype');
+      }
+      @font-face {
+        font-family: 'GT America Condensed';
+        font-style: italic;
+        font-display: swap;
+        font-weight: 100;
+        src: local('GT America Condensed'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Thin%20Italic.woff2')
+            format('woff2'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Thin%20Italic.woff')
+            format('woff'),
+          url('https://fonts.synthetix.io/GT%20America%20Condensed%20Thin%20Italic.otf')
+            format('opentype');
+      }
+      @font-face {
+        font-family: 'GT America Mono';
+        font-style: normal;
+        font-display: swap;
+        font-weight: 700;
+        src: local('GT America Mono'),
+          url('https://fonts.synthetix.io/GT%20America%20Mono%20Bold.woff2') format('woff2'),
+          url('https://fonts.synthetix.io/GT%20America%20Mono%20Bold.woff') format('woff'),
+          url('https://fonts.synthetix.io/GT%20America%20Mono%20Bold.otf') format('opentype');
+      }
+    `}
   />
 );
