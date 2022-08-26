72c7c55207a295d5fbd70702f1dd11f1d1665fe8
Peiman <peymannaderi3@yahoo.com>
2022-08-26 23:53:16 +0430

    feat: add use approve hook

---
+++ v3/ui/src/hooks/useApprove.ts
@@ -0,0 +1,43 @@
+import { BigNumberish } from 'ethers';
+import { useCallback, useMemo } from 'react';
+import { erc20ABI, useAccount, useContractRead, useContractWrite, useNetwork } from 'wagmi';
+
+export const useApprove = (contractAddress: string, amount: BigNumberish, spender: string) => {
+  const { address: accountAddress } = useAccount();
+  const { chain: activeChain } = useNetwork();
+  const hasWalletConnected = Boolean(activeChain);
+
+  const { writeAsync, isLoading } = useContractWrite({
+    mode: 'recklesslyUnprepared',
+    addressOrName: contractAddress,
+    contractInterface: erc20ABI,
+    functionName: 'approve',
+    args: [spender, amount],
+  });
+
+  const { data: allowance, refetch: refetchAllowance } = useContractRead({
+    addressOrName: contractAddress,
+    contractInterface: erc20ABI,
+    functionName: 'allowance',
+    args: [accountAddress, spender],
+    enabled: hasWalletConnected,
+  });
+
+  const sufficientAllowance = useMemo(() => {
+    return allowance && allowance.gt(0) && allowance.gte(amount);
+  }, [allowance, amount]);
+
+  const approve = useCallback(async () => {
+    if (!sufficientAllowance) {
+      await writeAsync();
+      refetchAllowance();
+    }
+  }, [refetchAllowance, sufficientAllowance, writeAsync]);
+
+  return {
+    isLoading,
+    approve,
+    refetchAllowance,
+    requireApproval: !sufficientAllowance,
+  };
+};
