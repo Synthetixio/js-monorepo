f475154298932788b97feab3bd8f2cac3c6778a5
Sunny Vempati <sunnyvempati@gmail.com>
2022-09-01 19:12:24 -0500

    remove hardhat deployments

--- v3/ui/ts-deployments/hardhat/synthetix.CoreAccountRBACMixinModuleMock.ts
+++
@@ -1,8 +0,0 @@
-export const address = '0xB91ac80B781a7a0592f76D503b72240e41685c24';
-export const abi = [
-  'error PermissionDenied(uint256 accountId, bytes32 permission, address target)',
-  'function mock_AccountRBACMixin_deposit(uint256 accountId, uint256 newDepositMock)',
-  'function mock_AccountRBACMixin_getDepositMock() view returns (uint256)',
-  'function mock_AccountRBACMixin_getMintMock() view returns (uint256)',
-  'function mock_AccountRBACMixin_mint(uint256 accountId, uint256 newMintMock)',
-];
--- v3/ui/ts-deployments/hardhat/synthetix.CoreMulticallModule.ts
+++
@@ -1,2 +0,0 @@
-export const address = '0x6A2aeBdD74C86F96b9f39CCAb25aD0f3c87b0F09';
-export const abi = ['function multicall(bytes[] data) payable returns (bytes[] results)'];
