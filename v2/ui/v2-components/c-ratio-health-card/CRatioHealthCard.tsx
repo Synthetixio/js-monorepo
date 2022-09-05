1403a6cd2a37d6cb8b87261a36a35f06b70328e2
joey <joey@cc.snxdao.io>
2022-09-05 13:03:35 +1000

    Add translation for CRatioHealthCard

--- v2/ui/v2-components/c-ratio-health-card/CRatioHealthCard.tsx
+++ v2/ui/v2-components/c-ratio-health-card/CRatioHealthCard.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import { getHealthVariant } from '@snx-v2/getHealthVariant';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { CRatioProgressBar } from './CRatioProgressBar';
 
 type Props = {
@@ -12,39 +13,42 @@ export const CRatioHealthCard: React.FC<Props> = ({
   targetCratioPercentage,
   liquidationCratioPercentage,
   currentCRatioPercentage,
-}) => (
-  <Box>
-    <Flex justifyContent="space-between" alignItems="center" marginBottom="2">
-      <Box>
-        <Heading size="md">Collateralization Ratio Health</Heading>
-        <Text>In order to keep gathering rewards you must keep you C-Ratio above Target</Text>
-      </Box>
-      <Flex
-        bg="blackAlpha.800"
-        alignItems="center"
-        borderRadius="md"
-        border="1px"
-        borderColor="gray.900"
-        padding="2"
-      >
-        <Text
-          color={getHealthVariant({
-            targetCratioPercentage,
-            liquidationCratioPercentage,
-            currentCRatioPercentage,
-          })}
-          fontSize="2xl"
-          align="center"
-          fontFamily="mono"
+}) => {
+  const { t } = useTranslation();
+  return (
+    <Box>
+      <Flex justifyContent="space-between" alignItems="center" marginBottom="2">
+        <Box>
+          <Heading size="md">{t('staking-v2.cratio-health-card.heading')}</Heading>
+          <Text> {t('staking-v2.cratio-health-card.sub-heading')}</Text>
+        </Box>
+        <Flex
+          bg="blackAlpha.800"
+          alignItems="center"
+          borderRadius="md"
+          border="1px"
+          borderColor="gray.900"
+          padding="2"
         >
-          {currentCRatioPercentage}%
-        </Text>
+          <Text
+            color={getHealthVariant({
+              targetCratioPercentage,
+              liquidationCratioPercentage,
+              currentCRatioPercentage,
+            })}
+            fontSize="2xl"
+            align="center"
+            fontFamily="mono"
+          >
+            {currentCRatioPercentage}%
+          </Text>
+        </Flex>
       </Flex>
-    </Flex>
-    <CRatioProgressBar
-      targetCratioPercentage={targetCratioPercentage}
-      liquidationCratioPercentage={liquidationCratioPercentage}
-      currentCRatioPercentage={currentCRatioPercentage}
-    />
-  </Box>
-);
+      <CRatioProgressBar
+        targetCratioPercentage={targetCratioPercentage}
+        liquidationCratioPercentage={liquidationCratioPercentage}
+        currentCRatioPercentage={currentCRatioPercentage}
+      />
+    </Box>
+  );
+};
