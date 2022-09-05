e3b241cd459abdfa11b0264f6232c0ebf7bf5809
jmzwar <james.zwar@protonmail.com>
2022-09-05 17:30:49 +1000

    fix unused

--- v2/ui/content/DashboardPage.tsx
+++ v2/ui/content/DashboardPage.tsx
@@ -16,8 +16,6 @@ import useSelectedPriceCurrency from 'hooks/useSelectedPriceCurrency';
 import StakedValue from 'sections/shared/modals/StakedValueModal/StakedValueBox';
 import ActiveDebt from 'sections/shared/modals/DebtValueModal/DebtValueBox';
 import Connector from 'containers/Connector';
-import { StatItem } from 'v2-components/stat-item';
-import { Box } from '@chakra-ui/react';
 
 const DashboardPage: FC = () => {
   const { t } = useTranslation();
