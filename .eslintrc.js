75d7c004b11c56857bb16fdd5dd58782b2985c69
joey <joey@cc.snxdao.io>
2022-09-05 11:47:07 +1000

    Add react/jsx-curly-brace-presence for v3 too

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,6 +68,7 @@ module.exports = {
 
       rules: {
         quotes: 'off',
+        'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
 
         // react-hooks
         'react-hooks/rules-of-hooks': 'error',
