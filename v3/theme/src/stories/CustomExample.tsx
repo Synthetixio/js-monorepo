e7c3b0b909bd5197acff4798ec13c5834f881260
max <39312833+fritzschoff@users.noreply.github.com>
2022-09-05 10:44:29 +0200

    fix: fix color of tooltip arrow (#296)

---
+++ v3/theme/src/stories/CustomExample.tsx
@@ -0,0 +1,17 @@
+import { Box, Button, CircularProgress, Spinner, Tooltip } from '@chakra-ui/react';
+import React from 'react';
+
+export const CustomExample = () => {
+  return (
+    <Box p={4} background={'whiteAlpha.200'}>
+      <Spinner />
+      <Button isLoading colorScheme="gray" variant={'outline'}>
+        Loading
+      </Button>
+      <CircularProgress isIndeterminate />
+      <Tooltip label="test" hasArrow>
+        sjhdsa
+      </Tooltip>
+    </Box>
+  );
+};
