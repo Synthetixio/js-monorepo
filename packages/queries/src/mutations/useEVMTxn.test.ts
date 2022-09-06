191b379a7fb9555c37d9e2eb99f771e3d04779b3
Nikita <nikita@cc.snxdao.io>
2022-09-06 11:33:33 +0800

    Refactor useEVMTxn.test.ts

--- packages/queries/src/mutations/useEVMTxn.test.ts
+++ packages/queries/src/mutations/useEVMTxn.test.ts
@@ -1,74 +1,42 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { renderHook } from '@testing-library/react-hooks';
-import { BigNumber } from '@ethersproject/bignumber';
-import { getWrapper } from '../testUtils';
-import useEVMTxn from './useEVMTxn';
-
 describe('useEVMTxn', () => {
-  test('Does not call estimateGas when txn is null', async () => {
-    const estimateGasMock = jest.fn().mockResolvedValue(10);
-    const sendTransactionMock = jest.fn();
+  let useEVMTxn;
+  let react;
+  let reactQuery;
 
-    const mockCTX = {
-      signer: {
-        estimateGas: estimateGasMock,
-        sendTransaction: sendTransactionMock,
-      },
+  beforeEach(async () => {
+    react = {
+      useState: jest.fn(() => []),
+      useEffect: jest.fn(),
+    };
+    reactQuery = {
+      useMutation: jest.fn(() => ({ mutationResult: 'mutationResult' })),
+      useQuery: jest.fn(() => 'useQuery'),
     };
 
-    const wrapper = getWrapper();
+    jest.doMock('react', () => react);
+    jest.doMock('react-query', () => reactQuery);
 
-    const hookResult = renderHook(() => useEVMTxn(mockCTX as any, null), {
-      wrapper,
-    });
-    try {
-      // We do not expect any state update so this should timeout
-      await hookResult.waitForNextUpdate({ timeout: 100 });
-    } catch (error) {
-      expect(error).toBeDefined();
-      expect(estimateGasMock).not.toBeCalled();
-    }
+    ({ default: useEVMTxn } = await import('./useEVMTxn'));
   });
-  test('Does not call estimateGas twice when the txn is the same', async () => {
-    const estimateGasMock = jest.fn().mockResolvedValue(10);
-    const sendTransactionMock = jest.fn();
 
-    const mockCTX = {
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  test.only('Does not call estimateGas when txn is null', async () => {
+    const estimateGas = jest.fn(() => Promise.resolve(10));
+    const sendTransaction = jest.fn();
+
+    const ctx = {
       signer: {
-        estimateGas: estimateGasMock,
-        sendTransaction: sendTransactionMock,
+        estimateGas,
+        sendTransaction,
       },
     };
+    const txn = null;
+    useEVMTxn(ctx, txn);
 
-    const wrapper = getWrapper();
-    const hookResult = renderHook(
-      () =>
-        useEVMTxn(mockCTX as any, {
-          data: 'byte',
-          value: BigNumber.from(10),
-          nonce: undefined,
-          from: '1',
-          to: '2',
-        }),
-      {
-        wrapper,
-      }
-    );
-    await hookResult.waitForNextUpdate();
-
-    expect(estimateGasMock).toBeCalledTimes(1);
-    expect(estimateGasMock).toBeCalledWith({
-      data: 'byte',
-      value: BigNumber.from(10),
-      nonce: undefined,
-      from: '1',
-      to: '2',
-    });
-    expect(hookResult.result.current.gasLimit?.toString()).toBe('10');
-
-    // This asserts a infinity rerender bug we had due to not calling toString on txn.value,
-    // without calling toString we will end up in a infinity loop and this will fail
-    hookResult.rerender();
-    expect(estimateGasMock).toBeCalledTimes(1);
+    expect(reactQuery.useMutation).toBeCalled();
+    expect(estimateGas).not.toBeCalled();
   });
 });
