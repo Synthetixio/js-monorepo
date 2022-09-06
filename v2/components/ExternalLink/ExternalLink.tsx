a0d9dd39d2a2c622b8e637a388848a012d34bf7a
joey <joey@cc.snxdao.io>
2022-09-06 17:04:57 +1000

    Spread props

--- v2/components/ExternalLink/ExternalLink.tsx
+++ v2/components/ExternalLink/ExternalLink.tsx
@@ -2,9 +2,9 @@ import React, { PropsWithChildren } from 'react';
 import { Link, LinkProps } from '@chakra-ui/react';
 import { ArrowTopRight } from '@snx-v2/icons';
 
-export const ExternalLink = ({ children }: PropsWithChildren<LinkProps>) => {
+export const ExternalLink = ({ children, ...props }: PropsWithChildren<LinkProps>) => {
   return (
-    <Link display="flex" alignItems="center" textColor="cyan">
+    <Link display="flex" alignItems="center" textColor="cyan" {...props}>
       {children}
       <ArrowTopRight ml="1" />
     </Link>
