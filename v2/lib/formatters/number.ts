7f5b3f7ec882c0d1ce86d7154afde56c0f0b6336
joey <joey@cc.snxdao.io>
2022-09-08 11:34:50 +1000

    Add locales to options

--- v2/lib/formatters/number.ts
+++ v2/lib/formatters/number.ts
@@ -4,16 +4,22 @@ export function numberWithCommas(value: string) {
   return parts.join('.');
 }
 
-export const formatNumberToUsd = (val: number, options?: Intl.NumberFormatOptions) => {
-  const optionsToUse = { style: 'currency', currency: 'USD', ...options };
-  return new Intl.NumberFormat('en-EN', optionsToUse).format(Number(val));
+interface Options extends Intl.NumberFormatOptions {
+  locales?: string | string[];
+}
+export const formatNumberToUsd = (val: number | string, options?: Options) => {
+  const optionsWithDefault = { style: 'currency', currency: 'USD', locales: 'en-EN', ...options };
+  return new Intl.NumberFormat(optionsWithDefault.locales, optionsWithDefault).format(Number(val));
 };
 
-export const formatNumber = (value: number | string, options?: Intl.NumberFormatOptions) => {
+export const formatNumber = (value: number | string, options?: Options) => {
   const optionsWithDefault = {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
+    locales: 'en-EN',
     ...options,
   };
-  return new Intl.NumberFormat('en-EN', optionsWithDefault).format(Number(value));
+  return new Intl.NumberFormat(optionsWithDefault.locales, optionsWithDefault).format(
+    Number(value)
+  );
 };
