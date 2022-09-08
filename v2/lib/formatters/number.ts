45433408c543538a5eac677c92ff72f3f353bac5
joey <joey@cc.snxdao.io>
2022-09-08 10:50:29 +1000

    Add more number formatters - Relying on the native Intl.NumberFormat

--- v2/lib/formatters/number.ts
+++ v2/lib/formatters/number.ts
@@ -3,3 +3,17 @@ export function numberWithCommas(value: string) {
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return parts.join('.');
 }
+
+export const formatNumberToUsd = (val: number, options?: Intl.NumberFormatOptions) => {
+  const optionsToUse = { style: 'currency', currency: 'USD', ...options };
+  return new Intl.NumberFormat('en-EN', optionsToUse).format(Number(val));
+};
+
+export const formatNumber = (value: number | string, options?: Intl.NumberFormatOptions) => {
+  const optionsWithDefault = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    ...options,
+  };
+  return new Intl.NumberFormat('en-EN', optionsWithDefault).format(Number(value));
+};
--- v2/lib/formatters/numbers.test.ts
+++
@@ -1,8 +0,0 @@
-import { numberWithCommas } from './number';
-
-describe('number', () => {
-  test('numberWithCommas', () => {
-    const result = numberWithCommas('123123');
-    expect(result).toBe('123,123');
-  });
-});
