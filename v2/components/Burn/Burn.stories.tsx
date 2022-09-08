cfeda09cb81547b0e8c0f4df2cf20f9e69efba33
jmzwar <james.zwar@protonmail.com>
2022-09-08 10:46:21 +1000

    init

---
+++ v2/components/Burn/Burn.stories.tsx
@@ -0,0 +1,18 @@
+import { Burn } from '.';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { wei } from '@synthetixio/wei';
+
+export default {
+  title: 'Burn',
+  component: Burn,
+} as ComponentMeta<typeof Burn>;
+
+const Template: ComponentStory<typeof Burn> = (_args) => <Burn {..._args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  snxBalance: wei(2000),
+  susdBalance: wei(2000),
+  exchangeRate: 0.25,
+  gasPrice: wei(0.01),
+};
---
+++ v2/components/Burn/index.tsx
@@ -0,0 +1,1 @@
+export { Burn } from './Burn';
