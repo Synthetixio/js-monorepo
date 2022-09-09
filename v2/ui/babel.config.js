100f6e103df68ca85043ee9431a1d2b513d86446
Nikita <nikita@cc.snxdao.io>
2022-09-09 13:50:23 +0800

    Replace swc with babel

--- v2/ui/babel.config.js
+++ v2/ui/babel.config.js
@@ -1,7 +1,65 @@
 module.exports = {
   presets: [
-    [require.resolve('@babel/preset-react'), { runtime: 'automatic' }],
-    [require.resolve('@babel/preset-env'), { targets: { browsers: ['last 1 Chrome version'] } }],
     require.resolve('@babel/preset-typescript'),
+    [require.resolve('@babel/preset-react'), { runtime: 'automatic' }],
   ],
+
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.'],
+        alias: {
+          i18n: './i18n.ts',
+          assets: './assets',
+          components: './components',
+          constants: './constants',
+          containers: './containers',
+          content: './content',
+          contracts: './contracts',
+          hoc: './hoc',
+          hooks: './hooks',
+          lib: './lib',
+          mutations: './mutations',
+          queries: './queries',
+          scripts: './scripts',
+          sections: './sections',
+          store: './store',
+          styles: './styles',
+          translations: './translations',
+          typings: './typings',
+          utils: './utils',
+        },
+      },
+    ],
+  ],
+
+  env: {
+    production: {
+      presets: [
+        [
+          require.resolve('@babel/preset-env'),
+          {
+            modules: false,
+            targets: {
+              browsers: require('./package.json').browserslist,
+            },
+          },
+        ],
+      ],
+    },
+
+    development: {
+      presets: [
+        [
+          require.resolve('@babel/preset-env'),
+          {
+            modules: false,
+            targets: { browsers: ['last 1 Chrome version'] },
+          },
+        ],
+      ],
+      plugins: [require.resolve('react-refresh/babel')],
+    },
+  },
 };
--- v2/ui/webpack.config.js
+++ v2/ui/webpack.config.js
@@ -1,7 +1,12 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 const generate = require('./scripts/minify-synthetix-contract');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 function optimiseContracts() {
   const networks = ['goerli', 'goerli-ovm', 'mainnet', 'mainnet-ovm'];
@@ -48,70 +53,9 @@ const tsxRule = {
     fullySpecified: false,
   },
   use: {
-    loader: require.resolve('swc-loader'),
+    loader: require.resolve('babel-loader'),
     options: {
-      test: '.tsx?$',
-      sourceMaps: true,
-      env: {
-        mode: 'entry',
-        coreJs: '3.22',
-      },
-      jsc: {
-        baseUrl: path.resolve(__dirname),
-        paths: {
-          i18n: [path.resolve(__dirname, 'i18n.ts')],
-          'assets/*': [path.resolve(__dirname, 'assets', '*')],
-          'components/*': [path.resolve(__dirname, 'components', '*')],
-          'constants/*': [path.resolve(__dirname, 'constants', '*')],
-          containers: [path.resolve(__dirname, 'containers')],
-          'containers/*': [path.resolve(__dirname, 'containers', '*')],
-          'content/*': [path.resolve(__dirname, 'content', '*')],
-          contracts: [path.resolve(__dirname, 'contracts')],
-          'contracts/*': [path.resolve(__dirname, 'contracts', '*')],
-          'hoc/*': [path.resolve(__dirname, 'hoc', '*')],
-          'hooks/*': [path.resolve(__dirname, 'hooks', '*')],
-          'lib/*': [path.resolve(__dirname, 'lib', '*')],
-          'mutations/*': [path.resolve(__dirname, 'mutations', '*')],
-          'queries/*': [path.resolve(__dirname, 'queries', '*')],
-          'scripts/*': [path.resolve(__dirname, 'scripts', '*')],
-          'sections/*': [path.resolve(__dirname, 'sections', '*')],
-          'store/*': [path.resolve(__dirname, 'store', '*')],
-          'styles/*': [path.resolve(__dirname, 'styles', '*')],
-          'translations/*': [path.resolve(__dirname, 'translations', '*')],
-          'typings/*': [path.resolve(__dirname, 'typings', '*')],
-          'utils/*': [path.resolve(__dirname, 'utils', '*')],
-        },
-        parser: {
-          syntax: 'typescript',
-          tsx: true,
-          decorators: false,
-          dynamicImport: true,
-        },
-        transform: {
-          react: {
-            runtime: 'automatic',
-          },
-          optimizer: {
-            globals: {
-              vars: {
-                'process.env.NEXT_PUBLIC_PORTIS_APP_ID': JSON.stringify(
-                  process.env.NEXT_PUBLIC_PORTIS_APP_ID
-                ),
-                'process.env.NEXT_PUBLIC_BN_NOTIFY_API_KEY': JSON.stringify(
-                  process.env.NEXT_PUBLIC_BN_NOTIFY_API_KEY
-                ),
-                'process.env.NEXT_PUBLIC_BN_ONBOARD_API_KEY': JSON.stringify(
-                  process.env.NEXT_PUBLIC_BN_ONBOARD_API_KEY
-                ),
-                'process.env.NEXT_PUBLIC_INFURA_PROJECT_ID': JSON.stringify(
-                  process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
-                ),
-                'process.env.NODE_ENV': JSON.stringify('development'),
-              },
-            },
-          },
-        },
-      },
+      configFile: path.resolve(__dirname, 'babel.config.js'),
     },
   },
 };
@@ -157,16 +101,18 @@ const devServer = {
     progress: false,
   },
 
+  static: './public',
+
+  headers: { 'Access-Control-Allow-Origin': '*' },
   allowedHosts: 'all',
   open: false,
   compress: true,
 };
 
 module.exports = {
-  //  devtool: 'eval',
-  devtool: false,
+  devtool: 'eval',
   devServer,
-  mode: 'development',
+  mode: isProd ? 'production' : 'development',
   entry: './index.ts',
   output: {
     path: path.resolve(__dirname, 'out'),
@@ -175,17 +121,44 @@ module.exports = {
   plugins: [htmlPlugin]
     .concat(contractPlugins)
     .concat([
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-        //      process: 'process/browser.js',
-      }),
-    ])
-    .concat([
       new webpack.NormalModuleReplacementPlugin(
         /^@tanstack\/react-query$/,
         require.resolve('@tanstack/react-query')
       ),
-    ]),
+    ])
+    .concat([
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development'),
+        'process.env.NEXT_PUBLIC_PORTIS_APP_ID': JSON.stringify(
+          process.env.NEXT_PUBLIC_PORTIS_APP_ID
+        ),
+        'process.env.NEXT_PUBLIC_BN_NOTIFY_API_KEY': JSON.stringify(
+          process.env.NEXT_PUBLIC_BN_NOTIFY_API_KEY
+        ),
+        'process.env.NEXT_PUBLIC_BN_ONBOARD_API_KEY': JSON.stringify(
+          process.env.NEXT_PUBLIC_BN_ONBOARD_API_KEY
+        ),
+        'process.env.NEXT_PUBLIC_INFURA_PROJECT_ID': JSON.stringify(
+          process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
+        ),
+      }),
+    ])
+    .concat(isProd ? [] : [new ReactRefreshWebpackPlugin({ overlay: false })])
+    .concat(
+      process.env.GENERATE_BUNDLE_REPORT === 'true'
+        ? [
+            new BundleAnalyzerPlugin({
+              analyzerMode: 'static',
+              reportFilename: path.resolve(__dirname, 'tmp', 'webpack.html'),
+              openAnalyzer: false,
+              generateStatsFile: false,
+            }),
+          ]
+        : []
+    ),
 
   resolve: {
     fallback: {
