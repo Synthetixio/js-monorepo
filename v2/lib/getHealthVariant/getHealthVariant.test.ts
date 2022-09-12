ddf72b4dc54fe19c33903859ccf44c730459d2bb
joey <joey@cc.snxdao.io>
2022-09-12 10:43:01 +1000

    Not staking should be considered success

--- v2/lib/getHealthVariant/getHealthVariant.test.ts
+++ v2/lib/getHealthVariant/getHealthVariant.test.ts
@@ -1,6 +1,15 @@
 import { getHealthVariant } from './getHealthVariant';
 
 describe('getVariant', () => {
+  test('not staking', () => {
+    const arg = {
+      liquidationCratioPercentage: 130,
+      targetCratioPercentage: 400,
+      currentCRatioPercentage: 0,
+    };
+    const result = getHealthVariant(arg);
+    expect(result).toBe('success');
+  });
   test('success', () => {
     const arg = {
       liquidationCratioPercentage: 130,
