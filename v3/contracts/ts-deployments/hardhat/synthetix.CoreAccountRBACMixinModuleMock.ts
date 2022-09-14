e4667086f524588de947862fb74c188b0254752b
Peiman <peymannaderi3@yahoo.com>
2022-09-14 14:14:25 +0430

    fix: remove hardhat from v3 contracts

--- v3/contracts/ts-deployments/hardhat/synthetix.CoreAccountRBACMixinModuleMock.ts
+++
@@ -1,8 +0,0 @@
-export const address = '0xB91ac80B781a7a0592f76D503b72240e41685c24';
-export const abi = [
-  'error PermissionDenied(uint256 accountId, bytes32 permission, address target)',
-  'function mockAccountRBACMixinDeposit(uint256 accountId, uint256 newDepositMock)',
-  'function mockAccountRBACMixinGetDepositMock() view returns (uint256)',
-  'function mockAccountRBACMixinGetMintMock() view returns (uint256)',
-  'function mockAccountRBACMixinMint(uint256 accountId, uint256 newMintMock)',
-];
