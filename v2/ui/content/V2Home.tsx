d136eb2e117d7a1886771250f08430a5f77b1528
joey <joey@cc.snxdao.io>
2022-09-16 09:46:58 +1000

    Remove unnecessary props

--- v2/ui/content/V2Home.tsx
+++ v2/ui/content/V2Home.tsx
@@ -1,31 +1,20 @@
 import { Box, Container, Flex } from '@chakra-ui/react';
-import { useRecoilValue } from 'recoil';
 import { CRatioBanner } from '@snx-v2/CRatioBanner';
 import { CRatioHealthCard } from '@snx-v2/CRatioHealthCard';
 import { BalanceBox } from '@snx-v2/BalanceBox';
 import { MainActionCards } from '@snx-v2/MainActionCards';
-import { delegateWalletState } from '../store/wallet';
-import Connector from '../containers/Connector';
 
 const V2Home = () => {
-  const delegateWallet = useRecoilValue(delegateWalletState);
-  const { walletAddress, network, provider } = Connector.useContainer();
-  const walletAddressToUse = delegateWallet?.address ?? walletAddress;
-  const propsFromConnector = {
-    walletAddress: walletAddressToUse,
-    provider,
-    networkId: network?.id,
-  };
   return (
     <Container maxW="1200px">
       <Box height="42px" position="absolute" left="0" right="0">
-        <CRatioBanner {...propsFromConnector} />
+        <CRatioBanner />
       </Box>
       <Box height="42px" />
       <Flex mt="4" flexDirection={['column', 'column', 'column', 'row']}>
         <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-          <CRatioHealthCard {...propsFromConnector} />
-          <MainActionCards {...propsFromConnector} />
+          <CRatioHealthCard />
+          <MainActionCards />
         </Box>
         <Flex
           ml="6"
@@ -33,7 +22,7 @@ const V2Home = () => {
           maxWidth="287px"
           width="full"
         >
-          <BalanceBox {...propsFromConnector} />
+          <BalanceBox />
         </Flex>
       </Flex>
     </Container>
