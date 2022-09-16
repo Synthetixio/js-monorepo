83e2433dedc2cb6cb0b95356e1ce9fffc6b0b49a
joey <joey@cc.snxdao.io>
2022-09-16 17:06:36 +1000

    Add useExchangeRatesData - A hook for fetching all synth prices + SNX price

---
+++ v2/lib/useExchangeRatesData/useExchangeRatesData.ts
@@ -0,0 +1,45 @@
+import { useContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { ContractContext } from '@snx-v2/ContractContext';
+import { useSynthUtil, useExchangeRates } from '@snx-v2/useSynthetixContracts';
+import { formatBytes32String, parseBytes32String } from '@ethersproject/strings';
+import Wei, { wei } from '@synthetixio/wei';
+import { BigNumber } from 'ethers';
+
+const processQueryData = (result: [[string[], BigNumber[]], BigNumber[]]) => {
+  const [[synthNames, synthRates], [snxRate]] = result;
+  const ratesByName = synthNames.reduce(
+    (acc: Record<string, Wei | undefined>, synthName, index) => {
+      acc[parseBytes32String(synthName)] = wei(synthRates[index]);
+      return acc;
+    },
+    { SNX: wei(snxRate) }
+  );
+  return ratesByName;
+};
+export const useExchangeRatesData = () => {
+  const { networkId } = useContext(ContractContext);
+  const { data: SynthUtil } = useSynthUtil();
+  const { data: ExchangeRates } = useExchangeRates();
+
+  return useQuery(
+    ['exchangeRatesData', networkId],
+    async () => {
+      if (!SynthUtil || !ExchangeRates) {
+        throw Error('Query should not be enabled if contracts are missing');
+      }
+      const SNX = formatBytes32String('SNX');
+      const nonSynthRates = [SNX];
+
+      return await Promise.all([
+        SynthUtil.synthsRates(),
+        ExchangeRates.ratesForCurrencies(nonSynthRates),
+      ]);
+    },
+    {
+      select: processQueryData,
+      enabled: Boolean(networkId && SynthUtil && ExchangeRates),
+      staleTime: 1000,
+    }
+  );
+};
