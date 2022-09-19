6a9abb497eaa462174004a8f0b06e658ca2a035c
Nikita <nikita@cc.snxdao.io>
2022-09-19 14:30:27 +0800

    Inject custom Provider and Signer before app starts up

--- v2/cypress/cypress/support/e2e.js
+++ v2/cypress/cypress/support/e2e.js
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers';
+
 beforeEach(() => {
   cy.intercept('https://analytics.synthetix.io/matomo.js', { statusCode: 204 }).as('matomo');
 
@@ -13,7 +15,49 @@ beforeEach(() => {
     });
   }).as('graphql');
 
-  cy.window().then((w) => {
-    w.localStorage.UNSAFE_IMPORT = 'true';
+  cy.on('window:before:load', (win) => {
+    win.localStorage.setItem('UNSAFE_IMPORT', 'true');
+    win.localStorage.setItem('selectedWallet', '["MetaMask"]');
+
+    class Signer {
+      constructor(provider) {
+        this._isSigner = true;
+        this.getAddress = async () => Cypress.env('WALLET_ADDRESS');
+        this.provider = provider;
+        this.wallet = ethers.Wallet.fromMnemonic(Cypress.env('WALLET_MNEMONIC'));
+        this.signMessage = (message) => this.wallet.signMessage(message);
+      }
+    }
+
+    win.ethereum = new Proxy(
+      new ethers.providers.JsonRpcProvider(Cypress.env('TENDERLY_RPC_URL')),
+      {
+        get(target, prop, _receiver) {
+          switch (prop) {
+            case 'isMetaMask':
+              return true;
+
+            case 'getSigner':
+              return () => new Signer(win.ethereum);
+
+            case 'request':
+              return async ({ method, params }) => {
+                switch (method) {
+                  case 'eth_accounts':
+                  case 'eth_requestAccounts':
+                    return [Cypress.env('WALLET_ADDRESS')];
+                  case 'eth_chainId':
+                    return '0x1';
+                  default:
+                    return await target.send(method, params);
+                }
+              };
+
+            default:
+              return target[prop];
+          }
+        },
+      }
+    );
   });
 });
