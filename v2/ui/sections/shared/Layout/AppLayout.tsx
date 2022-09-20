fe41df7190f816fd8063448b09b7fe63056cbfa9
jmzwar <james.zwar@protonmail.com>
2022-09-21 08:50:08 +1000

    remove useEffect

--- v2/ui/sections/shared/Layout/AppLayout.tsx
+++ v2/ui/sections/shared/Layout/AppLayout.tsx
@@ -1,19 +1,15 @@
-import { FC, ReactNode, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { FC, ReactNode } from 'react';
 import styled from 'styled-components';
 
 import { DESKTOP_SIDE_NAV_WIDTH, DESKTOP_BODY_PADDING } from '@snx-v1/constantsUi';
-import ROUTES from 'constants/routes';
+
 import NotificationContainer from 'constants/NotificationContainer';
 
 import media from '@snx-v1/media';
-import { delegateWalletState } from 'store/wallet';
+
 import Header from './Header';
 import SideNav from './SideNav';
 import { Header as V2Header } from '../../../v2-components/Header';
-import useSynthetixQueries from '@synthetixio/queries';
-import Connector from 'containers/Connector';
 import useLocalStorage from 'hooks/useLocalStorage';
 import { LOCAL_STORAGE_KEYS } from 'constants/storage';
 import { Box } from '@chakra-ui/react';
@@ -23,21 +19,6 @@ type AppLayoutProps = {
 };
 
 const AppLayout: FC<AppLayoutProps> = ({ children }) => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { isL2 } = Connector.useContainer();
-
-  const { useIsBridgeActiveQuery } = useSynthetixQueries();
-
-  const depositsInactive = !(useIsBridgeActiveQuery().data ?? true); // Deposits are active by default to prevent redirects when status unknown
-  const delegateWallet = useRecoilValue(delegateWalletState);
-
-  useEffect(() => {
-    if (delegateWallet && location.pathname !== ROUTES.Home) {
-      navigate(ROUTES.Home);
-    }
-  }, [isL2, depositsInactive, delegateWallet, location.pathname, navigate]);
-
   const [STAKING_V2_ENABLED] = useLocalStorage(LOCAL_STORAGE_KEYS.STAKING_V2_ENABLED, false);
 
   return (
