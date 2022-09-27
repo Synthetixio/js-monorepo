5222adb8efda36de4a4afa592cf0a7a11e1f79c2
joey <joey@cc.snxdao.io>
2022-09-27 15:39:19 +1000

    Add skeleton for CRatio Health card

--- v2/components/CRatioHealthCard/CRatioHealthCard.cy.tsx
+++ v2/components/CRatioHealthCard/CRatioHealthCard.cy.tsx
@@ -2,6 +2,22 @@ import { Box } from '@chakra-ui/react';
 import { CRatioHealthCardUi } from './CRatioHealthCard';
 
 describe('CRatioHealthCard.cy.tsx', () => {
+  it('renders skeleton', () => {
+    cy.viewport(500, 300);
+    cy.mount(
+      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+        <CRatioHealthCardUi
+          {...{
+            targetCratioPercentage: undefined,
+            liquidationCratioPercentage: undefined,
+            currentCRatioPercentage: undefined,
+          }}
+        />
+      </Box>
+    );
+    cy.contains('h2', 'Collateralization Ratio Health').should('be.visible');
+    cy.get('.chakra-skeleton').should('be.visible');
+  });
   it('renders green', () => {
     cy.viewport(500, 300);
     cy.mount(
