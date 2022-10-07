1df43ac05ab762fe6315f1855ad2aad074c3db81
Nikita <nikita@cc.snxdao.io>
2022-10-07 11:40:17 +0800

    Don't show balance until we fetch it

--- v2/cypress/cypress/e2e/loans.e2e.js
+++ v2/cypress/cypress/e2e/loans.e2e.js
@@ -1,11 +1,18 @@
 describe('loans', () => {
   it('Can borrow SNX with ETH collateral', () => {
+    // TX confirmation can take a while
+    const TIMEOUT_TX = 120000;
+    // Data fetching from tenderly can take some extra time
+    const TIMEOUT_DATA_FETCH = 30000;
+
     cy.visit('http://localhost:3000/loans');
     cy.get('[id="Borrow Synths-tab"]').should('be.visible');
 
-    cy.get('[data-testid="loans current balance"]')
-      .invoke('attr', 'data-balance')
-      .should('be.gte', 2);
+    cy.get('[data-testid="loans current eth balance"]', {
+      timeout: TIMEOUT_DATA_FETCH,
+    }).should(($0) => {
+      expect(parseFloat($0.data('balance'))).to.be.gte(2);
+    });
 
     // input for the debt asset
     cy.get('[data-testid="loans form"] input[type="number"]').eq(0).type(100);
@@ -24,11 +31,12 @@ describe('loans', () => {
         expect($dialog).to.contain('2.00 ETH');
       });
 
-    // TX confirmation can take a while
-    cy.get('[role="dialog"]', { timeout: 120000 }).should('not.exist');
+    cy.get('[role="dialog"]', { timeout: TIMEOUT_TX }).should('not.exist');
 
     cy.get('[id="Active Borrows-tab"][aria-selected="true"]').should('exist');
     cy.get('[id="Active Borrows-tabpanel"]').should('exist');
-    cy.get('[id="Active Borrows-tabpanel"] [data-testid="loan actions button"]').should('exist');
+    cy.get('[id="Active Borrows-tabpanel"] [data-testid="loan actions button"]', {
+      timeout: TIMEOUT_DATA_FETCH,
+    }).should('exist');
   });
 });
