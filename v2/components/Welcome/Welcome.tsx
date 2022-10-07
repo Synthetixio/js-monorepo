5f70617728a0609c508c60de6c6954e046ddceea
jmzwar <52280438+jmzwar@users.noreply.github.com>
2022-10-07 14:37:21 +1100

    welcome to synthetix (#384)
    * welcome to synthetix
    
    * pr feedback
    
    * wip
    
    * update storybook

---
+++ v2/components/Welcome/Welcome.tsx
@@ -0,0 +1,34 @@
+import { Text, Link, Flex, Box, Button, BoxProps } from '@chakra-ui/react';
+import { EXTERNAL_LINKS } from '@snx-v2/Constants';
+import { Trans, useTranslation } from 'react-i18next';
+
+export const Welcome = ({ ...props }: BoxProps) => {
+  const { t } = useTranslation();
+  return (
+    <Box {...props}>
+      <Text color="whiteAlpha.900" variant="heading" fontWeight="700" fontSize="4xl">
+        {t('staking-v2.home.title')}
+      </Text>
+      <Flex alignItems="center">
+        <Text color="gray.600" variant="heading" fontSize="small">
+          <Trans
+            i18nKey="staking-v2.home.description"
+            components={[
+              <Link color="cyan.400" target="_blank" href={EXTERNAL_LINKS.Synthetix.Litepaper} />,
+            ]}
+          />
+        </Text>
+        <Button
+          bg="transparent"
+          bgImage="none"
+          color="cyan.400"
+          borderWidth={1}
+          borderColor="cyan.400"
+          onClick={() => console.log('To introduction')}
+        >
+          {t('staking-v2.home.action')}
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
--- v2/lib/Constants/index.ts
+++ v2/lib/Constants/index.ts
@@ -1,2 +1,3 @@
 export * from './Constants';
 export * from './dSnx';
+export * from './links';
