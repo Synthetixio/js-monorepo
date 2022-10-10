505a5913a98b41ede73adb43ed95f8d276d7fce0
joey <joey@cc.snxdao.io>
2022-10-10 17:07:12 -0500

    Move txnreducer to package

---
+++ v2/lib/txnReducer/txnReducer.ts
@@ -0,0 +1,62 @@
+export type TransactionStatus = 'unsent' | 'prompting' | 'pending' | 'success' | 'error';
+
+type MintMutationState = {
+  error: Error | null;
+  modalOpen: boolean;
+  txnStatus: TransactionStatus;
+};
+
+export const initialState: MintMutationState = {
+  error: null,
+  modalOpen: false,
+  txnStatus: 'unsent',
+};
+
+export type Actions =
+  | { type: 'prompting' }
+  | { type: 'pending' }
+  | { type: 'success' }
+  | { type: 'error'; payload: { error: Error } }
+  | { type: 'settled' };
+
+export function reducer(state: MintMutationState, action: Actions): MintMutationState {
+  switch (action.type) {
+    case 'prompting':
+      return {
+        ...state,
+        txnStatus: 'prompting',
+        modalOpen: true,
+        error: null,
+      };
+
+    case 'pending':
+      return {
+        ...state,
+        txnStatus: 'pending',
+      };
+
+    case 'success':
+      return {
+        ...state,
+        txnStatus: 'success',
+      };
+
+    case 'error':
+      return {
+        ...state,
+        txnStatus: 'error',
+        error: action.payload.error,
+      };
+
+    case 'settled':
+      return {
+        ...state,
+        modalOpen: false,
+        txnStatus: 'unsent',
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+}
