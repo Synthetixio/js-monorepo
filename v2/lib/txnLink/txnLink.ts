284d864633fd8c6e90829ff46f59b2b0ce62aaf1
joey <joey@cc.snxdao.io>
2022-10-17 12:25:28 -0500

    Add txnLink module

---
+++ v2/lib/txnLink/txnLink.ts
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { ContractContext } from '@snx-v2/ContractContext';
+import { NetworkIdByName, NetworkNameById } from '@snx-v2/useSynthetixContracts';
+
+export const getTxnLink = (networkId: number | null, txnId?: string) => {
+  if (!txnId) return null;
+  if (networkId !== NetworkIdByName.mainnet) {
+    return `https://${NetworkNameById[networkId]}.etherscan.io/tx/${txnId}`;
+  }
+  return `https://etherscan.io/tx/${txnId}`;
+};
+
+export const useGetTxnLink = (txnHash?: string) => {
+  const { networkId } = useContext(ContractContext);
+  return getTxnLink(networkId, txnHash);
+};
