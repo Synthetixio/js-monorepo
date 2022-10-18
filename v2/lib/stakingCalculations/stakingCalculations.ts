f0bfa7f3c5cafa13219128fe62c220ceb5c9ca8b
joey <joey@cc.snxdao.io>
2022-10-18 14:20:41 -0500

    Add change calculations

--- v2/lib/stakingCalculations/stakingCalculations.ts
+++ v2/lib/stakingCalculations/stakingCalculations.ts
@@ -57,3 +57,50 @@ export const calculateUnstakingAmountFromBurn = calculateCollateralFromDebt;
 export const calculateBurnAmountFromUnstaking = calculateDebtFromCollateral;
 export const calculateMintAmountFromStaking = calculateDebtFromCollateral;
 export const calculateStakeAmountFromMint = calculateCollateralFromDebt;
+
+export const calculateChangesFromMint = ({
+  stakeAmountSNX,
+  mintAmountsUSD,
+  debtBalance,
+  stakedSnx,
+  transferable,
+  sUSDBalance,
+}: {
+  stakeAmountSNX: number;
+  mintAmountsUSD: number;
+  debtBalance: number;
+  stakedSnx: number;
+  transferable: number;
+  sUSDBalance: number;
+}) => {
+  const newDebtBalance = debtBalance + mintAmountsUSD;
+  const newStakedAmountSnx = stakedSnx + stakeAmountSNX;
+  const newCratio = newDebtBalance / newStakedAmountSnx;
+  const newTransferable = transferable - stakeAmountSNX;
+  const newSUSDBalance = sUSDBalance + mintAmountsUSD;
+
+  return { newDebtBalance, newStakedAmountSnx, newCratio, newTransferable, newSUSDBalance };
+};
+
+export const calculateChangesFromBurn = ({
+  snxUnstakingAmount,
+  burnAmountSusd,
+  debtBalance,
+  stakedSnx,
+  transferable,
+  sUSDBalance,
+}: {
+  snxUnstakingAmount: number;
+  burnAmountSusd: number;
+  debtBalance: number;
+  stakedSnx: number;
+  transferable: number;
+  sUSDBalance: number;
+}) => {
+  const newDebtBalance = Math.max(debtBalance - burnAmountSusd, 0);
+  const newStakedAmountSnx = Math.max(stakedSnx - snxUnstakingAmount);
+  const newCratio = newDebtBalance / newStakedAmountSnx || 0;
+  const newTransferable = transferable + snxUnstakingAmount;
+  const newSUSDBalance = sUSDBalance - burnAmountSusd;
+  return { newDebtBalance, newStakedAmountSnx, newCratio, newTransferable, newSUSDBalance };
+};
