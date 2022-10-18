ffd00145121e57b646a632323c9e9b4578a0b39d
Nikita <nikita@cc.snxdao.io>
2022-10-18 13:17:21 +0800

    Fix types

--- v2/lib/txnLink/txnLink.ts
+++ v2/lib/txnLink/txnLink.ts
@@ -1,8 +1,9 @@
 import { useContext } from 'react';
 import { ContractContext } from '@snx-v2/ContractContext';
 import { NetworkIdByName, NetworkNameById } from '@snx-v2/useSynthetixContracts';
+import type { NetworkId } from '@snx-v2/useSynthetixContracts';
 
-export const getTxnLink = (networkId: number | null, txnId: string | null) => {
+export const getTxnLink = (networkId: NetworkId, txnId: string | null) => {
   if (!txnId) return null;
   if (networkId !== NetworkIdByName.mainnet) {
     return `https://${NetworkNameById[networkId]}.etherscan.io/tx/${txnId}`;
@@ -12,5 +13,5 @@ export const getTxnLink = (networkId: number | null, txnId: string | null) => {
 
 export const useGetTxnLink = (txnHash: string | null) => {
   const { networkId } = useContext(ContractContext);
-  return getTxnLink(networkId, txnHash);
+  return getTxnLink(networkId as NetworkId, txnHash);
 };
