a32d1e2ef33d91f512e255d80b51c973455a1d8f
Joey <5688912+bachstatter@users.noreply.github.com>
2022-10-20 08:14:57 -0500

    Change calculations (#415)
    * Add change calculations
    
    * To calculate c-ratio correctly we need collateral value in usd
    
    * Create MintOrBurnChanges component
    
    * Use MintOrBurnChanges in Mint
    
    * Remove SNX balance below the input field
    
    * Some design polish for MintOrBurnChanges
    
    * Correct info icon size on mint and burn
    
    * Correct border radius
    
    * Deps

--- v2/lib/stakingCalculations/stakingCalculations.test.ts
+++ v2/lib/stakingCalculations/stakingCalculations.test.ts
@@ -1,12 +1,15 @@
 import { wei } from '@synthetixio/wei';
 import {
   calculateBurnAmountFromUnstaking,
+  calculateChangesFromBurn,
+  calculateChangesFromMint,
   calculateMintAmountFromStaking,
   calculateStakeAmountFromMint,
   calculateStakedSnx,
   calculateUnstakedStakedSnx,
   calculateUnstakingAmountFromBurn,
 } from './stakingCalculations';
+
 describe('stakingCalculation', () => {
   describe('calculateStakedSnx', () => {
     test('returns zero when no data', () => {
@@ -108,6 +111,91 @@ describe('stakingCalculation', () => {
   describe('calculateMintAmountFromStaking', () => {
     test('when target 400% (0.25%) and snx price 2', () => {
       expect(calculateMintAmountFromStaking('20', 0.25, 2)).toBe('10.00');
+    });
+  });
+  describe('calculateChangesFromMint', () => {
+    test('not staked, staking 10SNX', () => {
+      const stakeAmountSNX = 10;
+      const mintAmountsUSD = 5;
+      const stakedSnx = 0;
+      const debtBalance = 0;
+      const transferable = 50;
+      const sUSDBalance = 0;
+      const collateralUsdValue = 10;
+
+      expect(
+        calculateChangesFromMint({
+          stakeAmountSNX,
+          mintAmountsUSD,
+          stakedSnx,
+          debtBalance,
+          transferable,
+          sUSDBalance,
+          collateralUsdValue,
+        })
+      ).toEqual({
+        newCratio: 0.5,
+        newDebtBalance: 5,
+        newStakedAmountSnx: 10,
+        newTransferable: 40,
+        newSUSDBalance: 5,
+      });
+    });
+    test('already staked, staking 10SNX more', () => {
+      const stakeAmountSNX = 10;
+      const mintAmountsUSD = 5;
+      const stakedSnx = 5;
+      const debtBalance = 2;
+      const transferable = 50;
+      const sUSDBalance = 10;
+      const collateralUsdValue = 60;
+
+      expect(
+        calculateChangesFromMint({
+          stakeAmountSNX,
+          mintAmountsUSD,
+          stakedSnx,
+          debtBalance,
+          transferable,
+          sUSDBalance,
+          collateralUsdValue,
+        })
+      ).toEqual({
+        newCratio: 7 / collateralUsdValue,
+        newDebtBalance: 7,
+        newStakedAmountSnx: 15,
+        newTransferable: 40,
+        newSUSDBalance: 15,
+      });
+    });
+  });
+  describe('calculateChangesFromBurn', () => {
+    test('burning 10SNX', () => {
+      const snxUnstakingAmount = 10;
+      const burnAmountSusd = 5;
+      const stakedSnx = 20;
+      const debtBalance = 10;
+      const transferable = 50;
+      const sUSDBalance = 10;
+      const collateralUsdValue = 100;
+
+      expect(
+        calculateChangesFromBurn({
+          snxUnstakingAmount,
+          burnAmountSusd,
+          stakedSnx,
+          debtBalance,
+          transferable,
+          sUSDBalance,
+          collateralUsdValue,
+        })
+      ).toEqual({
+        newCratio: 5 / collateralUsdValue,
+        newDebtBalance: 5,
+        newStakedAmountSnx: 10,
+        newTransferable: 60,
+        newSUSDBalance: 5,
+      });
     });
   });
 });
