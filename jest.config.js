7fd0b285903bb76934082f0bd90cdae59fd51bde
Nikita <nikita@cc.snxdao.io>
2022-10-21 12:49:04 +0800

    Simplify tests in CI

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,14 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/v2/ui/tests/setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['<rootDir>/**/*.test.{js,jsx,ts,tsx}'],
-  collectCoverageFrom: ['<rootDir>/**/*.{js,jsx,ts,tsx}', '!<rootDir>/**/*.d.{ts,tsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/packages/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/tools/codegen-graph-ts/**/*.d.{js,jsx,ts,tsx}',
+    '<rootDir>/tools/generate-subgraph-query/**/*.d.{js,jsx,ts,tsx}',
+    '<rootDir>/v1/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/v2/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/v3/theme/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/**/*.d.ts',
+    '!<rootDir>/**/*.test.{js,jsx,ts,tsx}',
+  ],
 };
