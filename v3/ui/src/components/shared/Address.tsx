2ebc88ce8ee5b2295840845a4c1b746b452ad140
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2022-10-21 13:14:24 -0400

    v3 ui - max mint and etherscan links (#427)
    * max mint and etherscan links
    
    * fix spacing
    
    * lint fix

--- v3/ui/src/components/shared/Address.tsx
+++ v3/ui/src/components/shared/Address.tsx
@@ -1,7 +1,8 @@
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Link, Text } from '@chakra-ui/react';
 import { FC } from 'react';
-import Etherscan from '../../assets/svgs/etherscan.svg';
-import Tenderly from '../../assets/svgs/tenderly.svg';
+import { useRecoilState } from 'recoil';
+import { chainIdState } from '../../utils/state';
 
 interface Props {
   address?: string;
@@ -12,18 +13,38 @@ export const formatShortAddress = (address: string) =>
   `${address.slice(0, 6)}....${address.slice(-4)}`;
 
 export const Address: FC<Props> = ({ address, displayFullAddress }) => {
+  const [localChainId] = useRecoilState(chainIdState);
   if (!address) return null;
+
+  let url;
+  switch (localChainId) {
+    case 5:
+      url = `https://goerli.etherscan.io/address/${address}`;
+      break;
+    case 5:
+      url = `https://optimistic.etherscan.io/${address}`;
+      break;
+    case 420:
+      url = `https://goerli-optimism.etherscan.io/address/${address}`;
+      break;
+    default:
+      url = `https://etherscan.io/address/${address}`;
+  }
 
   // ENS support here?
   const addressDisplay = displayFullAddress ? address : formatShortAddress(address);
   return (
     <Text>
       {addressDisplay}
-      <Link display="inline-block" opacity="0.66" ml="2">
-        <Tenderly width="10px" height="10px" />
-      </Link>
-      <Link display="inline-block" opacity="0.66" ml="2">
-        <Etherscan height="10px" width="10px" />
+      <Link
+        href={url}
+        isExternal
+        color="cyan.500"
+        ml="1.5"
+        display="inline-block"
+        transform="translateY(-2px)"
+      >
+        <ExternalLinkIcon />
       </Link>
     </Text>
   );
