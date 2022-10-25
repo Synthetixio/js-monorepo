70a592998b988ed3daa1ff4351e6289c3e632af7
Noah Litvin <noah.litvin@gmail.com>
2022-10-25 15:03:34 -0400

    v3 ui fix

--- v3/ui/src/layouts/Default/DefaultLayout.tsx
+++ v3/ui/src/layouts/Default/DefaultLayout.tsx
@@ -20,7 +20,7 @@ export const DefaultLayout: React.FC = () => {
         <NetworkChain>
           <Initializer>
             <Header />
-            <Container display="flex" flexDir="column" width="container.lg" flex="1">
+            <Container display="flex" flexDir="column" maxW="2xl" flex="1">
               <Outlet />
             </Container>
           </Initializer>
