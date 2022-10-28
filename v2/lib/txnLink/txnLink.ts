c6f47f6ad5649feb9e12dbea0632e07a091c11cb
joey <joey@cc.snxdao.io>
2022-10-28 12:56:40 +1100

    Add getEtherscanBaseUrl to txnLink module

--- v2/lib/txnLink/txnLink.ts
+++ v2/lib/txnLink/txnLink.ts
@@ -3,12 +3,16 @@ import { ContractContext } from '@snx-v2/ContractContext';
 import { NetworkIdByName, NetworkNameById } from '@snx-v2/useSynthetixContracts';
 import type { NetworkId } from '@snx-v2/useSynthetixContracts';
 
-export const getTxnLink = (networkId: NetworkId, txnId: string | null) => {
-  if (!txnId) return null;
+export const getEtherscanBaseUrl = (networkId: number) => {
   if (networkId !== NetworkIdByName.mainnet) {
-    return `https://${NetworkNameById[networkId]}.etherscan.io/tx/${txnId}`;
+    return `https://${NetworkNameById[networkId]}.etherscan.io`;
   }
-  return `https://etherscan.io/tx/${txnId}`;
+  return `https://etherscan.io`;
+};
+export const getTxnLink = (networkId: number, txnId: string | null) => {
+  if (!txnId) return null;
+  const baseUrl = getEtherscanBaseUrl(networkId);
+  return `${baseUrl}/tx/${txnId}`;
 };
 
 export const useGetTxnLink = (txnHash: string | null) => {
