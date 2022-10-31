094aed93c6d20748ffbc1e7778b8f043eecb0a81
Joey <5688912+bachstatter@users.noreply.github.com>
2022-10-31 16:37:47 +1100

    Synth desc (#448)
    * Add synthByName package
    
    * Use synthByName to get description
    
    * Update story book
    
    * Fix types
    
    * Add wallet type (#449)

--- v2/lib/ContractContext/ContractContext.tsx
+++ v2/lib/ContractContext/ContractContext.tsx
@@ -3,7 +3,11 @@ import { createContext } from 'react';
 export const ContractContext = createContext<{
   networkId: number | null;
   walletAddress: string | null;
+  ensName: string | null;
+  walletType: string | null;
 }>({
   networkId: null,
   walletAddress: null,
+  ensName: null,
+  walletType: null,
 });
