825cc0badffa79d4610e715e2bbe99394e2618e0
joey <joey@cc.snxdao.io>
2022-10-31 15:14:53 +1100

    Add synthByName package

---
+++ v2/lib/synthsByName/synthsByName.ts
@@ -0,0 +1,31 @@
+import { useContext } from 'react';
+import { NetworkIdByName } from '@snx-v2/useSynthetixContracts';
+import { ContractContext } from '@snx-v2/ContractContext';
+import { useQuery } from '@tanstack/react-query';
+
+export const loadSynthsByNameFromNetwork = (networkId: number) => {
+  switch (networkId) {
+    case NetworkIdByName['mainnet']:
+      return import('@synthetixio/contracts/build/mainnet/synths');
+    case NetworkIdByName['mainnet-ovm']:
+      return import('@synthetixio/contracts/build/mainnet-ovm/synths');
+    case NetworkIdByName['goerli']:
+      return import('@synthetixio/contracts/build/goerli/synths');
+    case NetworkIdByName['goerli-ovm']:
+      return import('@synthetixio/contracts/build/goerli-ovm/synths');
+    default:
+      return import('@synthetixio/contracts/build/mainnet/synths');
+  }
+};
+
+export const useGetSynthsByName = () => {
+  const { networkId } = useContext(ContractContext);
+  return useQuery(
+    ['useGetSynthsByName', networkId],
+    async () => {
+      if (!networkId) throw Error('Require network id');
+      return await loadSynthsByNameFromNetwork(networkId);
+    },
+    { enabled: Boolean(networkId), staleTime: Infinity, cacheTime: Infinity }
+  );
+};
