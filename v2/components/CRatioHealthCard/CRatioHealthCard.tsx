5a7e7085bbdfa66540e565d64a1fd62bb7f339ef
James Zwar <jameszwar@Jamess-MacBook-Pro.local>
2022-11-02 10:23:52 +1100

    Cratio loading + other minor fixes

--- v2/components/CRatioHealthCard/CRatioHealthCard.tsx
+++ v2/components/CRatioHealthCard/CRatioHealthCard.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Heading, Skeleton, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import { getHealthVariant } from '@snx-v2/getHealthVariant';
 import { useDebtData } from '@snx-v2/useDebtData';
-import React from 'react';
+import { CRatioHealthPercentage } from '@snx-v2/CRatioHealthPercentage';
 import { useTranslation } from 'react-i18next';
 import { CRatioProgressBar } from './CRatioProgressBar';
 
@@ -9,68 +10,59 @@ type UiProps = {
   liquidationCratioPercentage?: number;
   targetCratioPercentage?: number;
   currentCRatioPercentage?: number;
+  isLoading: boolean;
 };
+
 export const CRatioHealthCardUi: React.FC<UiProps> = ({
   targetCratioPercentage,
   liquidationCratioPercentage,
   currentCRatioPercentage,
+  isLoading,
 }) => {
   const { t } = useTranslation();
+
+  const variant = getHealthVariant({
+    targetCratioPercentage,
+    liquidationCratioPercentage,
+    currentCRatioPercentage,
+  });
 
   return (
     <Box>
       <Flex justifyContent="space-between" alignItems="center" marginBottom="2">
         <Box>
-          <Heading size="md">{t('staking-v2.cratio-health-card.heading')}</Heading>
-          <Text> {t('staking-v2.cratio-health-card.sub-heading')}</Text>
+          <Heading size="md" mb={1} fontWeight="bold" color="whiteAlpha.900" lineHeight="6">
+            {t('staking-v2.cratio-health-card.heading')}
+          </Heading>
+          <Text fontSize="sm" color="whiteAlpha.600">
+            {t('staking-v2.cratio-health-card.sub-heading')}
+          </Text>
         </Box>
-        <Flex
-          bg="blackAlpha.800"
-          alignItems="center"
-          borderRadius="md"
-          border="1px"
-          borderColor="gray.900"
-          padding="2"
-        >
-          <Skeleton minWidth="50px" isLoaded={currentCRatioPercentage !== undefined}>
-            <Text
-              color={getHealthVariant({
-                targetCratioPercentage,
-                liquidationCratioPercentage,
-                currentCRatioPercentage,
-              })}
-              fontSize="2xl"
-              fontWeight="black"
-              align="center"
-              fontFamily="mono"
-            >
-              {currentCRatioPercentage && Math.floor(currentCRatioPercentage)}%
-            </Text>
-          </Skeleton>
-        </Flex>
-      </Flex>
-
-      {targetCratioPercentage && liquidationCratioPercentage && currentCRatioPercentage ? (
-        <CRatioProgressBar
-          targetCratioPercentage={targetCratioPercentage}
-          liquidationCratioPercentage={liquidationCratioPercentage}
+        <CRatioHealthPercentage
+          variant={variant}
           currentCRatioPercentage={currentCRatioPercentage}
+          isLoading={isLoading}
         />
-      ) : (
-        <Skeleton w="100%" minHeight="100px" mb={4} />
-      )}
+      </Flex>
+      <CRatioProgressBar
+        targetCratioPercentage={targetCratioPercentage || 0}
+        liquidationCratioPercentage={liquidationCratioPercentage || 0}
+        currentCRatioPercentage={currentCRatioPercentage || 0}
+        isLoading={isLoading}
+      />
     </Box>
   );
 };
 
 export const CRatioHealthCard: React.FC = () => {
-  const { data: debtData } = useDebtData();
+  const { data: debtData, isLoading } = useDebtData();
 
   return (
     <CRatioHealthCardUi
       currentCRatioPercentage={debtData?.currentCRatioPercentage.toNumber()}
       targetCratioPercentage={debtData?.targetCRatioPercentage.toNumber()}
       liquidationCratioPercentage={debtData?.liquidationRatioPercentage.toNumber()}
+      isLoading={isLoading}
     />
   );
 };
