d0c9761b91a38caac221f033debfd5cc5c298686
joey <joey@cc.snxdao.io>
2022-11-07 22:12:34 +1100

    Add home button to mint and burn

--- v2/ui/content/V2Burn.tsx
+++ v2/ui/content/V2Burn.tsx
@@ -1,15 +1,16 @@
 import { Container, Box } from '@chakra-ui/react';
 import { Burn } from '@snx-v2/Burn';
 import { delegateWalletState } from 'store/wallet';
-
 import { useRecoilValue } from 'recoil';
+import { HomeButton } from '@snx-v2/HomeButton';
 
 const V2Burn = () => {
   const delegateWallet = useRecoilValue(delegateWalletState);
   return (
     <>
       <Box bg="navy.900" height="100%">
-        <Container pt={12} pb={16} bg="navy.900" maxW="4xl">
+        <Container pt={4} pb={16} bg="navy.900" maxW="4xl">
+          <HomeButton />
           <Burn delegateWalletAddress={delegateWallet?.address} />
         </Container>
       </Box>
