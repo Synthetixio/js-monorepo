119571e6e485808a7b8f6a6719790c1d9199a488
joey <joey@cc.snxdao.io>
2022-11-11 14:15:38 +1100

    Add stat items

--- v2/components/WalletBalances/WalletBalances.tsx
+++ v2/components/WalletBalances/WalletBalances.tsx
@@ -1,6 +1,56 @@
-const WalletBalancesUi = () => {
-  return <p> Hello world</p>;
+import { Box, Flex, Stat, StatLabel, StatNumber, StatProps } from '@chakra-ui/react';
+import { formatNumber } from '@snx-v2/formatters';
+import { useDebtData } from '@snx-v2/useDebtData';
+import { useGetDSnxBalance } from '@snx-v2/useDSnxBalance';
+import { useSynthsBalances } from '@snx-v2/useSynthsBalances';
+
+const StatItem: React.FC<{
+  label: string;
+  value?: number;
+  statProps: StatProps;
+}> = ({ value, label, statProps }) => {
+  return (
+    <Stat {...statProps}>
+      <StatLabel color="gray.500">{label}</StatLabel>
+      <StatNumber color="gray.50" fontWeight="bold">
+        {value ? formatNumber(value) : '-'}
+      </StatNumber>
+    </Stat>
+  );
+};
+const WalletBalancesUi: React.FC<{
+  totalSynthBalance?: number;
+  dSNXBalance?: number;
+  debtBalance?: number;
+}> = ({ totalSynthBalance, dSNXBalance, debtBalance }) => {
+  return (
+    <Box>
+      <Flex>
+        <StatItem label="Active Debt" value={debtBalance} statProps={{ textAlign: 'left' }} />
+        <StatItem
+          label="dSNX Balance"
+          value={dSNXBalance}
+          statProps={{ marginX: 2, textAlign: 'center' }}
+        />
+        <StatItem
+          label="Total Synth Balance"
+          value={totalSynthBalance}
+          statProps={{ textAlign: 'right' }}
+        />
+      </Flex>
+    </Box>
+  );
 };
 export const WalletBalances = () => {
-  return <WalletBalancesUi />;
+  const { data: debtData } = useDebtData();
+  const { data: synthsData } = useSynthsBalances();
+  const { data: dSNXBalance } = useGetDSnxBalance();
+
+  return (
+    <WalletBalancesUi
+      debtBalance={debtData?.debtBalance.toNumber()}
+      dSNXBalance={dSNXBalance?.toNumber()}
+      totalSynthBalance={synthsData?.totalUSDBalance.toNumber()}
+    />
+  );
 };
