75b25d31725bbc650652f856059df4e3b78612ed
joey <joey@cc.snxdao.io>
2022-11-12 17:27:40 +1100

    Lets user pass container styles to StatBox - Also border radius should be base (even though design have more round corner right now)

--- v2/components/StatBox/StatBox.tsx
+++ v2/components/StatBox/StatBox.tsx
@@ -1,24 +1,27 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, FlexProps, Text } from '@chakra-ui/react';
 
-interface RewardsStatProps {
+export const StatBox = ({
+  label,
+  amount,
+  containerStyles,
+}: {
   label: string;
   amount: string;
-  align?: 'start' | 'center' | 'end';
-}
-
-export const StatBox = ({ label, amount, align = 'center' }: RewardsStatProps) => {
+  containerStyles?: FlexProps;
+}) => {
   return (
     <Flex
-      alignItems={align}
+      alignItems="center"
       flexDirection="column"
       w="33%"
       maxW="325px"
       py="14px"
       px={6}
-      borderRadius="xl"
+      borderRadius="base"
       borderWidth="1px"
       borderColor="gray.900"
       bg="whiteAlpha.50"
+      {...containerStyles}
     >
       <Text fontFamily="heading" fontWeight="semibold" fontSize="sm" color="gray.500">
         {label}
