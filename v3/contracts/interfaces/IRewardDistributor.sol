09bfafefcd4f7453175329167a7a1615103071cb
Peiman <peymannaderi3@yahoo.com>
2022-11-14 19:26:56 +0330

    feat: update IRewardDistributor

--- v3/contracts/interfaces/IRewardDistributor.sol
+++ v3/contracts/interfaces/IRewardDistributor.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 /// @title Interface a reward distributor.
 interface IRewardDistributor {
-    /// @notice returns a human-readable name for the reward distributor
+    /// @notice Returns a human-readable name for the reward distributor
     function name() external returns (string memory);
 
-    /// called by system
+    /// @notice This function should revert if msg.sender is not the Synthetix CoreProxy address.
     function payout(
         uint128 accountId,
         uint128 poolId,
@@ -14,4 +14,8 @@ interface IRewardDistributor {
         address sender,
         uint256 amount
     ) external returns (bool);
+
+    /// @notice Address to ERC-20 token distributed by this distributor, for display purposes only
+    /// @dev Return address(0) if providing non ERC-20 rewards
+    function token() external returns (address);
 }
