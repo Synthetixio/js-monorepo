b1847cd8a72a9fec17be5f94f18db12109bebd19
joey <joey@cc.snxdao.io>
2022-11-22 10:49:05 +1100

    Fix tests - Using dependency injection for the progress bar in CratioHealthCard

--- v2/components/CRatioHealthCard/CRatioHealthCard.cy.tsx
+++ v2/components/CRatioHealthCard/CRatioHealthCard.cy.tsx
@@ -1,94 +1,66 @@
 import { Box } from '@chakra-ui/react';
 import { CRatioHealthCardUi } from './CRatioHealthCard';
 
-describe('CRatioHealthCard.cy.tsx', () => {
-  it('renders green', () => {
-    cy.viewport(500, 300);
-    cy.mount(
-      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-        <CRatioHealthCardUi
-          isLoading={false}
-          {...{
-            targetCratioPercentage: 400,
-            liquidationCratioPercentage: 150,
-            currentCRatioPercentage: 440,
-          }}
-        />
-      </Box>
-    );
-    cy.contains('h2', 'Collateralization Ratio Health').should('be.visible');
-    cy.contains('p', '440%').should('be.visible');
+it('renders green', () => {
+  cy.viewport(500, 300);
+  cy.mount(
+    <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+      <CRatioHealthCardUi
+        CRatioProgressBar={<div role="progressbar">Im a progress bar</div>}
+        isLoading={false}
+        {...{
+          targetCratioPercentage: 400,
+          liquidationCratioPercentage: 150,
+          currentCRatioPercentage: 440,
+        }}
+      />
+    </Box>
+  );
+  cy.contains('h2', 'Collateralization Ratio Health').should('be.visible');
+  cy.contains('p', '440%').should('be.visible');
 
-    cy.contains('p', 'Liquidation < 150%').find('span').trigger('mouseover');
-    cy.root().should('contain', 'You may be flagged for liquidation');
+  cy.get('[role="progressbar"]').should('be.visible');
+});
 
-    cy.contains('p', 'Target 400%').find('span').trigger('mouseover');
-    cy.root().should('contain', 'Required to claim rewards');
+it('renders orange', () => {
+  cy.viewport(500, 300);
+  cy.mount(
+    <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+      <CRatioHealthCardUi
+        CRatioProgressBar={<div role="progressbar">Im a progress bar</div>}
+        isLoading={false}
+        {...{
+          targetCratioPercentage: 600,
+          liquidationCratioPercentage: 100,
+          currentCRatioPercentage: 200,
+        }}
+      />
+    </Box>
+  );
+  cy.contains('h2', 'Collateralization Ratio Health').should('be.visible');
+  cy.contains('p', '200%').should('be.visible');
 
-    cy.get('[role="progressbar"]').should('be.visible');
-  });
+  cy.get('[role="progressbar"]').should('be.visible');
+});
 
-  it('renders orange', () => {
-    cy.viewport(500, 300);
-    cy.mount(
-      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-        <CRatioHealthCardUi
-          isLoading={false}
-          {...{
-            targetCratioPercentage: 600,
-            liquidationCratioPercentage: 100,
-            currentCRatioPercentage: 200,
-          }}
-        />
-      </Box>
-    );
-    cy.contains('h2', 'Collateralization Ratio Health').should('be.visible');
-    cy.contains('p', '200%').should('be.visible');
-    cy.contains('p', 'Liquidation < 100%').should('be.visible');
-    cy.contains('p', 'Target 600%').should('be.visible');
+it('renders red', () => {
+  cy.viewport(500, 300);
+  cy.mount(
+    <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+      <CRatioHealthCardUi
+        CRatioProgressBar={<div role="progressbar">Im a progress bar</div>}
+        isLoading={false}
+        {...{
+          targetCratioPercentage: 200,
+          liquidationCratioPercentage: 100,
+          currentCRatioPercentage: 50,
+        }}
+      />
+    </Box>
+  );
 
-    cy.get('[role="progressbar"]').should('be.visible');
-  });
+  cy.contains('h2', 'Collateralization Ratio Health').should('be.visible');
+  cy.contains('p', '50%').should('be.visible');
 
-  it('renders red', () => {
-    cy.viewport(500, 300);
-    cy.mount(
-      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-        <CRatioHealthCardUi
-          isLoading={false}
-          {...{
-            targetCratioPercentage: 200,
-            liquidationCratioPercentage: 100,
-            currentCRatioPercentage: 50,
-          }}
-        />
-      </Box>
-    );
-
-    cy.contains('h2', 'Collateralization Ratio Health').should('be.visible');
-    cy.contains('p', '50%').should('be.visible');
-    cy.contains('p', 'Liquidation < 100%').should('be.visible');
-    cy.contains('p', 'Target 200%').should('be.visible');
-
-    cy.get('[role="progressbar"]').should('be.visible');
-
-    cy.get('[data-testid="current c-ration triangle"]').should('be.visible');
-  });
-  it('avoids overlapping labels when really large c-ratio', () => {
-    cy.viewport(500, 300);
-    cy.mount(
-      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-        <CRatioHealthCardUi
-          isLoading={false}
-          {...{
-            targetCratioPercentage: 350,
-            liquidationCratioPercentage: 100,
-            currentCRatioPercentage: 1500,
-          }}
-        />
-      </Box>
-    );
-
-    cy.get('[data-testid="current c-ration triangle"]').should('not.be.visible');
-  });
+  cy.get('[role="progressbar"]').should('be.visible');
 });
--- v2/components/CRatioProgressBar/CRatioProgressBar.cy.tsx
+++ v2/components/CRatioProgressBar/CRatioProgressBar.cy.tsx
@@ -8,164 +8,186 @@ const darkOrange = 'rgb(150, 66, 10)';
 const red = 'rgb(255, 74, 96)';
 const darkRed = 'rgb(155, 44, 44)';
 
-describe('CRatioProgressBarUi', () => {
-  describe('no new c-ratio', () => {
-    it('renders green', () => {
-      cy.viewport(500, 300);
-      cy.mount(
-        <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-          <CRatioProgressBarUi
-            {...{
-              targetThreshold: 0.05,
-              isLoading: false,
-              targetCratioPercentage: 400,
-              liquidationCratioPercentage: 150,
-              currentCRatioPercentage: 440,
-            }}
-          />
-        </Box>
-      );
+describe('no new c-ratio', () => {
+  it('renders green and tooltops', () => {
+    cy.viewport(500, 300);
+    cy.mount(
+      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+        <CRatioProgressBarUi
+          {...{
+            targetThreshold: 0.05,
+            isLoading: false,
+            targetCratioPercentage: 400,
+            liquidationCratioPercentage: 150,
+            currentCRatioPercentage: 440,
+          }}
+        />
+      </Box>
+    );
 
-      cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        green
-      );
-      cy.get('[data-testid="non highlighted progress bar"]').should('not.exist');
-    });
+    cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      green
+    );
+    cy.get('[data-testid="non highlighted progress bar"]').should('not.exist');
+    cy.contains('p', 'Liquidation < 150%').find('span').trigger('mouseover');
+    cy.root().should('contain', 'You may be flagged for liquidation');
 
-    it('renders orange', () => {
-      cy.viewport(500, 300);
-      cy.mount(
-        <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-          <CRatioProgressBarUi
-            {...{
-              targetThreshold: 0.05,
-              isLoading: false,
-              targetCratioPercentage: 600,
-              liquidationCratioPercentage: 100,
-              currentCRatioPercentage: 200,
-            }}
-          />
-        </Box>
-      );
-
-      cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        orange
-      );
-      cy.get('[data-testid="non highlighted progress bar"]').should('not.exist');
-    });
-    it('renders red', () => {
-      cy.viewport(500, 300);
-      cy.mount(
-        <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-          <CRatioProgressBarUi
-            {...{
-              targetThreshold: 0.05,
-              isLoading: false,
-              targetCratioPercentage: 200,
-              liquidationCratioPercentage: 100,
-              currentCRatioPercentage: 50,
-            }}
-          />
-        </Box>
-      );
-
-      cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        red
-      );
-      cy.get('[data-testid="non highlighted progress bar"]').should('not.exist');
-    });
+    cy.contains('p', 'Target 400%').find('span').trigger('mouseover');
+    cy.root().should('contain', 'Required to claim rewards');
   });
 
-  describe('with new c-ratio', () => {
-    it('renders green when new cratio is above target', () => {
-      cy.viewport(500, 300);
-      cy.mount(
-        <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-          <CRatioProgressBarUi
-            {...{
-              targetCratioPercentage: 350,
-              liquidationCratioPercentage: 100,
-              currentCRatioPercentage: 310,
-              newCratioPercentage: 400,
-              targetThreshold: 0.05,
-              isLoading: false,
-            }}
-          />
-        </Box>
-      );
+  it('renders orange', () => {
+    cy.viewport(500, 300);
+    cy.mount(
+      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+        <CRatioProgressBarUi
+          {...{
+            targetThreshold: 0.05,
+            isLoading: false,
+            targetCratioPercentage: 600,
+            liquidationCratioPercentage: 100,
+            currentCRatioPercentage: 200,
+          }}
+        />
+      </Box>
+    );
 
-      cy.get('[data-testid="highlighted progress bar"] [role="progressbar"] ').should(
-        'have.css',
-        'background-color',
-        green
-      );
-      cy.get('[data-testid="non highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        darkGreen
-      );
-    });
-    it('renders orange when new cratio is below target', () => {
-      cy.viewport(500, 300);
-      cy.mount(
-        <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-          <CRatioProgressBarUi
-            {...{
-              targetCratioPercentage: 350,
-              liquidationCratioPercentage: 100,
-              currentCRatioPercentage: 400,
-              newCratioPercentage: 200,
-              targetThreshold: 0.05,
-              isLoading: false,
-            }}
-          />
-        </Box>
-      );
-
-      cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        orange
-      );
-      cy.get('[data-testid="non highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        darkOrange
-      );
-    });
-    it('renders red when new cratio is below liq ratio', () => {
-      cy.viewport(500, 300);
-      cy.mount(
-        <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
-          <CRatioProgressBarUi
-            {...{
-              targetCratioPercentage: 350,
-              liquidationCratioPercentage: 100,
-              currentCRatioPercentage: 400,
-              newCratioPercentage: 50,
-              targetThreshold: 0.05,
-              isLoading: false,
-            }}
-          />
-        </Box>
-      );
-
-      cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        red
-      );
-      cy.get('[data-testid="non highlighted progress bar"] [role="progressbar"]').should(
-        'have.css',
-        'background-color',
-        darkRed
-      );
-    });
+    cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      orange
+    );
+    cy.get('[data-testid="non highlighted progress bar"]').should('not.exist');
   });
+  it('renders red', () => {
+    cy.viewport(500, 300);
+    cy.mount(
+      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+        <CRatioProgressBarUi
+          {...{
+            targetThreshold: 0.05,
+            isLoading: false,
+            targetCratioPercentage: 200,
+            liquidationCratioPercentage: 100,
+            currentCRatioPercentage: 50,
+          }}
+        />
+      </Box>
+    );
+
+    cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      red
+    );
+    cy.get('[data-testid="non highlighted progress bar"]').should('not.exist');
+  });
+});
+
+describe('with new c-ratio', () => {
+  it('renders green when new cratio is above target', () => {
+    cy.viewport(500, 300);
+    cy.mount(
+      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+        <CRatioProgressBarUi
+          {...{
+            targetCratioPercentage: 350,
+            liquidationCratioPercentage: 100,
+            currentCRatioPercentage: 310,
+            newCratioPercentage: 400,
+            targetThreshold: 0.05,
+            isLoading: false,
+          }}
+        />
+      </Box>
+    );
+
+    cy.get('[data-testid="highlighted progress bar"] [role="progressbar"] ').should(
+      'have.css',
+      'background-color',
+      green
+    );
+    cy.get('[data-testid="non highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      darkGreen
+    );
+  });
+  it('renders orange when new cratio is below target', () => {
+    cy.viewport(500, 300);
+    cy.mount(
+      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+        <CRatioProgressBarUi
+          {...{
+            targetCratioPercentage: 350,
+            liquidationCratioPercentage: 100,
+            currentCRatioPercentage: 400,
+            newCratioPercentage: 200,
+            targetThreshold: 0.05,
+            isLoading: false,
+          }}
+        />
+      </Box>
+    );
+
+    cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      orange
+    );
+    cy.get('[data-testid="non highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      darkOrange
+    );
+  });
+  it('renders red when new cratio is below liq ratio', () => {
+    cy.viewport(500, 300);
+    cy.mount(
+      <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+        <CRatioProgressBarUi
+          {...{
+            targetCratioPercentage: 350,
+            liquidationCratioPercentage: 100,
+            currentCRatioPercentage: 400,
+            newCratioPercentage: 50,
+            targetThreshold: 0.05,
+            isLoading: false,
+          }}
+        />
+      </Box>
+    );
+
+    cy.get('[data-testid="highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      red
+    );
+    cy.get('[data-testid="non highlighted progress bar"] [role="progressbar"]').should(
+      'have.css',
+      'background-color',
+      darkRed
+    );
+  });
+});
+
+it('avoids overlapping labels when really large c-ratio', () => {
+  cy.viewport(500, 300);
+  cy.mount(
+    <Box paddingY="7" paddingX="4" bg="navy.900" flex="1">
+      <CRatioProgressBarUi
+        {...{
+          targetCratioPercentage: 350,
+          liquidationCratioPercentage: 100,
+          currentCRatioPercentage: 1500,
+          targetThreshold: 0.05,
+          isLoading: false,
+        }}
+      />
+    </Box>
+  );
+
+  cy.get('[data-testid="current c-ration triangle"]').should('not.be.visible');
 });
