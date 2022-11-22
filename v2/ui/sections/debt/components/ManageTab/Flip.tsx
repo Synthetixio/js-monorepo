855c60b72858bd4cd43de1b2fda8d2c1e809c5ba
Nikita <nikita@cc.snxdao.io>
2022-11-22 16:05:01 +1100

    Replace 3rd row of tabs in dSNX swap interface with a Flip icon

---
+++ v2/ui/sections/debt/components/ManageTab/Flip.tsx
@@ -0,0 +1,28 @@
+import type { FC } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { RepeatIcon } from '@chakra-ui/icons';
+import styled from 'styled-components';
+
+export const Flip: FC<{ to: string }> = ({ to }) => {
+  return (
+    <Link to={to} style={{}}>
+      <RepeatIcon style={{ width: '100%', height: '100%' }} />
+    </Link>
+  );
+};
+
+const Link = styled(RouterLink)`
+  margin-top: -38px;
+  width: 32px;
+  height: 32px;
+  border-radius: 32px;
+  background: #06061b;
+  color: rgb(0, 209, 255);
+  padding: 6px;
+  box-sizing: content-box;
+  transition: transform 200ms;
+
+  &:hover {
+    transform: rotate(-180deg);
+  }
+`;
