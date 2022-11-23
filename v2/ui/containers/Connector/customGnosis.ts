3241c958fc9552ee31ccf792b8f21b1b51fbb38a
jmzwar <james@jmzwar.com>
2022-11-23 16:38:03 +1100

    gnosis and trust init

---
+++ v2/ui/containers/Connector/customGnosis.ts
@@ -0,0 +1,52 @@
+interface gnosisOptions {
+  whitelistedDomains?: RegExp[];
+}
+
+function gnosis(options?: gnosisOptions) {
+  const {
+    whitelistedDomains = [
+      /^https:\/\/gnosis-safe\.io$/,
+      /^https:\/\/app\.safe\.global$/,
+      /^https:\/\/safe\.global$/,
+    ],
+  } = options || {};
+  return () => ({
+    label: 'Safe',
+    getIcon: async () => (await import('@web3-onboard/gnosis/dist/icon')).default,
+    getInterface: async () => {
+      const { default: SafeAppsSDK } = await import('@gnosis.pm/safe-apps-sdk');
+      const { SafeAppProvider } = await import('@gnosis.pm/safe-apps-provider');
+      const { createEIP1193Provider } = await import('@web3-onboard/common');
+      const SafeAppProviderConstructor =
+        // @ts-ignore
+        SafeAppsSDK.default || SafeAppsSDK;
+      const opts = {
+        whitelistedDomains,
+      };
+      const appsSdk = new SafeAppProviderConstructor(opts);
+      const safe = await Promise.race([
+        appsSdk.safe.getInfo(),
+        new Promise((resolve) => setTimeout(resolve, 200)),
+      ]);
+      if (!safe) {
+        throw new Error(
+          `App must be loaded in a Safe App context, head to <a href="https://gnosis-safe.io/app">the Safe</a> and open this website as an app.`
+        );
+      }
+      const provider = new SafeAppProvider(
+        safe,
+        // @ts-ignore
+        appsSdk
+      );
+      const patchedProvider = createEIP1193Provider(provider, {
+        eth_requestAccounts: () => Promise.resolve([safe.safeAddress]),
+      });
+      return {
+        provider: patchedProvider,
+        instance: appsSdk,
+      };
+    },
+  });
+}
+
+export default gnosis;
