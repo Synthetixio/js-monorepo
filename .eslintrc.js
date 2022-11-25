30a0fea50ec550dc59640d0513a3855932a61e73
max <39312833+fritzschoff@users.noreply.github.com>
2022-11-25 15:17:48 +0100

    Subgraphs v3 (#475)
    * added template
    
    * template deps
    
    * worked on change requests
    
    * removed unused dep
    
    * run prettier
    
    * updating eslint
    
    * setup for subgraph unit test
    
    * added usd events from market manager module
    
    * setup and first unit test
    
    * added more unit tests
    
    * push unit tests
    
    * push for debug
    
    * fixed issue with tulpe value
    
    * added net issuance
    
    * updating tests
    
    * added two missing test caes
    
    * updated unit tests
    
    * removed comment
    
    * updated handlers
    
    * fixing tests
    
    * updated tests
    
    * removed log import
    
    * Change name to @snx-v3/subgraph as the package not intended to be published and should not use `@synthetixio` namespace
    
    * Commit subgraph generated files
    
    * Adjust eslint to support AssemblyScript
    
    * Ignore Subgraph tests
    
    * Add subgraphs tests to CI
    
    * reexporting handle function from the test file so test coverage tool can work properly
    
    Co-authored-by: Nikita <nikita@cc.snxdao.io>

--- .eslintrc.js
+++ .eslintrc.js
@@ -151,5 +151,22 @@ module.exports = {
       },
       rules: {},
     },
+
+    {
+      files: ['v3/subgraphs/**/*'],
+      env: {},
+      globals: {
+        changetype: true,
+        i32: true,
+        i64: true,
+        assert: true,
+      },
+      rules: {
+        'prefer-const': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-array-constructor': 'off',
+      },
+    },
   ],
 };
