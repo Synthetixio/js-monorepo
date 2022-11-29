32ecb64afbdc13de03fddebb471dad5f0f7b64e7
Nikita <nikita@cc.snxdao.io>
2022-11-29 11:33:18 +0800

    Remove contract ts-deployments from v3/ui

--- v3/ui/ts-deployments/goerli/CCIP.ts
+++
@@ -1,4 +0,0 @@
-export const address = '0xa189971a2c5aca0dfc5ee7a2c44a2ae27b3cf389';
-export const abi = [
-  'function ccipSend(uint256 destChainId, tuple(bytes receiver, bytes data, address[] tokens, uint256[] amounts, uint256 gasLimit) message)',
-];
--- v3/ui/ts-deployments/optimism-goerli/synthetix.UtilsModule.ts
+++
@@ -1,8 +0,0 @@
-export const address = '0xf3Ae6E6e25F50dc30663799a0C27d35b65CA788D';
-export const abi = [
-  'error AlreadyInitialized()',
-  'error MismatchAssociatedSystemKind(bytes32 expected, bytes32 actual)',
-  'error Unauthorized(address addr)',
-  'function mintInitialSystemToken(address to, uint256 amount)',
-  'function registerCcip(address ccipSend, address ccipReceive, address ccipTokenPool)',
-];
