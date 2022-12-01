d70081a832046b9be325489ac63e492025c38daf
joey <joey@cc.snxdao.io>
2022-12-01 21:30:42 +1100

    Better query key and make sure enable key and if that throws matches. - Also remove unnecessary ?

--- v2/lib/useGetUpcomingRewards/useGetUpcomingRewards.ts
+++ v2/lib/useGetUpcomingRewards/useGetUpcomingRewards.ts
@@ -5,29 +5,29 @@ import { useQuery } from '@tanstack/react-query';
 
 export const useGetUpcomingRewards = () => {
   const { data: exchangeRateData } = useExchangeRatesData();
-  const { data: liquidationData, isLoading: isLiquidationLoading } = useGetLiquidationRewards();
-  const { data: rewardsData, isLoading: isRewardsLoading } = useRewardsAvailable();
+  const { data: liquidationData } = useGetLiquidationRewards();
+  const { data: rewardsData } = useRewardsAvailable();
 
   const SNXRate = exchangeRateData?.SNX?.toNumber();
 
-  const enabled = Boolean(SNXRate && !isLiquidationLoading && !isRewardsLoading);
+  const enabled = Boolean(SNXRate && liquidationData && rewardsData);
   return useQuery(
-    [SNXRate],
+    ['useGetUpcomingRewards', SNXRate],
     () => {
       if (!SNXRate || !liquidationData || !rewardsData) throw Error('Missing required data');
       let total = 0;
 
-      if (!rewardsData?.hasClaimed) {
+      if (!rewardsData.hasClaimed) {
         // Fees component
-        total += rewardsData?.sUSDRewards.toNumber();
+        total += rewardsData.sUSDRewards.toNumber();
         // Inflation Component
-        const inflationComponent = rewardsData?.snxRewards.mul(SNXRate).toNumber();
+        const inflationComponent = rewardsData.snxRewards.mul(SNXRate).toNumber();
         total += inflationComponent;
       }
 
-      if (liquidationData?.liquidatorRewards.gt(0)) {
+      if (liquidationData.liquidatorRewards.gt(0)) {
         // Liquidations Component
-        total += liquidationData?.liquidatorRewards.mul(SNXRate).toNumber();
+        total += liquidationData.liquidatorRewards.mul(SNXRate).toNumber();
       }
 
       return total;
