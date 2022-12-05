505ea7d827d1004a4ec23f9c6b728f81601676e9
joey <joey@cc.snxdao.io>
2022-12-05 13:56:43 +1100

    Fix type errors for useStakingPositions - StakingPositionType expect values to be bignumber not big.js - select doesn't work with wagmi, so use formatData function - Add type guards for the values

--- v3/ui/src/hooks/useStakingPositions.ts
+++ v3/ui/src/hooks/useStakingPositions.ts
@@ -6,7 +6,7 @@ import { poolsData } from '../utils/constants';
 import { useSynthetixProxyEvent } from './useContractEvent';
 import { CollateralType, StakingPositionType } from '../utils/types';
 import { formatValue } from '../utils/helpers';
-import Big from 'big.js';
+import { BigNumber } from 'ethers';
 
 interface StakingCall {
   poolId: string;
@@ -33,7 +33,7 @@ export const useStakingPositions = (accountId: string) => {
     });
   });
 
-  const getStakingPositions = useContractReads({
+  const stakingPositionsQueryResult = useContractReads({
     enabled: true,
     contracts: calls.map((call) => ({
       addressOrName: snxProxy?.address,
@@ -41,54 +41,58 @@ export const useStakingPositions = (accountId: string) => {
       functionName: call.functionName,
       args: [accountId, call.poolId, call.collateral.address],
     })),
-    select: (data) => {
-      const results = data.map((value, index) => ({
-        index,
-        value,
-        functionName: calls[index].functionName,
-      }));
-
-      const collaterals = results.filter((val) => val.functionName === 'getPositionCollateral');
-      const debts = results.filter((val) => val.functionName === 'getPositionDebt');
-      const positions: Record<string, StakingPositionType> = {};
-
-      collaterals.forEach((c, i) => {
-        if (!c.value || c.value.amount.eq(0)) {
-          return;
-        }
-        const { poolId, collateral } = calls[c.index];
-        const key = `${poolId}-${collateral.symbol}`;
-
-        const debt = Big(formatValue(debts[i].value || 0, 18) || 0);
-        const collateralValue = formatValue(c.value.value || 0, collateral.priceDecimals);
-
-        const cRatio = !debt.eq(0) ? Big(collateralValue).mul(100).div(debt) : Big(0);
-
-        positions[key] = {
-          id: key,
-          poolId,
-          poolName: poolsData[poolId.toString()]?.name,
-          collateralType: collateral,
-          accountId,
-          collateralAmount: c.value.amount,
-          cRatio,
-          debt,
-        };
-      });
-
-      return positions;
-    },
   });
+  const formatData = () => {
+    if (!stakingPositionsQueryResult.data) return undefined;
+    const results = stakingPositionsQueryResult.data.map((value, index) => ({
+      index,
+      value,
+      functionName: calls[index].functionName,
+    }));
+
+    const collaterals = results.filter((val) => val.functionName === 'getPositionCollateral');
+    const debts = results.filter((val) => val.functionName === 'getPositionDebt');
+    const positions: Record<string, StakingPositionType> = {};
+
+    collaterals.forEach((c, i) => {
+      if (!c.value || c.value.amount.eq(0)) {
+        return;
+      }
+      const debt = debts[i].value;
+      if (!BigNumber.isBigNumber(debt)) {
+        return;
+      }
+      const { poolId, collateral } = calls[c.index];
+      const key = `${poolId}-${collateral.symbol}`;
+
+      const collateralValue = formatValue(c.value.value || 0, collateral.priceDecimals);
+      const cRatio = !debt.eq(0)
+        ? BigNumber.from(collateralValue).mul(100).div(debt)
+        : BigNumber.from(0);
+
+      positions[key] = {
+        id: key,
+        poolId,
+        poolName: poolsData[poolId.toString()]?.name,
+        collateralType: collateral,
+        accountId,
+        collateralAmount: c.value.amount,
+        cRatio,
+        debt,
+      };
+    });
+    return positions;
+  };
 
   useSynthetixProxyEvent({
     eventName: 'DelegationUpdated',
     listener: (event) => {
       const [userAccountId] = event;
       if (accountId === userAccountId.toString()) {
-        getStakingPositions.refetch();
+        stakingPositionsQueryResult.refetch();
       }
     },
   });
 
-  return getStakingPositions;
+  return { ...stakingPositionsQueryResult, data: formatData() };
 };
