5aa3e0a2e6276d9e28d7eb4264ee62acb36ce84b
Nikita <nikita@cc.snxdao.io>
2022-12-12 14:15:41 +0800

    Codegen v3/contracts types

---
+++ v3/contracts/src/optimism-goerli/oracle_manager.core_router.CoreModule.ts
@@ -0,0 +1,28 @@
+// !!! DO NOT EDIT !!! Automatically generated file
+
+export const address = '0xe24fbfA8AACbF2dB1f52BB0d729627BD573dED67';
+export const abi = [
+  'error AlreadyInitialized()',
+  'error ImplementationIsSterile(address implementation)',
+  'error NoChange()',
+  'error NotAContract(address contr)',
+  'error NotNominated(address addr)',
+  'error Unauthorized(address addr)',
+  'error UpgradeSimulationFailed()',
+  'error ZeroAddress()',
+  'event OwnerChanged(address oldOwner, address newOwner)',
+  'event OwnerNominated(address newOwner)',
+  'event Upgraded(address indexed self, address implementation)',
+  'function acceptOwnership()',
+  'function getImplementation() view returns (address)',
+  'function initializeOwnerModule(address initialOwner)',
+  'function isOwnerModuleInitialized() view returns (bool)',
+  'function nominateNewOwner(address newNominatedOwner)',
+  'function nominatedOwner() view returns (address)',
+  'function owner() view returns (address)',
+  'function renounceNomination()',
+  'function simulateUpgradeTo(address newImplementation)',
+  'function upgradeTo(address newImplementation)',
+];
+export const name = 'CoreModule';
+export const source = 'contracts/CoreModule.sol';
---
+++ v3/contracts/src/optimism-goerli/oracle_manager.core_router.Proxy.ts
@@ -0,0 +1,28 @@
+// !!! DO NOT EDIT !!! Automatically generated file
+
+export const address = '0xA3D28fF234c11Da8f55f2b7D90aB009c93B31DD1';
+export const abi = [
+  'error AlreadyInitialized()',
+  'error ImplementationIsSterile(address implementation)',
+  'error NoChange()',
+  'error NotAContract(address contr)',
+  'error NotNominated(address addr)',
+  'error Unauthorized(address addr)',
+  'error UpgradeSimulationFailed()',
+  'error ZeroAddress()',
+  'event OwnerChanged(address oldOwner, address newOwner)',
+  'event OwnerNominated(address newOwner)',
+  'event Upgraded(address indexed self, address implementation)',
+  'function acceptOwnership()',
+  'function getImplementation() view returns (address)',
+  'function initializeOwnerModule(address initialOwner)',
+  'function isOwnerModuleInitialized() view returns (bool)',
+  'function nominateNewOwner(address newNominatedOwner)',
+  'function nominatedOwner() view returns (address)',
+  'function owner() view returns (address)',
+  'function renounceNomination()',
+  'function simulateUpgradeTo(address newImplementation)',
+  'function upgradeTo(address newImplementation)',
+];
+export const name = 'Proxy';
+export const source = 'contracts/Proxy.sol';
