583859143cee16bca8f1291189c0c123a76a6346
joey <joey@cc.snxdao.io>
2022-12-19 10:51:12 +1100

    Clean up schema, graphql query and simpler mock data

--- v3/ui/src/hooks/useGetPoolData.ts
+++ v3/ui/src/hooks/useGetPoolData.ts
@@ -5,7 +5,7 @@ import { useQuery } from '@tanstack/react-query';
 import { calculateMarketPnl } from '../utils/calculations';
 import { formatGraphBigDecimal, getSubgraphUrl } from '../utils/subgraph';
 import { z } from 'zod';
-import { BigNumber } from 'ethers';
+import { wei } from '@synthetixio/wei';
 
 const GraphBigIntSchema = z.string().transform((src) => formatGraphBigDecimal(src));
 const GraphBigDecimalSchema = z.string().transform((src) => formatGraphBigDecimal(src));
@@ -17,8 +17,7 @@ export const MarketSnapshotByWeekSchema = z
     usd_withdrawn: GraphBigDecimalSchema,
     net_issuance: GraphBigDecimalSchema, // withdrawn - deposited
     reported_debt: GraphBigDecimalSchema,
-    created_at: z.number(),
-    updated_at: z.number(),
+    updated_at: z.string(),
     updates_in_period: z.number(),
   })
   .transform((market) => ({
@@ -30,14 +29,12 @@ const MarketSchema = z
   .object({
     id: z.string(),
     address: z.string(),
-    //configurations: MarketConfiguration[] // recursive
     usd_deposited: GraphBigDecimalSchema,
     usd_withdrawn: GraphBigDecimalSchema,
     net_issuance: GraphBigDecimalSchema, // withdrawn - deposited
     reported_debt: GraphBigDecimalSchema,
-    created_at: z.number(),
-    updated_at: z.number(),
-    market_snapshot_by_week: z.array(MarketSnapshotByWeekSchema),
+    updated_at: z.string(),
+    market_snapshots_by_week: z.array(MarketSnapshotByWeekSchema),
   })
   .transform((market) => ({
     ...market,
@@ -46,16 +43,17 @@ const MarketSchema = z
 
 const MarketConfigurationSchema = z.object({
   id: z.string(), //PoolId-MarketId
-  // Pool: Pool dont think typescript likes recursive types and we dont need it here, so lets not query the pool
   market: MarketSchema,
   weight: GraphBigIntSchema,
   max_debt_share_value: GraphBigDecimalSchema,
 });
 export const PoolSchema = z.object({
-  name: z.string(),
+  id: z.string(),
+  name: z.union([z.string(), z.null()]),
   total_weight: GraphBigIntSchema,
   configurations: z.array(MarketConfigurationSchema),
 });
+export type Pool = z.infer<typeof PoolSchema>;
 
 const PoolDataResultSchema = z.object({
   data: z.object({
@@ -68,6 +66,8 @@ const PoolsDataDocument = gql`
   query pool($id: String) {
     pool(id: $id) {
       id
+      name
+      total_weight
       configurations {
         id
         weight
@@ -79,6 +79,7 @@ const PoolsDataDocument = gql`
           usd_withdrawn
           net_issuance
           reported_debt
+          updated_at
           market_snapshots_by_week(first: 2) {
             id
             usd_deposited
@@ -86,7 +87,6 @@ const PoolsDataDocument = gql`
             net_issuance
             reported_debt
             updated_at
-            created_at
             updates_in_period
           }
         }
@@ -105,109 +105,61 @@ const getPoolData = async (chainName: string, id: string) => {
     const { message } = json.errors[0];
     throw new Error(message);
   }
-  return PoolDataResultSchema.parse(json.data);
+  return PoolDataResultSchema.parse(json);
 };
 
-export const useGetPoolData1 = (id?: string) => {
+export const useGetPoolData = (id?: string) => {
   const [localChainId] = useRecoilState(chainIdState);
   const chainName = getChainNameById(localChainId);
   return useQuery(
-    ['useGetPoolData', chainName],
+    ['useGetPoolData', chainName, id],
     async () => {
       if (!chainName || !id) throw Error('Query expected chainName and id to be defined');
       const poolData = await getPoolData(chainName, id);
-      const pool = poolData.data.pool || getMockData();
+      const pool = addMockData(poolData.data.pool);
       return pool;
     },
-    { enabled: Boolean(chainName && id), staleTime: 1 }
+    { enabled: Boolean(chainName && id), staleTime: 10000 }
   );
 };
 
-function getMockData(): z.infer<typeof PoolSchema> {
-  return {
-    name: 'Spartan Pool',
-    total_weight: BigNumber.from('100'),
-    configurations: [
+function addMockData(pool: Pool | null): Pool | null {
+  if (!pool) return null;
+  const usd_withdrawn = wei('1500');
+  const usd_deposited = wei('1000');
+  const net_issuance = usd_withdrawn.sub(usd_deposited);
+  const reported_debt = wei('1500');
+  const market = pool.configurations[0].market;
+  const fakeMarket: z.infer<typeof MarketSchema> = {
+    ...market,
+    usd_withdrawn,
+    usd_deposited,
+    net_issuance,
+    reported_debt,
+    pnl: calculateMarketPnl(net_issuance, reported_debt),
+    market_snapshots_by_week: [
       {
-        id: 'pool123-market456',
-        market: {
-          id: 'market456',
-          address: '0x123...',
-          usd_deposited: BigNumber.from('1000.00'),
-          usd_withdrawn: BigNumber.from('500.00'),
-          net_issuance: BigNumber.from('500.00'),
-          reported_debt: BigNumber.from('10000.00'),
-          pnl: BigNumber.from(9500),
-          created_at: 1,
-          updated_at: 1,
-          market_snapshot_by_week: [
-            {
-              id: '213',
-              usd_deposited: BigNumber.from('1000.00'),
-              usd_withdrawn: BigNumber.from('500.00'),
-              net_issuance: BigNumber.from('500.00'),
-              reported_debt: BigNumber.from('10000.00'),
-              pnl: BigNumber.from('-9500'),
-              created_at: 1,
-              updated_at: 1,
-              updates_in_period: 2,
-            },
-            {
-              id: '213',
-              usd_deposited: BigNumber.from('1000.00'),
-              usd_withdrawn: BigNumber.from('500.00'),
-              net_issuance: BigNumber.from('500.00'),
-              reported_debt: BigNumber.from('100.00'),
-              pnl: BigNumber.from('400'),
-              created_at: 1,
-              updated_at: 1,
-              updates_in_period: 2,
-            },
-          ],
-        },
-        weight: BigNumber.from('50'),
-        max_debt_share_value: BigNumber.from('500.00'),
+        id: '2',
+        usd_withdrawn,
+        usd_deposited,
+        net_issuance,
+        reported_debt,
+        pnl: calculateMarketPnl(net_issuance, reported_debt),
+        updated_at: market.updated_at,
+        updates_in_period: 2,
       },
       {
-        id: 'pool123-market789',
-        market: {
-          id: 'market456',
-          address: '0x123...',
-          usd_deposited: BigNumber.from('1000.00'),
-          usd_withdrawn: BigNumber.from('500.00'),
-          net_issuance: BigNumber.from('500.00'),
-          reported_debt: BigNumber.from('10000.00'),
-          pnl: BigNumber.from(9500),
-          created_at: 1,
-          updated_at: 1,
-          market_snapshot_by_week: [
-            {
-              id: '213',
-              usd_deposited: BigNumber.from('1000.00'),
-              usd_withdrawn: BigNumber.from('500.00'),
-              net_issuance: BigNumber.from('500.00'),
-              reported_debt: BigNumber.from('10000.00'),
-              pnl: BigNumber.from('-9500'),
-              created_at: 1,
-              updated_at: 1,
-              updates_in_period: 2,
-            },
-            {
-              id: '213',
-              usd_deposited: BigNumber.from('1000.00'),
-              usd_withdrawn: BigNumber.from('500.00'),
-              net_issuance: BigNumber.from('500.00'),
-              reported_debt: BigNumber.from('100.00'),
-              pnl: BigNumber.from('400'),
-              created_at: 1,
-              updated_at: 1,
-              updates_in_period: 2,
-            },
-          ],
-        },
-        weight: BigNumber.from('50'),
-        max_debt_share_value: BigNumber.from('500.00'),
+        id: '1',
+        usd_withdrawn: wei('1000'),
+        usd_deposited: wei('1500'),
+        net_issuance: wei('500'),
+        reported_debt: wei('400'),
+        pnl: calculateMarketPnl(wei('500'), wei('400')),
+        updated_at: market.updated_at,
+        updates_in_period: 2,
       },
     ],
   };
+  pool.configurations[0].market = fakeMarket;
+  return pool;
 }
--- v3/ui/src/utils/subgraph.ts
+++ v3/ui/src/utils/subgraph.ts
@@ -1,5 +1,5 @@
-import { parseEther } from 'ethers/lib/utils';
+import { wei } from '@synthetixio/wei';
 
 export const getSubgraphUrl = (networkName = 'goerli') =>
   `https://api.thegraph.com/subgraphs/name/snx-v3/${networkName}`;
-export const formatGraphBigDecimal = parseEther;
+export const formatGraphBigDecimal = wei;
