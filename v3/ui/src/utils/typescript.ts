3a0ae370ac5db288484e3bf05ebff3550260e043
joey <joey@cc.snxdao.io>
2022-12-20 09:59:15 +1100

    Remove unused module

--- v3/ui/src/utils/typescript.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is useful if you have a Record<string,number | undefined>
- * and want to get the values.
- * Calling Object.values() would return (number| undefined)[].
- * Whereas calling this function would return number[]
- */
-export function values<O extends Record<string, unknown>>(o: O) {
-  return Object.values(o) as Exclude<O[keyof O], undefined>[];
-}
