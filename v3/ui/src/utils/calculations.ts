b207c592c589add0030a88329d5966061881a2f0
joey <joey@cc.snxdao.io>
2022-12-21 10:02:16 +1100

    Import pool type instead of schema

--- v3/ui/src/utils/calculations.ts
+++ v3/ui/src/utils/calculations.ts
@@ -1,4 +1,4 @@
-import { MarketSnapshotByWeekSchema, PoolSchema } from '../hooks/useGetPoolData';
+import { MarketSnapshotByWeekSchema, Pool } from '../hooks/useGetPoolData';
 import { z } from 'zod';
 import Wei, { wei } from '@synthetixio/wei';
 
@@ -17,14 +17,13 @@ export const calculateSevenDaysPnlGrowth = (marketSnapshots?: MarketSnapshotByWe
   return { value: end.sub(start), percentage: end.sub(start).div(start) };
 };
 
-type PoolData = z.infer<typeof PoolSchema>;
-export const calculatePoolPerformanceLifetime = (poolData: PoolData) => {
+export const calculatePoolPerformanceLifetime = (poolData: Pool) => {
   return poolData.configurations.reduce((acc, { market }) => {
     return acc.add(market.pnl);
   }, wei(0));
 };
 
-export const calculatePoolPerformanceSevenDays = (poolData: PoolData) => {
+export const calculatePoolPerformanceSevenDays = (poolData: Pool) => {
   const total = calculatePoolPerformanceLifetime(poolData);
   const totalSevenDaysAgo = poolData.configurations.reduce((acc, { market }) => {
     return acc.add(market.market_snapshots_by_week[1]?.pnl || wei(0));
