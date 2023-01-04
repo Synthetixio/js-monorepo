69f5e7b8d5e27c983edf1e6ab1fab0ffa12ea7fb
joey <joey@cc.snxdao.io>
2023-01-04 11:10:23 +1100

    Add Collateral Section

--- v3/ui/src/pages/pools/pool.tsx
+++ v3/ui/src/pages/pools/pool.tsx
@@ -1,17 +1,13 @@
 import { Box, Flex } from '@chakra-ui/react';
 import { Helmet } from 'react-helmet';
-import { useParams } from 'react-router-dom';
 import { PoolHeader } from './PoolHeader';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { Link } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
-import { useVaultCollaterals } from '@snx-v3/useVaultCollaterals';
 import { MarketSection } from './MarketSection';
+import { CollateralSection } from './CollateralSection';
 
 export const Pool = () => {
-  const { id } = useParams();
-
-  const x = useVaultCollaterals(id ? parseFloat(id) : undefined);
   return (
     <>
       <Helmet>
@@ -35,16 +31,7 @@ export const Pool = () => {
       <Flex>
         <Box w="40%" mr={2}>
           <PoolHeader />
-          <Box
-            mt={4}
-            borderColor="gray.900"
-            borderWidth="1px"
-            height="400px"
-            borderRadius="base"
-            padding={4}
-          >
-            Collateral, TODO
-          </Box>
+          <CollateralSection />
         </Box>
         <Box w="60%" ml={2}>
           <MarketSection />
