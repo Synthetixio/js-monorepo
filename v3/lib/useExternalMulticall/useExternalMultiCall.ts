552b207801c2c934be3ca90da4bfd2ecaa8e95e1
Joey <5688912+bachstatter@users.noreply.github.com>
2023-01-05 11:49:51 +1100

    UseMarketNamesById (#597)
    * Add back external multicall contract
    
    * Add hook for external multicall
    
    * useBlockchain in useExternalMulticall
    
    * useMarketNamesById without wagmi, in module and using multicall
    
    * Remove old hook

---
+++ v3/lib/useExternalMulticall/useExternalMultiCall.ts
@@ -0,0 +1,34 @@
+import { ethers } from 'ethers';
+import { useQuery } from '@tanstack/react-query';
+import type { _Multicall3 as MulticallGoerli } from '@synthetixio/v3-contracts/build/goerli/_Multicall3';
+import type { _Multicall3 as MulticallOptimismGoerli } from '@synthetixio/v3-contracts/build/optimism-goerli/_Multicall3';
+import { useProvider, useSigner } from '@snx-v3/useBlockchain';
+
+export async function importMulticall(chainName: string) {
+  switch (chainName) {
+    case 'goerli':
+      return import('@synthetixio/v3-contracts/build/goerli/_Multicall3');
+    case 'optimism-goerli':
+      return import('@synthetixio/v3-contracts/build/optimism-goerli/_Multicall3');
+    default:
+      throw new Error(`Unsupported chain ${chainName}`);
+  }
+}
+export const useExternalMulticall = () => {
+  const provider = useProvider();
+  const signer = useSigner();
+
+  return useQuery({
+    queryKey: [provider.network.name, { withSigner: Boolean(signer) }, 'ExternalMulticall'],
+    queryFn: async () => {
+      const Multicall = await importMulticall(provider.network.name);
+      return new ethers.Contract(Multicall.address, Multicall.abi, signer || provider) as
+        | MulticallGoerli
+        | MulticallOptimismGoerli;
+    },
+    enabled: Boolean(provider.network.name && (signer || provider)),
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  });
+};
+export type MulticallContractType = MulticallGoerli | MulticallOptimismGoerli;
