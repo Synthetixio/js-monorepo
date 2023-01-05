a0e482db7e99ef9df165ec13956537560512f3e7
joey <joey@cc.snxdao.io>
2023-01-05 14:29:34 +1100

    usePools should load names too

--- v3/lib/usePools/usePools.ts
+++ v3/lib/usePools/usePools.ts
@@ -1,23 +1,44 @@
 import { useQuery } from '@tanstack/react-query';
-import { useCoreProxy } from '@snx-v3/useCoreProxy';
-import { useNetwork } from '@snx-v3/useBlockchain';
+import { CoreProxyContractType, useCoreProxy } from '@snx-v3/useCoreProxy';
+import { z } from 'zod';
+import { ZodBigNumber } from '@snx-v3/zod';
 
+const PoolSchema = z.object({
+  id: ZodBigNumber.transform((x) => x.toString()),
+  name: z.string().default('Unnamed Pool'),
+});
+const loadPoolNames = async ({
+  CoreProxyContract,
+  poolIds,
+}: {
+  CoreProxyContract: CoreProxyContractType;
+  poolIds: string[];
+}) => {
+  const calls = poolIds.map((id) =>
+    CoreProxyContract.interface.encodeFunctionData('getPoolName', [id])
+  );
+  const multicallResult = await CoreProxyContract.callStatic.multicall(calls);
+  return multicallResult.map((bytes, i) => {
+    const decodedName = CoreProxyContract.interface.decodeFunctionResult('getPoolName', bytes)[0];
+    return PoolSchema.parse({ id: poolIds[i], name: decodedName });
+  });
+};
 export const usePools = () => {
-  const network = useNetwork();
-  const { data: CoreProxy } = useCoreProxy();
+  const { data: CoreProxyContract } = useCoreProxy();
   return useQuery({
-    queryKey: [network.name, 'pools'],
+    queryKey: [{ CoreProxyContract: CoreProxyContract?.address }, 'pools'],
     queryFn: async () => {
-      if (!CoreProxy) throw new Error('CoreProxy');
+      if (!CoreProxyContract) throw new Error('CoreProxy');
       const [preferredPool, approvedPools] = await Promise.all([
-        CoreProxy.getPreferredPool(),
-        CoreProxy.getApprovedPools(),
+        CoreProxyContract.getPreferredPool(),
+        CoreProxyContract.getApprovedPools(),
       ]);
-      return [preferredPool]
+      const poolIds = [preferredPool]
         .concat(approvedPools.filter((id) => !id.eq(preferredPool)))
         .map((id) => id.toString());
+      return await loadPoolNames({ CoreProxyContract: CoreProxyContract, poolIds });
     },
     placeholderData: [],
-    enabled: Boolean(CoreProxy && network.name),
+    enabled: Boolean(CoreProxyContract),
   });
 };
