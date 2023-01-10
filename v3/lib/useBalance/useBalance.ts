7fb85f0f60ca83b3daa7a07683357472dbee0653
joey <joey@cc.snxdao.io>
2023-01-11 09:41:31 +1100

    Add useBalance hook without wagmi

---
+++ v3/lib/useBalance/useBalance.ts
@@ -0,0 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
+import { useAccount, useProvider } from '@snx-v3/useBlockchain';
+import { Contract } from 'ethers';
+import { ZodBigNumber } from '@snx-v3/zod';
+import { wei } from '@synthetixio/wei';
+import { InfuraProvider } from '@ethersproject/providers';
+
+const BalanceSchema = ZodBigNumber.transform((x) => wei(x));
+const abi = ['function balanceOf(address) view returns (uint256)'];
+export const useBalance = ({
+  tokenAddress,
+  networkId,
+}: {
+  tokenAddress?: string | 'ETH';
+  networkId?: number;
+}) => {
+  const { address: accountAddress } = useAccount();
+  const connectedProvider = useProvider();
+
+  return useQuery({
+    queryKey: [{ tokenAddress, networkId, accountAddress }, 'balance'],
+    queryFn: async () => {
+      if (!tokenAddress) throw Error('Query should not be enabled');
+      const provider =
+        connectedProvider.network.chainId === networkId
+          ? connectedProvider
+          : new InfuraProvider(networkId, process.env.NEXT_PUBLIC_INFURA_PROJECT_ID);
+      if (tokenAddress === 'ETH') {
+        // if no token is passed get ETH balance
+        const balBn = await provider.getBalance(accountAddress);
+        return BalanceSchema.parse(balBn);
+      }
+      const contract = new Contract(tokenAddress, abi, provider);
+      const balBn = await contract.balanceOf(accountAddress);
+      return BalanceSchema.parse(balBn);
+    },
+    enabled: Boolean(accountAddress && tokenAddress),
+  });
+};
