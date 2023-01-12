02d6bb7794cf08fe2c316e671c46248a71164131
joey <joey@cc.snxdao.io>
2023-01-13 10:36:42 +1100

    If no collateral been delegated, show - instead of 0

--- v3/ui/src/pages/Home/VaultRow.tsx
+++ v3/ui/src/pages/Home/VaultRow.tsx
@@ -33,22 +33,29 @@ function VaultRowUi({
     <Tr>
       <Td>
         <Image alt="collateral image" width="24px" height="24px" src={collateralType.logo} />
-        <Amount value={liquidityPosition ? liquidityPosition.collateralValue : '0'} prefix="$" />
+        {liquidityPosition?.collateralValue.gt(0) ? (
+          <Amount value={liquidityPosition.collateralValue} prefix="$" />
+        ) : (
+          '-'
+        )}
         <Text fontSize="xs" opacity="0.66" mt="1">
-          <Amount
-            value={liquidityPosition ? liquidityPosition.collateralAmount : '0'}
-            suffix={` ${symbol}`}
-          />
+          {liquidityPosition?.collateralValue.gt(0) ? (
+            <Amount value={liquidityPosition.collateralAmount} suffix={` ${symbol}`} />
+          ) : (
+            '-'
+          )}
         </Text>
       </Td>
       <Td>
-        <Amount value={liquidityPosition ? liquidityPosition.debt : '0'} prefix="$" />
+        {liquidityPosition?.debt.gt(0) ? <Amount value={liquidityPosition.debt} prefix="$" /> : '-'}
       </Td>
       <Td>
-        <Amount value={liquidityPosition ? liquidityPosition.cRatio : '0'} suffix="%" />
-        <Text fontSize="xs" opacity="0.66" mt="1">
-          <Amount value={liquidityPosition ? collateralType.liquidationRatioD18 : '0'} suffix="%" />
-        </Text>
+        {liquidityPosition?.cRatio.gt(0) ? (
+          <Amount value={liquidityPosition.cRatio} suffix="%" />
+        ) : (
+          '-'
+        )}
+      </Td>
       <Td>
         <Amount value={collateralType.issuanceRatioD18.mul(100)} suffix="%" />
         <Tooltip label="Liquidation Ratio">
