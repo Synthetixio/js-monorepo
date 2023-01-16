4d607d49f291fa5d337092c8f37e3fcc19c63c12
Noisekit <nikita@cc.snxdao.io>
2023-01-16 10:57:15 +0800

    Add simplified @snx-v3/useTokenBalance and deprecate @snx-v3/useBalance

---
+++ v3/lib/useTokenBalance/useTokenBalance.ts
@@ -0,0 +1,43 @@
+import { assertAddressType } from '@snx-v3/assertAddressType';
+import { wei } from '@synthetixio/wei';
+import { useQuery } from '@tanstack/react-query';
+import { useAccount, useNetwork, useProvider } from '@snx-v3/useBlockchain';
+import { Contract } from 'ethers';
+import { ZodBigNumber } from '@snx-v3/zod';
+import { InfuraProvider } from '@ethersproject/providers';
+
+const BalanceSchema = ZodBigNumber.transform((x) => wei(x));
+const abi = ['function balanceOf(address) view returns (uint256)'];
+
+export const useTokenBalance = (address?: string, networkId?: number) => {
+  const { address: accountAddress } = useAccount();
+  const connectedProvider = useProvider();
+  const network = useNetwork();
+
+  const tokenAddress = assertAddressType(address) ? address : undefined;
+
+  // const { data: balanceData, refetch } = useBalance({
+  //   tokenAddress: assertAddressType(tokenAddress) ? tokenAddress : undefined,
+  //   customNetwork: chainId,
+  // });
+  //
+  // return {
+  //   value: balanceData || wei(0),
+  //   formattedValue: balanceData?.toString(2),
+  //   refetch,
+  // };
+
+  return useQuery({
+    queryKey: [{ networkId: networkId ?? network.id, accountAddress }, 'tokenBalance'],
+    queryFn: async () => {
+      if (!tokenAddress || !accountAddress) throw Error('Query should not be enabled');
+      const provider =
+        networkId && networkId !== network.id
+          ? new InfuraProvider(networkId, process.env.NEXT_PUBLIC_INFURA_PROJECT_ID)
+          : connectedProvider;
+      const contract = new Contract(tokenAddress, abi, provider);
+      return BalanceSchema.parse(await contract.balanceOf(accountAddress));
+    },
+    enabled: Boolean((networkId ?? network.id) && accountAddress && tokenAddress),
+  });
+};
