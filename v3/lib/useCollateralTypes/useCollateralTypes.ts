88e7fde8fe000851c2588038902641960aeae032
joey <joey@cc.snxdao.io>
2023-01-19 10:13:55 +1100

    Add display symbol to collateralType

--- v3/lib/useCollateralTypes/useCollateralTypes.ts
+++ v3/lib/useCollateralTypes/useCollateralTypes.ts
@@ -18,6 +18,7 @@ const CollateralConfigurationSchema = z.object({
 });
 const CollateralTypeSchema = CollateralConfigurationSchema.extend({
   symbol: z.string(),
+  displaySymbol: z.string(),
   price: ZodBigNumber.transform((x) => wei(x)),
   logo: z.string(),
 });
@@ -89,6 +90,7 @@ async function loadCollateralTypes({
     tokenAddress: config.tokenAddress,
     price: prices[i],
     symbol: symbols[i],
+    displaySymbol: symbols[i] === 'WETH' ? 'ETH' : symbols[i],
     // TODO: map symbol to icon
     logo: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F/logo.png',
   }));
