4d1dfeb34c1de102444565e8b71cd9ce8c7954c0
Noisekit <nikita@cc.snxdao.io>
2023-01-20 14:34:57 +0800

    Update references to new contracts

--- v3/lib/useExternalMulticall/useExternalMultiCall.ts
+++ v3/lib/useExternalMulticall/useExternalMultiCall.ts
@@ -1,15 +1,15 @@
 import { ethers } from 'ethers';
 import { useQuery } from '@tanstack/react-query';
-import type { _Multicall3 as MulticallGoerli } from '@synthetixio/v3-contracts/build/goerli/_Multicall3';
-import type { _Multicall3 as MulticallOptimismGoerli } from '@synthetixio/v3-contracts/build/optimism-goerli/_Multicall3';
+import type { Multicall3 as MulticallGoerli } from '@synthetixio/v3-contracts/build/goerli/Multicall3';
+import type { Multicall3 as MulticallOptimismGoerli } from '@synthetixio/v3-contracts/build/optimism-goerli/Multicall3';
 import { useProvider, useSigner } from '@snx-v3/useBlockchain';
 
 export async function importMulticall(chainName: string) {
   switch (chainName) {
     case 'goerli':
-      return import('@synthetixio/v3-contracts/build/goerli/_Multicall3');
+      return import('@synthetixio/v3-contracts/build/goerli/Multicall3');
     case 'optimism-goerli':
-      return import('@synthetixio/v3-contracts/build/optimism-goerli/_Multicall3');
+      return import('@synthetixio/v3-contracts/build/optimism-goerli/Multicall3');
     default:
       throw new Error(`Unsupported chain ${chainName}`);
   }
