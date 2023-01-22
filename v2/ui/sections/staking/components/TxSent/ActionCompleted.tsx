bc9057470ef8a1b87f30615ce1d2598091bde043
joey <joey@cc.snxdao.io>
2023-01-23 09:33:47 +1100

    Remove curve since their api returning wrong apy

--- v2/ui/sections/staking/components/TxSent/ActionCompleted.tsx
+++ v2/ui/sections/staking/components/TxSent/ActionCompleted.tsx
@@ -2,27 +2,13 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useSetRecoilState } from 'recoil';
-
-import {
-  FlexDivRowCentered,
-  FlexDivCentered,
-  FlexDivColCentered,
-  ExternalLink,
-  boxShadowBlue,
-} from '@snx-v1/styles';
-
+import { FlexDivRowCentered, FlexDivCentered, ExternalLink, boxShadowBlue } from '@snx-v1/styles';
 import Success from 'assets/svg/app/success.svg';
-
 import ROUTES from 'constants/routes';
-import { CryptoCurrency, Synths } from 'constants/currency';
-
+import { Synths } from 'constants/currency';
 import Etherscan from 'containers/BlockExplorer';
-
-import { formatPercent } from 'utils/formatters/number';
 import { amountToBurnState, amountToMintState, burnTypeState, mintTypeState } from 'store/staking';
-import Currency from 'components/Currency';
-import useLPData from 'hooks/useLPData';
-import { LP } from 'sections/earn/types';
+
 import {
   SectionHeader,
   SectionSubtext,
@@ -53,7 +39,6 @@ const ActionCompleted: React.FC<ActionCompletedProps> = ({
   const { t } = useTranslation();
   const navigate = useNavigate();
   const { isMainnet } = Connector.useContainer();
-  const lpData = useLPData();
 
   const { blockExplorerInstance } = Etherscan.useContainer();
   const link = blockExplorerInstance != null ? blockExplorerInstance.txLink(hash ?? '') : undefined;
@@ -150,20 +135,7 @@ const ActionCompleted: React.FC<ActionCompletedProps> = ({
       <SectionHeader>
         {t('staking.actions.mint.completed.title', { synth: Synths.sUSD })}
       </SectionHeader>
-      <MiddleSection>
-        <StyledExternalLink href={ROUTES.Earn.sUSD_EXTERNAL}>
-          <MainInfoBox>
-            <Currency.Icon currencyKey={CryptoCurrency.CRV} width="20" height="20" />
-            <MiddleInfoSection>
-              {t('staking.actions.mint.completed.curve', { synth: Synths.sUSD })}
-            </MiddleInfoSection>
-            <RightInfoSection>
-              <AprText>{t('staking.actions.mint.completed.est-apr')}</AprText>
-              <AprValue>{formatPercent(lpData[LP.CURVE_sUSD].APR)}</AprValue>
-            </RightInfoSection>
-          </MainInfoBox>
-        </StyledExternalLink>
-      </MiddleSection>
+
       <SectionSubtext>{t('staking.actions.mint.completed.subtext')}</SectionSubtext>
       <ButtonWrap>
         <LeftButton
@@ -182,49 +154,6 @@ const ActionCompleted: React.FC<ActionCompletedProps> = ({
     </Container>
   );
 };
-
-const MainInfoBox = styled(FlexDivCentered)`
-  background-color: ${(props) => props.theme.colors.navy};
-  justify-content: space-around;
-  color: ${(props) => props.theme.colors.white};
-  height: 95px;
-  width: 100%;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
-const MiddleInfoSection = styled.div`
-  word-wrap: normal;
-  height: auto;
-  width: 180px;
-  text-align: left;
-  font-family: ${(props) => props.theme.fonts.interBold};
-  font-size: 14px;
-`;
-
-const RightInfoSection = styled(FlexDivColCentered)`
-  border: 1px solid #161b44;
-  padding: 10px;
-  font-size: 10px;
-  border-radius: 4px;
-  background: ${(props) => props.theme.colors.black};
-  justify-content: center;
-`;
-
-const AprText = styled.div`
-  color: ${(props) => props.theme.colors.gray};
-  margin-bottom: 5px;
-  font-family: ${(props) => props.theme.fonts.interBold};
-  font-size: 12px;
-  text-transform: uppercase;
-`;
-
-const AprValue = styled.div`
-  color: ${(props) => props.theme.colors.white};
-  margin-bottom: 5px;
-  font-family: ${(props) => props.theme.fonts.extended};
-  font-size: 14px;
-`;
 
 const BaseButton = styled.div`
   @media (max-width: 768px) {
@@ -263,11 +192,6 @@ const HedgeButton = styled(BaseButton)`
   background-color: ${(props) => props.theme.colors.grayBlue};
   color: ${(props) => props.theme.colors.blue};
   text-transform: uppercase;
-`;
-
-const StyledExternalLink = styled(ExternalLink)`
-  display: block;
-  width: 100%;
 `;
 
 export default ActionCompleted;
