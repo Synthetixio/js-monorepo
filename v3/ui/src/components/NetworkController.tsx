5a1559ab0c657df6394c091a365994aec9ab8131
jmzwar <james@jmzwar.com>
2023-01-22 20:28:28 +1100

    header + alt

--- v3/ui/src/components/NetworkController.tsx
+++ v3/ui/src/components/NetworkController.tsx
@@ -1,8 +1,15 @@
-import { Box, Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
+import { Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { EthereumIcon, OptimismIcon, FailedIcon, ChevronUp, ChevronDown } from '@snx-v2/icons';
+import {
+  EthereumIcon,
+  OptimismIcon,
+  FailedIcon,
+  ChevronUp,
+  ChevronDown,
+  WalletIcon,
+} from '@snx-v3/icons';
 import { useAccount } from '@snx-v3/useBlockchain';
-import { NetworkId } from '@synthetixio/contracts-interface';
+
 import { switchNetwork } from '@wagmi/core';
 
 const activeIcon = (currentNetwork: number) => {
@@ -27,7 +34,6 @@ export function NetworkController() {
   return (
     <ConnectButton.Custom>
       {({ account, chain, openAccountModal, openChainModal, openConnectModal, mounted }) => {
-        console.log('Chian is', chain);
         const { name, icon } = activeIcon(chain?.id || 1);
         return (
           <Flex
@@ -48,24 +54,48 @@ export function NetworkController() {
                     variant="outline"
                     colorScheme="gray"
                     sx={{ '> span': { display: 'flex', alignItems: 'center' } }}
+                    mr={1}
                   >
                     {icon}
 
-                    <Box display={{ base: 'none', md: 'block' }}>
-                      <Text variant="nav" fontSize="sm" fontWeight={700} ml={1.5} mr={2}>
-                        {name}
-                      </Text>
+                    <Text
+                      variant="nav"
+                      fontSize="sm"
+                      fontWeight={700}
+                      ml={1.5}
+                      mr={2}
+                      display={{ base: 'none', md: 'initial' }}
+                    >
+                      {name}
+                    </Text>
+                    <Flex display={{ base: 'none', md: 'initial' }}>
                       {isOpen ? <ChevronUp color="cyan" /> : <ChevronDown color="cyan.500" />}
-                    </Box>
+                    </Flex>
                   </MenuButton>
                   <MenuList>
-                    <MenuItem onClick={() => console.log('To mainnet')}>
+                    <MenuItem
+                      onClick={() => {
+                        if (address) {
+                          switchNetwork({ chainId: 1 });
+                        } else {
+                          openConnectModal();
+                        }
+                      }}
+                    >
                       <EthereumIcon />
                       <Text variant="nav" ml={2}>
                         Ethereum Mainnet
                       </Text>
                     </MenuItem>
-                    <MenuItem onClick={() => console.log('To optimism')}>
+                    <MenuItem
+                      onClick={() => {
+                        if (address) {
+                          switchNetwork({ chainId: 10 });
+                        } else {
+                          openConnectModal();
+                        }
+                      }}
+                    >
                       <OptimismIcon />
                       <Text variant="nav" ml={2}>
                         Optimism
@@ -75,78 +105,44 @@ export function NetworkController() {
                 </>
               )}
             </Menu>
-
-            {address ? (
+            {address && account && mounted ? (
               <Button
                 variant="outline"
                 colorScheme="gray"
-                onClick={openChainModal}
-                style={{ display: 'flex', alignItems: 'center' }}
-                type="button"
-                mr="4"
-                size={['sm', 'sm', 'sm', 'md']}
+                ml={2}
+                height={10}
+                py="6px"
+                px="9.5px"
+                onClick={() => {
+                  if (chain?.unsupported) {
+                    openChainModal();
+                  } else {
+                    openAccountModal();
+                  }
+                }}
               >
-                {chain?.hasIcon && (
-                  <span
-                    style={{
-                      background: chain?.iconBackground,
-                      width: 20,
-                      height: 20,
-                      borderRadius: 999,
-                      overflow: 'hidden',
-                      marginRight: 8,
-                    }}
-                  >
-                    {chain?.iconUrl && (
-                      <img
-                        width={20}
-                        height={20}
-                        alt={chain?.name ?? 'Chain icon'}
-                        src={chain?.iconUrl}
-                      />
-                    )}
-                  </span>
-                )}
-                {chain?.name}
-              </Button>
-            ) : null}
-            {(() => {
-              if (!mounted || !account || !chain) {
-                return (
-                  <Button
-                    onClick={openConnectModal}
-                    type="button"
-                    size={['sm', 'sm', 'sm', 'md']}
-                    data-testid="connect-wallet-button"
-                  >
-                    Connect Wallet
-                  </Button>
-                );
-              }
-              if (chain.unsupported) {
-                return (
-                  <Button
-                    colorScheme="red"
-                    onClick={openChainModal}
-                    type="button"
-                    size={['sm', 'sm', 'sm', 'md']}
-                  >
-                    Wrong network
-                  </Button>
-                );
-              }
-              return (
-                <Button
-                  colorScheme="gray"
-                  onClick={openAccountModal}
-                  type="button"
-                  size={['sm', 'sm', 'sm', 'md']}
-                  data-testid="account-button"
+                <WalletIcon />
+                <Text
+                  ml={1}
+                  color="whiteAlpha.800"
+                  fontWeight={700}
+                  fontSize="xs"
+                  userSelect="none"
                 >
-                  {account.displayName}
-                </Button>
-              );
-            })()}
+                  {account?.ensName || account.displayName}
+                </Text>
+              </Button>
+            ) : (
+              <Button
+                onClick={openConnectModal}
+                type="button"
+                size={['sm', 'sm', 'sm', 'md']}
+                ml={2}
+                data-testid="connect-wallet-button"
+              >
+                Connect Wallet
+              </Button>
+            )}
           </Flex>
         );
       }}
--- v3/ui/src/layouts/Default/Header.tsx
+++ v3/ui/src/layouts/Default/Header.tsx
@@ -3,7 +3,7 @@ import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { NetworkController } from '../../components/NetworkController';
 import { useEffect } from 'react';
 import { AccountsSelector } from '@snx-v3/AccountsSelector';
-import { Logo } from '@snx-v3/Icons';
+import { Logo, LogoIcon } from '@snx-v3/icons';
 
 export default function Header() {
   const { onClose } = useDisclosure();
@@ -20,6 +20,11 @@ export default function Header() {
           <Box display={{ base: 'none', md: 'inline-block' }}>
             <Link to="/" as={RouterLink} py={4} pr={2}>
               <Logo />
+            </Link>
+          </Box>
+          <Box display={{ md: 'none' }}>
+            <Link to="/" as={RouterLink} py={4} pr={2}>
+              <LogoIcon />
             </Link>
           </Box>
           <Flex justifyContent="center" alignItems="center">
