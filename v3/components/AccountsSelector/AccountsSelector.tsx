1d8ddfad515322ab89e12c82c7083ade0627905a
jmzwar <james@jmzwar.com>
2023-01-24 09:53:52 +1100

    mobile fix connect

--- v3/components/AccountsSelector/AccountsSelector.tsx
+++ v3/components/AccountsSelector/AccountsSelector.tsx
@@ -1,5 +1,5 @@
 // import { CheckIcon } from '@chakra-ui/icons';
-import { Button } from '@chakra-ui/react';
+import { Button, Fade } from '@chakra-ui/react';
 // import { createSearchParams, generatePath, Link as RouterLink } from 'react-router-dom';
 import { prettyString } from '@snx-v3/format';
 import { useParams } from '@snx-v3/useParams';
@@ -38,9 +38,11 @@ export function AccountsSelector() {
   }
 
   return (
-    <Button size="sm" as={Button} variant="outline" w="100%" maxW="180px" h="36px">
-      {`Account #${prettyString(params.accountId, 3, 3)}`}
-    </Button>
+    <Fade in={true}>
+      <Button size="sm" height="40px" as={Button} variant="outline" w="100%" maxW="180px" h="36px">
+        {`Account #${prettyString(params.accountId, 3, 3)}`}
+      </Button>
+    </Fade>
     // Temporarily disable account selector menu
     // <Menu>
     //   <MenuList fontSize="xs" bg="black" h="36px" py={0} border="1px solid rgba(255,255,255,0.33)">
