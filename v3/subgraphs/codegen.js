d246450e7f1dc45dfc45d7918556fc927b9b8fef
Noisekit <nikita@cc.snxdao.io>
2023-01-23 11:40:16 +0800

    Update startBlock for subgraph

--- v3/subgraphs/codegen.js
+++ v3/subgraphs/codegen.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 
+const ethers = require('ethers');
 const fs = require('fs');
 
 const [networkName] = process.argv.slice(2);
 
-const networks = JSON.parse(fs.readFileSync('./networks.json', 'utf8'));
-networks[networkName].CoreProxy.address =
-  require(`@synthetixio/v3-contracts/deployments/${networkName}/CoreProxy.json`).address;
-// TODO: startBlock???
+async function run() {
+  const provider = new ethers.providers.InfuraProvider(networkName, process.env.INFURA_KEY);
 
-fs.writeFileSync('./networks.json', JSON.stringify(networks, null, 2), 'utf8');
+  const networks = JSON.parse(fs.readFileSync('./networks.json', 'utf8'));
+
+  networks[networkName].CoreProxy.address =
+    require(`@synthetixio/v3-contracts/deployments/${networkName}/CoreProxy.json`).address;
+
+  const deployTx =
+    require(`@synthetixio/v3-contracts/deployments/${networkName}/InitialCoreProxy.json`).deployTxnHash;
+  const tx = await provider.getTransactionReceipt(deployTx);
+  networks[networkName].CoreProxy.startBlock = tx.blockNumber;
+
+  fs.writeFileSync('./networks.json', JSON.stringify(networks, null, 2), 'utf8');
+}
+
+run();
