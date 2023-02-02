a00f65bfc1f8ebb9c32a33a42fcce1ea44d241c9
jmzwar <james@jmzwar.com>
2023-02-02 14:28:14 +1100

    update terms modal

--- v2/components/TermsModal/TermsModal.tsx
+++ v2/components/TermsModal/TermsModal.tsx
@@ -8,17 +8,19 @@ import {
   Text,
   Button,
   Box,
+  Link,
+  ListItem,
+  UnorderedList,
 } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 import { theme } from '@synthetixio/v3-theme';
 import { LOCAL_STORAGE_KEYS } from '@snx-v2/Constants';
-// import { useTranslation } from 'react-i18next';
 
 interface TermsModalProps {
   defaultOpen: boolean;
 }
 
 export const TermsModal = ({ defaultOpen = true }: TermsModalProps) => {
-  // const { t } = useTranslation();
   const [isOpen, setOpen] = useState(defaultOpen);
   const [enabled, setEnabled] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
@@ -29,6 +31,8 @@ export const TermsModal = ({ defaultOpen = true }: TermsModalProps) => {
       setOpen(false);
     }
   };
+
+  console.log(ref.current?.scrollTop);
 
   return (
     <Modal isOpen={isOpen} onClose={() => {}}>
@@ -52,7 +56,7 @@ export const TermsModal = ({ defaultOpen = true }: TermsModalProps) => {
           <Box
             onScroll={() => {
               const condition = (ref.current && ref?.current?.scrollHeight / 2) || 300;
-              if (ref.current && ref.current?.scrollTop > condition) {
+              if (ref.current && ref.current?.scrollTop > condition + 30) {
                 setEnabled(true);
               } else {
                 setEnabled(false);
@@ -65,36 +69,94 @@ export const TermsModal = ({ defaultOpen = true }: TermsModalProps) => {
             height="320px"
             overflow="scroll"
           >
-            <Text>
-              Synthetix is a blockchain-based decentralized finance project. You are participating
-              at your own risk. Synthetix is offered for use “as is” and without any guarantees
-              regarding security. The protocol is made up of immutable code and can be accessed
-              through a variety of user interfaces. No central entity operates the Synthetix
-              protocol. Decisions related to the protocol are governed by a dispersed group of
-              participants who collectively govern and maintain the protocol. Synthetix DAO does not
-              unilaterally offer, maintain, operate, administer, or control any trading interfaces.
-              The only user interfaces maintained by Synthetix DAO are the governance and staking
-              interfaces herein. You can participate in the governance process by staking SNX tokens
-              in accordance with the rules and parameters summarized here (link to Governance
-              documents), and/or joining the Synthetix Discord (link) and contributing to the
-              conversation. The rules and parameters associated with the Synthetix protocol and
-              Synthetix DAO governance are subject to change at any time. Your use of Synthetix is
-              conditioned upon your acceptance to be bound by the Synthetix Term of Use, which can
-              be found here (link). The laws that apply to your use of Synthetix may vary based upon
-              the jurisdiction in which you are located. We strongly encourage you to speak with
-              legal counsel in your jurisdiction if you have any questions regarding your use of
-              Synthetix. By entering into this agreement, you are not agreeing to enter into a
-              partnership. You understand that Synthetix is a decentralized protocol provided on an
-              “as is” basis. You hereby release all present and future claims against Synthetix DAO
-              related to your use of the protocol, the SNX token, SNX DAO governance, and any other
-              facet of the protocol. You agree to indemnify and hold harmless SNX DAO and its
-              affiliates for any costs arising out of or relating to your use of the Synthetix
-              protocol. You are not accessing the protocol from Burma (Myanmar), Cuba, Iran, Sudan,
-              Syria, the Western Balkans, Belarus, Côte d’Ivoire, Democratic Republic of the Congo,
-              Iraq, Lebanon, Liberia, Libya, North Korea, Russia, certain sanctioned areas of
-              Ukraine, Somalia, Venezuela, Yemen, or Zimbabwe (collectively, “Prohibited
-              Jurisdictions”), or any other jurisdiction listed as a Specially Designated National
-              by the United States Office of Foreign Asset Control (“OFAC”).
+            <Text fontSize="14px">
+              <UnorderedList height="100%">
+                <ListItem>
+                  Synthetix is a blockchain-based decentralized finance project. You are
+                  participating at your own risk.
+                </ListItem>
+                <ListItem mt={2}>
+                  Synthetix is offered for use “as is” and without any guarantees regarding
+                  security. The protocol is made up of immutable code and can be accessed through a
+                  variety of user interfaces.
+                </ListItem>
+                <ListItem mt={2}>
+                  No central entity operates the Synthetix protocol. Decisions related to the
+                  protocol are governed by a dispersed group of participants who collectively govern
+                  and maintain the protocol.
+                </ListItem>
+                <ListItem mt={2}>
+                  Synthetix DAO does not unilaterally offer, maintain, operate, administer, or
+                  control any trading interfaces. The only user interfaces maintained by Synthetix
+                  DAO are the governance and staking interfaces herein.
+                </ListItem>
+                <ListItem mt={2}>
+                  You can participate in the governance process by staking SNX tokens in accordance
+                  with the rules and parameters summarized{' '}
+                  <Link
+                    href="https://governance.synthetix.io/"
+                    target="_blank"
+                    color="cyan.500"
+                    _focusVisible={{ outline: 'none' }}
+                  >
+                    here
+                  </Link>{' '}
+                  , and/or joining the{' '}
+                  <Link
+                    color="cyan.500"
+                    target="_blank"
+                    href="https://discord.com/invite/AEdUHzt"
+                    _focusVisible={{ outline: 'none' }}
+                  >
+                    Synthetix Discord
+                  </Link>{' '}
+                  and contributing to the conversation.
+                </ListItem>
+                <ListItem mt={2}>
+                  The rules and parameters associated with the Synthetix protocol and Synthetix DAO
+                  governance are subject to change at any time.
+                </ListItem>
+                <ListItem mt={2}>
+                  Your use of Synthetix is conditioned upon your acceptance to be bound by the
+                  Synthetix Term of Use, which can be found{' '}
+                  <Link
+                    as={ReactRouterLink}
+                    to="/terms"
+                    color="cyan.500"
+                    _focusVisible={{ outline: 'none' }}
+                  >
+                    here
+                  </Link>
+                  .
+                </ListItem>
+                <ListItem mt={2}>
+                  The laws that apply to your use of Synthetix may vary based upon the jurisdiction
+                  in which you are located. We strongly encourage you to speak with legal counsel in
+                  your jurisdiction if you have any questions regarding your use of Synthetix.
+                </ListItem>
+                <ListItem mt={2}>
+                  By entering into this agreement, you are not agreeing to enter into a partnership.
+                  You understand that Synthetix is a decentralized protocol provided on an “as is”
+                  basis.
+                </ListItem>
+                <ListItem mt={2}>
+                  You hereby release all present and future claims against Synthetix DAO related to
+                  your use of the protocol, the SNX token, SNX DAO governance, and any other facet
+                  of the protocol.
+                </ListItem>
+                <ListItem mt={2}>
+                  You agree to indemnify and hold harmless SNX DAO and its affiliates for any costs
+                  arising out of or relating to your use of the Synthetix protocol.
+                </ListItem>
+                <ListItem mt={2}>
+                  You are not accessing the protocol from Burma (Myanmar), Cuba, Iran, Sudan, Syria,
+                  the Western Balkans, Belarus, Côte d’Ivoire, Democratic Republic of the Congo,
+                  Iraq, Lebanon, Liberia, Libya, North Korea, Russia, certain sanctioned areas of
+                  Ukraine, Somalia, Venezuela, Yemen, or Zimbabwe (collectively, “Prohibited
+                  Jurisdictions”), or any other jurisdiction listed as a Specially Designated
+                  National by the United States Office of Foreign Asset Control (“OFAC”).
+                </ListItem>
+              </UnorderedList>
             </Text>
           </Box>
         </ModalBody>
