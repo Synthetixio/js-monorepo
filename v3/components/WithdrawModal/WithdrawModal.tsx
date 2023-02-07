4c565280e555f2bc57950f06678132015acebb29
Joey <5688912+bachstatter@users.noreply.github.com>
2023-02-07 14:11:22 +1100

    Withdraw (#691)
    * Add use withdraw hook
    
    * Add withdraw modal
    
    * Use withdraw modal
    
    * We can now remove manage position
    
    * Add amount to WIthdrawMachine context. And grab it from context rather than props
    
    * Dont render deposit modal when not active, it throws when I type in withdraw input
    
    * Dont show unwrap step for snx
    
    * Dont show wrap step for synthetix
    
    * Deps
    
    * Uwrap will not be called if collateral is not eth
    
    * Simplify onClose

---
+++ v3/components/WithdrawModal/WithdrawModal.tsx
@@ -0,0 +1,198 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useToast,
+} from '@chakra-ui/react';
+import { useCallback, useContext, useEffect } from 'react';
+import { CollateralType, useCollateralType } from '@snx-v3/useCollateralTypes';
+import { Amount } from '@snx-v3/Amount';
+import { useLiquidityPosition } from '@snx-v3/useLiquidityPosition';
+import { useUnWrapEth } from '@snx-v3/useWrapEth';
+import { Multistep } from '@snx-v3/Multistep';
+import { Wei, wei } from '@synthetixio/wei';
+import { FC } from 'react';
+import { useParams } from '@snx-v3/useParams';
+import { WithdrawMachine } from './WithdrawMachine';
+import { useMachine } from '@xstate/react';
+import { useWithdraw } from '@snx-v3/useWithdraw';
+import { ManagePositionContext } from '@snx-v3/ManagePositionContext';
+
+export const WithdrawModalUi: FC<{
+  amount: Wei;
+  isOpen: boolean;
+  onClose: () => void;
+  collateralType?: CollateralType;
+  state: string;
+  error: { error: Error; step: string } | null;
+  onSubmit: () => void;
+}> = ({ amount, isOpen, onClose, collateralType, onSubmit, state, error }) => {
+  return (
+    <Modal size="lg" isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
+      <ModalOverlay />
+      <ModalContent bg="black" color="white" data-testid="withdraw modal">
+        <ModalHeader>Complete this action</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text mb="2">Please execute the following transactions:</Text>
+
+          <Multistep
+            step={1}
+            title="Withdraw"
+            subtitle={
+              <Text as="div">
+                <Amount value={amount} suffix={` ${collateralType?.symbol}`} /> will be withdrawn
+              </Text>
+            }
+            status={{
+              failed: Boolean(error?.step === 'withdraw'),
+              disabled: amount.eq(0),
+              success: state === 'unwrap',
+              loading: state === 'withdraw' && !error,
+            }}
+          />
+          {collateralType?.symbol === 'WETH' ? (
+            <Multistep
+              step={2}
+              title={`Unwrap ${collateralType?.symbol}`}
+              subtitle="This will unwrap your WETH to ETH"
+              status={{
+                failed: Boolean(error?.step === 'unwrap'),
+                disabled: collateralType?.symbol !== 'WETH',
+                success: state === 'success',
+                loading: state === 'unwrap' && !error,
+              }}
+            />
+          ) : null}
+
+          <Button
+            disabled={['unwrap', 'withdraw'].includes(state) && !error}
+            onClick={onSubmit}
+            width="100%"
+            my="4"
+            data-testid="withdraw submit button"
+          >
+            {(() => {
+              switch (true) {
+                case Boolean(error):
+                  return 'Retry';
+                case ['unwrap', 'withdraw'].includes(state):
+                  return 'Processing...';
+                case state === 'success':
+                  return 'Done';
+                default:
+                  return 'Start';
+              }
+            })()}
+          </Button>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+export type WithdrawModalProps = FC<{
+  isOpen: boolean;
+  onClose: () => void;
+}>;
+export const WithdrawModal: WithdrawModalProps = ({ onClose, isOpen }) => {
+  const params = useParams();
+  const collateralType = useCollateralType(params.collateralSymbol);
+  const { collateralChange } = useContext(ManagePositionContext);
+  const { data: liquidityPosition, refetch: refetchLiquidityPosition } = useLiquidityPosition({
+    accountId: params.accountId,
+    tokenAddress: collateralType?.tokenAddress,
+    poolId: params.poolId,
+  });
+  const toast = useToast({ isClosable: true, duration: 9000 });
+
+  const { exec: unwrap } = useUnWrapEth();
+  const currentCollateral = liquidityPosition?.collateralAmount || wei(0);
+  const { exec: execWithdraw } = useWithdraw({
+    accountId: params.accountId,
+    poolId: params.poolId,
+    collateralTypeAddress: collateralType?.tokenAddress,
+    collateralChange,
+    currentCollateral: currentCollateral,
+  });
+  const [state, send] = useMachine(WithdrawMachine, {
+    context: {
+      amount: collateralChange.abs(),
+    },
+    services: {
+      withdraw: async () => {
+        try {
+          await execWithdraw();
+          await refetchLiquidityPosition();
+        } catch (error) {
+          toast.closeAll();
+          toast({
+            title: 'Withdraw failed',
+            description: 'Please try again.',
+            status: 'error',
+          });
+          throw Error('Wrapping failed', { cause: error });
+        }
+      },
+      unwrap: async () => {
+        try {
+          toast({
+            title: 'Unwrap',
+            description: 'Unwrapping WETH to ETH.',
+            status: 'info',
+          });
+
+          await unwrap(state.context.amount);
+        } catch (e) {
+          toast.closeAll();
+          toast({ title: 'Unwrap failed', description: 'Please try again.', status: 'error' });
+          throw Error('Unwrap failed', { cause: e });
+        }
+      },
+    },
+  });
+  const collateralChangeString = collateralChange.toString();
+  useEffect(() => {
+    send('SET_AMOUNT', { amount: wei(collateralChangeString).abs() });
+  }, [collateralChangeString, send]);
+
+  const onSubmit = useCallback(async () => {
+    if (state.matches('success')) {
+      send('RESET');
+      onClose();
+      return;
+    }
+    if (state.context.error) {
+      send('RETRY');
+      return;
+    }
+    send('RUN');
+  }, [onClose, send, state]);
+
+  return (
+    <WithdrawModalUi
+      amount={state.context.amount}
+      isOpen={isOpen}
+      onClose={onClose}
+      collateralType={collateralType}
+      state={(() => {
+        switch (true) {
+          case state.matches('unwrap'):
+            return 'unwrap';
+          case state.matches('withdraw'):
+            return 'withdraw';
+          case state.matches('success'):
+            return 'success';
+          default:
+            return 'idle';
+        }
+      })()}
+      error={state.context.error}
+      onSubmit={onSubmit}
+    />
+  );
+};
--- v3/ui/src/pages/Manage/ManageActions.tsx
+++ v3/ui/src/pages/Manage/ManageActions.tsx
@@ -21,7 +21,6 @@ import {
 } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Borrow } from './Borrow';
-import { useManagePosition } from './useManagePosition';
 import { Repay } from './Repay';
 import { Withdraw } from './Withdraw';
 import { Deposit } from './Deposit';
@@ -31,6 +30,7 @@ import { BorrowModal } from '@snx-v3/BorrowModal';
 import { safeImport } from '@synthetixio/safe-import';
 
 const DepositModal = lazy(() => safeImport(() => import('@snx-v3/DepositModal')));
+const WithdrawModal = lazy(() => safeImport(() => import('@snx-v3/WithdrawModal')));
 
 const validActions = ['borrow', 'deposit', 'repay', 'withdraw'] as const;
 const ManageActionSchema = z.enum(validActions);
@@ -139,20 +139,6 @@ export const ManageAction = () => {
   const parsedActionParam = ManageActionSchema.safeParse(params.manageAction);
   const parsedAction = parsedActionParam.success ? parsedActionParam.data : null;
 
-  const { exec } = useManagePosition({
-    accountId: params.accountId,
-    poolId: params.poolId,
-    collateralType,
-    collateralChange,
-    debtChange,
-    collateralAmount: liquidityPosition.data?.collateralAmount,
-    refetch: () => {
-      setCollateralChange(wei(0));
-      setDebtChange(wei(0));
-      liquidityPosition.refetch();
-    },
-  });
-
   const onSubmit = useCallback(
     (e: FormEvent) => {
       e.preventDefault();
@@ -160,14 +146,9 @@ export const ManageAction = () => {
       if (!form.reportValidity() || !isValid) {
         return;
       }
-      if (parsedAction === 'repay' || parsedAction === 'deposit' || parsedAction === 'borrow') {
-        setTxnModalOpen(parsedAction);
-      } else {
-        // TODO add more hooks for all actions and remove this
-        exec();
-      }
+      setTxnModalOpen(parsedAction);
     },
-    [exec, isValid, parsedAction]
+    [isValid, parsedAction]
   );
 
   useEffect(() => {
@@ -221,15 +202,28 @@ export const ManageAction = () => {
         isOpen={txnModalOpen === 'borrow'}
       />
       <Suspense fallback={null}>
-        <DepositModal
-          collateralChange={collateralChange}
+        {txnModalOpen === 'deposit' ? (
+          <DepositModal
+            collateralChange={collateralChange}
+            onClose={() => {
+              liquidityPosition.refetch();
+              setCollateralChange(wei(0));
+              setDebtChange(wei(0));
+              setTxnModalOpen(null);
+            }}
+            isOpen={txnModalOpen === 'deposit'}
+          />
+        ) : null}
+      </Suspense>
+      <Suspense fallback={null}>
+        <WithdrawModal
           onClose={() => {
             liquidityPosition.refetch();
             setCollateralChange(wei(0));
             setDebtChange(wei(0));
             setTxnModalOpen(null);
           }}
-          isOpen={txnModalOpen === 'deposit'}
+          isOpen={txnModalOpen === 'withdraw'}
         />
       </Suspense>
     </>
