57aaea004c7b89863cf09f0ac1f3e920c714af9c
Noisekit <nikita@cc.snxdao.io>
2023-02-07 14:20:05 +0800

    E2E creates new account with first deposit of SNX, skip WETH deposit test (until enabled) (#693)

---
+++ v3/cypress/cypress/tasks/getCollateralConfig.js
@@ -0,0 +1,25 @@
+import { ethers } from 'ethers';
+import * as CoreProxy from '@synthetixio/v3-contracts/src/goerli/CoreProxy';
+
+export async function getCollateralConfig(symbol) {
+  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
+
+  const coreProxy = new ethers.Contract(CoreProxy.address, CoreProxy.abi, provider);
+  const collateralConfigs = await coreProxy.getCollateralConfigurations(true);
+  for (const config of collateralConfigs) {
+    try {
+      const contract = new ethers.Contract(
+        config.tokenAddress,
+        ['function symbol() view returns (string)'],
+        provider
+      );
+      const collateralSymbol = await contract.symbol();
+      if (collateralSymbol === symbol) {
+        return config;
+      }
+    } catch (e) {
+      // nevermind
+    }
+  }
+  throw new Error(`Collateral config for "${symbol}" does not exist`);
+}
