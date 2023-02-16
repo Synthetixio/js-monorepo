4c35ec6b0350df1e9e29cb0a9d69e0ac3d46c03e
joey <joey@cc.snxdao.io>
2023-02-15 18:09:29 -0800

    Move out getting previous fee period for both networks to hook

--- v2/lib/useFeesBurnedInPeriod/useFeesBurnedInPeriod.ts
+++ v2/lib/useFeesBurnedInPeriod/useFeesBurnedInPeriod.ts
@@ -1,55 +1,32 @@
 import { useContext } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { providers } from 'ethers';
 import { ContractContext } from '@snx-v2/ContractContext';
 import { wei } from '@synthetixio/wei';
-import { getFeePool } from '@snx-v2/useSynthetixContracts';
-import { NetworkIdByName } from '@synthetixio/contracts-interface';
 import { useDebtShareDataPeriod } from '@snx-v2/useDebtShareDataPeriod';
+import { useFeePeriodMultiNetwork } from '@snx-v2/useFeePeriodMultiNetwork';
 
 export const useFeesBurnedInPeriod = (period = 1 /* Defaults to previous period*/) => {
   const { walletAddress, networkId } = useContext(ContractContext);
-  const { data: debtShareData } = useDebtShareDataPeriod();
+  const { data: debtShareData } = useDebtShareDataPeriod(period);
+  const { data: feePeriods } = useFeePeriodMultiNetwork(period);
   return useQuery({
     queryKey: ['stakingV2', 'useFeesBurnedInPeriod', networkId, walletAddress],
     queryFn: async () => {
-      if (!walletAddress || !debtShareData) throw Error('Query should not be enabled');
+      if (!walletAddress || !debtShareData || !feePeriods) {
+        throw Error('Query should not be enabled');
+      }
 
-      const optimismProvider = new providers.InfuraProvider(
-        NetworkIdByName['mainnet-ovm'],
-        process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
-      );
-      const mainnetProvider = new providers.InfuraProvider(
-        NetworkIdByName.mainnet,
-        process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
-      );
-      const [FeePoolOptimism, FeePoolMainnet] = await Promise.all([
-        getFeePool({
-          provider: optimismProvider,
-          networkId: NetworkIdByName['mainnet-ovm'],
-          signer: null,
-        }),
-        getFeePool({
-          provider: mainnetProvider,
-          networkId: NetworkIdByName.mainnet,
-          signer: null,
-        }),
-      ]);
-
-      const [prevFeePeriodOptimism, prevFeePeriodMainnet] = await Promise.all([
-        FeePoolOptimism.recentFeePeriods(period),
-        FeePoolMainnet.recentFeePeriods(period),
-      ]);
+      const { feePeriodMainnet, feePeriodOptimism } = feePeriods;
 
       const { userDebtSharePercentage } = debtShareData;
-      const totalBurn = wei(prevFeePeriodOptimism.feesToDistribute).add(
-        wei(prevFeePeriodMainnet.feesToDistribute)
+      const totalBurn = wei(feePeriodOptimism.feesToDistribute).add(
+        wei(feePeriodMainnet.feesToDistribute)
       );
 
       return totalBurn.mul(userDebtSharePercentage);
     },
 
-    enabled: Boolean(walletAddress && debtShareData),
+    enabled: Boolean(walletAddress && debtShareData && feePeriods),
     staleTime: 1000,
   });
 };
