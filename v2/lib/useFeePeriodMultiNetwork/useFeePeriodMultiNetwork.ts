dfe9a3a044cc79ed95fc61c29ffe5d856f2ac3a4
joey <joey@cc.snxdao.io>
2023-02-16 15:53:44 -0800

    Network id and wallet address not needed in useFeePeriodMultiNetwork

--- v2/lib/useFeePeriodMultiNetwork/useFeePeriodMultiNetwork.ts
+++ v2/lib/useFeePeriodMultiNetwork/useFeePeriodMultiNetwork.ts
@@ -1,14 +1,11 @@
-import { useContext } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { providers } from 'ethers';
-import { ContractContext } from '@snx-v2/ContractContext';
 import { getFeePool } from '@snx-v2/useSynthetixContracts';
 import { NetworkIdByName } from '@synthetixio/contracts-interface';
 
 export const useFeePeriodMultiNetwork = (period = 1 /* Defaults to previous period*/) => {
-  const { walletAddress, networkId } = useContext(ContractContext);
   return useQuery({
-    queryKey: ['stakingV2', 'useFeePeriodMultiNetwork', networkId, walletAddress],
+    queryKey: ['stakingV2', 'useFeePeriodMultiNetwork'],
     queryFn: async () => {
       const optimismProvider = new providers.InfuraProvider(
         NetworkIdByName['mainnet-ovm'],
@@ -39,6 +36,6 @@ export const useFeePeriodMultiNetwork = (period = 1 /* Defaults to previous peri
     },
 
     enabled: true,
-    staleTime: 1000,
+    staleTime: 10000,
   });
 };
