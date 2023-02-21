87d3f9b20ca2eaa1336b340724cf11386ee92132
joey <joey@cc.snxdao.io>
2023-02-21 14:33:37 -0700

    Remove unused containers

--- v2/ui/containers/index.tsx
+++ v2/ui/containers/index.tsx
@@ -2,21 +2,15 @@ import { FC, ReactNode } from 'react';
 
 import Connector from './Connector';
 import BlockExplorer from './BlockExplorer';
-import TransactionNotifier from './TransactionNotifier';
-import SideNav from './UI';
 
 type WithAppContainersProps = {
   children: ReactNode;
 };
 
 export const WithAppContainers: FC<WithAppContainersProps> = ({ children }) => (
-  <SideNav.Provider>
-    <Connector.Provider>
-      <BlockExplorer.Provider>
-        <TransactionNotifier.Provider>{children}</TransactionNotifier.Provider>
-      </BlockExplorer.Provider>
-    </Connector.Provider>
-  </SideNav.Provider>
+  <Connector.Provider>
+    <BlockExplorer.Provider>{children}</BlockExplorer.Provider>
+  </Connector.Provider>
 );
 
 export default WithAppContainers;
