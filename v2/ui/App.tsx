e886036da87cf5127a04308468de6dc1fb44df02
jmzwar <52280438+jmzwar@users.noreply.github.com>
2023-03-01 16:23:25 -0700

    update links to limo (#749)
    * update links
    
    * Update v2/lib/Constants/links.ts
    
    Co-authored-by: Noisekit <nikita@cc.snxdao.io>
    
    * Update v2/ui/constants/links.ts
    
    Co-authored-by: Noisekit <nikita@cc.snxdao.io>
    
    * Update v2/ui/translations/en.json
    
    Co-authored-by: Noisekit <nikita@cc.snxdao.io>
    
    * Update v2/ui/public/manifest.json
    
    Co-authored-by: Noisekit <nikita@cc.snxdao.io>
    
    ---------
    
    Co-authored-by: jmzwar <james@jmzwar.com>
    Co-authored-by: Noisekit <nikita@cc.snxdao.io>

--- v2/ui/App.tsx
+++ v2/ui/App.tsx
@@ -109,7 +109,7 @@ function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={t('meta.description')} />
         {/* open graph */}
-        <meta property="og:url" content="https://staking.synthetix.io/" />
+        <meta property="og:url" content="https://staking.synthetix.eth.limo/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={t('meta.og.title')} />
         <meta property="og:description" content={t('meta.description')} />
@@ -121,7 +121,7 @@ function App() {
         <meta name="twitter:site" content="@synthetix_io" />
         <meta name="twitter:creator" content="@synthetix_io" />
         <meta name="twitter:image" content="/images/staking-twitter.jpg" />
-        <meta name="twitter:url" content="https://staking.synthetix.io" />
+        <meta name="twitter:url" content="https://staking.synthetix.eth.limo/" />
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <ChakraProviderWithTheme>
