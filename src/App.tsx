9f9172ed2d9ba26fa3080eee9871ffd9f0c59443
joey <joey@cc.snxdao.io>
2023-03-01 18:00:48 -0700

    ☠️ scrolling liquidated

--- src/App.tsx
+++ src/App.tsx
@@ -53,8 +53,8 @@ export function App() {
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Flex height="100vh" maxHeight="100vh" flexDirection="column">
             <Header />
-            <Flex flex="1">
-              <Box flex="5">
+            <Flex flex="1" height="100%" minHeight={0}>
+              <Box flex="5" pb="32px">
                 <Market />
               </Box>
               <Box flex="2" maxHeight="100%">
