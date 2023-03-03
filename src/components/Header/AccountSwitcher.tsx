1dd65ae93b7ee1929d023b458bc3d90b8f740d82
Noah Litvin <noah.litvin@gmail.com>
2023-03-03 13:06:05 -0700

    account switcher

--- src/components/Header/AccountSwitcher.tsx
+++ src/components/Header/AccountSwitcher.tsx
@@ -1,80 +1,99 @@
 import { useAccount } from "wagmi";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
-  Button,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverContent,
-  PopoverTrigger,
-} from "@chakra-ui/react";
-import { TriangleDownIcon } from "@chakra-ui/icons";
-import {
+  perpsMarketAccountProxyABI,
+  perpsMarketAccountProxyAddress,
   usePerpsMarketAccountProxyBalanceOf,
-  usePerpsMarketAccountProxyTokenOfOwnerByIndex,
   usePerpsMarketPerpsMarketProxyCreateAccount,
 } from "../../generated";
 import { ethers } from "ethers";
+import { useContractReads } from "wagmi";
+import { useNavigate, useParams } from "react-router-dom";
+import { useMemo } from "react";
 
 export function AccountSwitcher() {
   const { isConnected, address } = useAccount();
 
-  let firstAccountId;
-  let writeCreateAccount;
+  const { accountId: selectedAccountId, marketId } = useParams();
+  const navigate = useNavigate();
 
-  if (isConnected) {
-    // Set up Create Account functionality
+  const { isLoading: acccountCountisLoading, data: acccountCountData } =
+    usePerpsMarketAccountProxyBalanceOf({
+      args: [address || "0x"],
+      enabled: Boolean(isConnected && address),
+    });
+
+  const { data: accountIdsData, isLoading: accountIdsIsLoading } =
+    useContractReads({
+      onSuccess: () => {
+        if (!accountIdsData || accountIdsData.length == 0) {
+          return;
+        }
+        if (!selectedAccountId && accountIdsData[0]) {
+          navigate(`/markets/${marketId}/${accountIdsData[0]}`);
+        }
+      },
+      enabled: Boolean(!acccountCountisLoading && acccountCountData),
+      contracts: Array.from(Array(acccountCountData?.toNumber()).keys())?.map(
+        (ind) => {
+          return {
+            address: perpsMarketAccountProxyAddress,
+            abi: perpsMarketAccountProxyABI,
+            functionName: "tokenOfOwnerByIndex",
+            args: [ind],
+          };
+        },
+      ),
+    });
+
+  const newId = useMemo(() => {
     const randomNumber = Math.floor(Math.random() * 10000) + 1;
-    const newId = ethers.BigNumber.from(randomNumber.toString());
-    const { write } = usePerpsMarketPerpsMarketProxyCreateAccount({
+    return ethers.BigNumber.from(randomNumber.toString());
+  }, []);
+
+  const { writeAsync: writeCreateAccount, isLoading: createAccountIsLoading } =
+    usePerpsMarketPerpsMarketProxyCreateAccount({
       args: [newId],
       mode: "recklesslyUnprepared",
     });
-    writeCreateAccount = write;
 
-    if (address) {
-      // Gather Account IDs associated with the connected wallet.
-      const {
-        isSuccess: isSuccessAccountTokensBalanceOf,
-        data: accountTokensBalanceOfData,
-      } = usePerpsMarketAccountProxyBalanceOf({ args: [address] });
-      if (
-        isSuccessAccountTokensBalanceOf &&
-        accountTokensBalanceOfData?.gt(0)
-      ) {
-        const {
-          isSuccess: tokenOfOwnerByIndexSuccess,
-          data: tokenOfOwnerByIndexData,
-        } = usePerpsMarketAccountProxyTokenOfOwnerByIndex({
-          args: [address, ethers.BigNumber.from("1")], // this should loop with accountTokensBalanceOfData iterations
-        });
-        if (tokenOfOwnerByIndexSuccess) {
-          firstAccountId = tokenOfOwnerByIndexData;
-        }
-      }
-    }
+  if (!isConnected) {
+    return null;
   }
 
-  return isConnected ? (
-    firstAccountId ? (
-      <Popover>
-        <PopoverTrigger>
-          <Button rightIcon={<TriangleDownIcon />} colorScheme="gray">
-            Account #{firstAccountId.toString()}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverBody>Account list + create account button</PopoverBody>
-        </PopoverContent>
-      </Popover>
-    ) : (
-      <Button
-        colorScheme="cyan"
-        disabled={!writeCreateAccount}
-        onClick={() => writeCreateAccount?.()}
-      >
-        Create Account
-      </Button>
-    )
-  ) : null;
+  const createAccount = async () => {
+    const transactionResponse = await writeCreateAccount();
+    await transactionResponse.wait();
+    navigate(`/markets/${marketId}/${newId}`);
+  };
+
+  return selectedAccountId ? (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+        Account #{selectedAccountId}
+      </MenuButton>
+      <MenuList>
+        {accountIdsData?.map((accountId) => (
+          <MenuItem
+            key={accountId as string}
+            onClick={() => navigate(`/markets/${marketId}/${accountId}`)}
+          >
+            <>Account #{accountId}</>
+          </MenuItem>
+        ))}
+        <MenuItem key={accountIdsData?.length} onClick={createAccount}>
+          Create New Account
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  ) : (
+    <Button
+      colorScheme="cyan"
+      isDisabled={!writeCreateAccount}
+      onClick={createAccount}
+    >
+      Create Account
+    </Button>
+  );
 }
