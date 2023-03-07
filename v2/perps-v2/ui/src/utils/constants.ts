48b8fc1b80dfe317f44f52e5997e2d3c997c94b1
max <fritzschoff@icloud.com>
2023-03-07 17:12:58 +0000

    reset to default api

--- v2/perps-v2/ui/src/utils/constants.ts
+++ v2/perps-v2/ui/src/utils/constants.ts
@@ -1,4 +1,3 @@
 export const OPTIMISM_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/kwenta/optimism-perps';
 export const PERPS_V2_DASHBOARD_GRAPH_URL =
-  'https://api.thegraph.com/subgraphs/id/QmNyooR2eQ2ZjeTqyKvaLrwmCj7HjWmw2Zqb8bufEfPN3o';
-// 'https://api.thegraph.com/subgraphs/name/fritzschoff/perps-dashboard';
+  'https://api.thegraph.com/subgraphs/name/fritzschoff/perps-dashboard';
