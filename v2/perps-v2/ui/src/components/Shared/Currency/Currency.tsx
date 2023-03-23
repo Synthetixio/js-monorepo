91644fe173af2e35d818bc0ccff2e93646093aa5
joey <joey@cc.snxdao.io>
2023-03-24 10:28:36 +1100

    More decimals

--- v2/perps-v2/ui/src/components/Shared/Currency/Currency.tsx
+++ v2/perps-v2/ui/src/components/Shared/Currency/Currency.tsx
@@ -3,12 +3,15 @@ import { formatNumberToUsd } from '@snx-v2/formatters';
 
 interface CurrencyProps {
   amount: number | null;
+  decimals?: number;
 }
 
-export const Currency = ({ amount }: CurrencyProps) => {
+export const Currency = ({ amount, decimals = 4 }: CurrencyProps) => {
   return (
     <Td border="none" fontSize="14px" lineHeight="20px" fontFamily="heading" fontWeight={500}>
-      <Fade in>{amount ? formatNumberToUsd(amount) : '-'}</Fade>
+      <Fade in>
+        {amount ? formatNumberToUsd(amount, { minimumFractionDigits: decimals }) : '-'}
+      </Fade>
     </Td>
   );
 };
