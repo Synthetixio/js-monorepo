00a22a880cfbe21e208500ccb354c0e87320b07b
Joey <5688912+bachstatter@users.noreply.github.com>
2023-04-04 12:06:49 +1000

    Fix leverage and add tests (#804)
    Goerli synced fine with this change

--- v2/perps-v2/perps-subgraph/tests/calculations.test.ts
+++ v2/perps-v2/perps-subgraph/tests/calculations.test.ts
@@ -1,5 +1,5 @@
 import { assert, describe, test } from 'matchstick-as';
-import { calculateAccruedFunding } from '../src/calculations';
+import { calculateAccruedFunding, calculateLeverage } from '../src/calculations';
 import { BigInt } from '@graphprotocol/graph-ts';
 
 describe('calculations', () => {
@@ -31,6 +31,43 @@ describe('calculations', () => {
       const size = BigInt.fromI32(3).times(BigInt.fromI32(10).pow(18));
       const result = calculateAccruedFunding(pastFunding, currentFunding, size);
       assert.bigIntEquals(result, BigInt.fromI32(30));
+    });
+  });
+  describe('calculateLeverage', () => {
+    test('should return 0 when size is 0', () => {
+      const size = BigInt.fromI32(0);
+      const lastPrice = BigInt.fromI32(100);
+      const margin = BigInt.fromI32(10);
+
+      const result = calculateLeverage(size, lastPrice, margin);
+      assert.bigIntEquals(result, BigInt.fromI32(0));
+    });
+
+    test('should calculate leverage correctly for positive size', () => {
+      const size = BigInt.fromI32(5);
+      const lastPrice = BigInt.fromI32(200);
+      const margin = BigInt.fromI32(10);
+
+      const result = calculateLeverage(size, lastPrice, margin);
+      assert.bigIntEquals(result, BigInt.fromI32(100));
+    });
+
+    test('should calculate leverage correctly for negative size', () => {
+      const size = BigInt.fromI32(-5);
+      const lastPrice = BigInt.fromI32(200);
+      const margin = BigInt.fromI32(10);
+
+      const result = calculateLeverage(size, lastPrice, margin);
+      assert.bigIntEquals(result, BigInt.fromI32(100));
+    });
+
+    test('should handle large values correctly', () => {
+      const size = BigInt.fromString('1000000000000000000');
+      const lastPrice = BigInt.fromString('50000000000000000000');
+      const margin = BigInt.fromString('10000000000000000000');
+
+      const result = calculateLeverage(size, lastPrice, margin);
+      assert.bigIntEquals(result, BigInt.fromString('5000000000000000000'));
     });
   });
 });
