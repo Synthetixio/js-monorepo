5e39c2079edfc7872074b1fdf86193008cdae730
Noisekit <nikita@cc.snxdao.io>
2023-04-28 12:16:22 +0800

    Bootstrap @snx-v3/spot-markets-ui (#832)

---
+++ v3/spot-markets/ui/README.md
@@ -0,0 +1,1 @@
+# V3 Spot Markets UI
---
+++ v3/spot-markets/ui/babel.config.js
@@ -0,0 +1,42 @@
+require.resolve('core-js');
+require.resolve('@babel/runtime-corejs3/core-js/date/now');
+
+module.exports = {
+  presets: [
+    require.resolve('@babel/preset-typescript'),
+    [require.resolve('@babel/preset-react'), { runtime: 'automatic' }],
+  ],
+
+  plugins: [[require.resolve('@babel/plugin-transform-runtime'), { corejs: 3 }]],
+
+  env: {
+    production: {
+      presets: [
+        [
+          require.resolve('@babel/preset-env'),
+          {
+            useBuiltIns: 'usage',
+            corejs: { version: 3, proposals: true },
+            modules: false,
+            targets: {
+              browsers: require('./package.json').browserslist,
+            },
+          },
+        ],
+      ],
+    },
+
+    development: {
+      presets: [
+        [
+          require.resolve('@babel/preset-env'),
+          {
+            modules: false,
+            targets: { browsers: ['last 1 Chrome version'] },
+          },
+        ],
+      ],
+      plugins: [require.resolve('react-refresh/babel')],
+    },
+  },
+};
