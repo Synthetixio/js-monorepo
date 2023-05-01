ed8b401a62a28acb7f3f5a17e856b8cdf64605b6
Noisekit <nikita@cc.snxdao.io>
2023-05-01 16:17:26 +0800

    Fix contract import for setConfig

--- v3/cypress/cypress/tasks/setConfig.js
+++ v3/cypress/cypress/tasks/setConfig.js
@@ -1,42 +1,42 @@
 import { ethers } from 'ethers';
-import * as CoreProxy from '@synthetixio/v3-contracts/src/goerli/CoreProxy';
+import { importCoreProxy } from './importCoreProxy';
 import { setEthBalance } from './setEthBalance';
 
 export async function setConfig({ key, value }) {
+  const CoreProxy = await importCoreProxy();
   const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
 
   const coreProxy = new ethers.Contract(CoreProxy.address, CoreProxy.abi, provider);
 
-  try {
-    const owner = await coreProxy.owner();
+  const owner = await coreProxy.owner();
 
-    const signer = provider.getSigner(owner);
-    console.log('setConfig', { owner });
+  const signer = provider.getSigner(owner);
+  console.log('setConfig', { owner });
 
-    await setEthBalance({ address: owner, balance: 1000 });
+  await setEthBalance({ address: owner, balance: 1000 });
 
-    await provider.send('anvil_impersonateAccount', [owner]);
-    const oldValue = ethers.utils.parseBytes32String(
-      await coreProxy.connect(signer).getConfig(ethers.utils.formatBytes32String(key))
-    );
-    console.log('setConfig', { key, oldValue });
+  await provider.send('anvil_impersonateAccount', [owner]);
+  console.log(
+    `await coreProxy.connect(signer).getConfig(ethers.utils.formatBytes32String(key))`,
+    await coreProxy.connect(signer).getConfig(ethers.utils.formatBytes32String(key))
+  );
+  const oldValue = parseInt(
+    await coreProxy.connect(signer).getConfig(ethers.utils.formatBytes32String(key)),
+    16
+  );
+  console.log('setConfig', { key, oldValue });
 
-    const tx = await coreProxy
-      .connect(signer)
-      .setConfig(
-        ethers.utils.formatBytes32String(key),
-        ethers.utils.formatBytes32String(`${value}`)
-      );
-    await tx.wait();
+  const tx = await coreProxy
+    .connect(signer)
+    .setConfig(ethers.utils.formatBytes32String(key), ethers.utils.formatBytes32String(`${value}`));
+  await tx.wait();
 
-    const newValue = ethers.utils.parseBytes32String(
-      await coreProxy.connect(signer).getConfig(ethers.utils.formatBytes32String(key))
-    );
-    console.log('setConfig', { key, newValue });
-    await provider.send('anvil_stopImpersonatingAccount', [owner]);
-  } catch (err) {
-    console.log('setConfig', err);
-  }
+  const newValue = parseInt(
+    await coreProxy.connect(signer).getConfig(ethers.utils.formatBytes32String(key)),
+    16
+  );
+  console.log('setConfig', { key, newValue });
+  await provider.send('anvil_stopImpersonatingAccount', [owner]);
 
   return null;
 }
