f78182b845764129682d2d340d496139d060548d
Noisekit <nikita@cc.snxdao.io>
2023-05-02 10:19:57 +0800

    Spot Markets prototype (#836)

--- v3/spot-markets/ui/prototype/src/components/Header/index.ts
+++
@@ -1,1 +0,0 @@
-export { AccountSwitcher } from "./AccountSwitcher";
--- v3/spot-markets/ui/src/pages/SpotMarket.tsx
+++ v3/spot-markets/ui/src/pages/SpotMarket.tsx
@@ -4,95 +4,65 @@ import {
   Flex,
   FormControl,
   FormLabel,
-  InputRightElement,
   Heading,
   Input,
   InputGroup,
-  Stat,
-  StatGroup,
-  StatLabel,
-  StatNumber,
+  InputRightElement,
   VStack,
-  Code,
-} from "@chakra-ui/react";
-import { Header } from "../../components";
-import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
-import {
-  useSpotMarketInfo,
-  useSpotMarketStat,
-} from "../../hooks/spot/useSpotMarketInfo";
-import { useParams } from "react-router-dom";
-import { spotMarkets } from "../../constants/markets";
-import { useTokenInfo } from "../../hooks/useTokenInfo";
+} from '@chakra-ui/react';
+import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
+import { useSpotMarketInfo, useSpotMarketStat } from '../hooks/useSpotMarketInfo';
+import { useParams } from 'react-router-dom';
+import { spotMarkets, defaultSpotMarket } from '../constants/markets';
+// import { useTokenInfo } from '../hooks/useTokenInfo';
 
 export function SpotMarket() {
   const { marketId } = useParams();
-  const id = spotMarkets[marketId?.toUpperCase() || ""].marketId;
-  const { synthAddress, marketName } = useSpotMarketInfo(id);
-  const { reportedDebt } = useSpotMarketStat(id);
-  const { symbol, decimals, name } = useTokenInfo(synthAddress);
+  const id =
+    marketId && marketId in spotMarkets
+      ? spotMarkets[marketId].marketId
+      : defaultSpotMarket.marketId;
+  const { data: spotMarketInfo } = useSpotMarketInfo(id);
+  const { data: spotMarketStat } = useSpotMarketStat(id);
+  // const { symbol, decimals, name } = useTokenInfo(synthAddress);
 
-  if (!id || !synthAddress) {
+  if (!id || !spotMarketInfo || !spotMarketStat) {
     return null;
   }
+
+  const { synthAddress, marketName } = spotMarketInfo;
+  const { reportedDebt } = spotMarketStat;
 
   return (
     <Flex height="100vh" maxHeight="100vh" flexDirection="column">
       <>
-        <Header isSpot />
         <Flex flex="1" height="100%" minHeight={0}>
-          <Box
-            borderRight="1px solid rgba(255,255,255,0.2)"
-            flex="2"
-            maxHeight="100%"
-          >
+          <Box borderRight="1px solid rgba(255,255,255,0.2)" flex="2" maxHeight="100%">
             <Box borderBottom="1px solid rgba(255,255,255,0.2)" p="4">
               <Heading size="md">ETH (Market Switcher)</Heading>
             </Box>
             <Box borderBottom="1px solid rgba(255,255,255,0.2)" p="4">
-              <div key="form" style={{ width: "100%" }}>
+              <div key="form" style={{ width: '100%' }}>
                 <VStack spacing={4} align="flex-start" w="100%">
                   <FormLabel htmlFor="amount">Order Type</FormLabel>
                   <Flex direction="row" width="100%" gap="4">
-                    <Button
-                      colorScheme={true ? "green" : "gray"}
-                      width="100%"
-                      mr="1"
-                    >
+                    <Button colorScheme={true ? 'green' : 'gray'} width="100%" mr="1">
                       Buy
                     </Button>
-                    <Button
-                      colorScheme={false ? "green" : "gray"}
-                      width="100%"
-                      mr="1"
-                    >
+                    <Button colorScheme={false ? 'green' : 'gray'} width="100%" mr="1">
                       Sell
                     </Button>
-                    <Button
-                      colorScheme={false ? "green" : "gray"}
-                      width="100%"
-                      mr="1"
-                    >
+                    <Button colorScheme={false ? 'green' : 'gray'} width="100%" mr="1">
                       Wrap
                     </Button>
-                    <Button
-                      colorScheme={false ? "gray" : "gray"}
-                      width="100%"
-                      ml="1"
-                    >
+                    <Button colorScheme={false ? 'gray' : 'gray'} width="100%" ml="1">
                       Unwrap
                     </Button>
                   </Flex>
                   <FormControl key="amount">
                     <FormLabel htmlFor="amount">Amount</FormLabel>
                     <InputGroup>
-                      <Input
-                        id="amount"
-                        name="amount"
-                        type="number"
-                        variant="filled"
-                        value={""}
-                      />
+                      <Input id="amount" name="amount" type="number" variant="filled" value="" />
                       <InputRightElement width="6rem">snxUSD</InputRightElement>
                     </InputGroup>
                   </FormControl>
@@ -102,7 +72,7 @@ export function SpotMarket() {
                     key="button"
                     type="submit"
                     size="lg"
-                    colorScheme={true ? "green" : "red"}
+                    colorScheme={true ? 'green' : 'red'}
                     width="full"
                   >
                     Submit
@@ -132,9 +102,9 @@ export function SpotMarket() {
 
               <Box mb="2">
                 <Heading size="xs">snxETH Issued</Heading>
-                {reportedDebt.toNumber().toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
+                {reportedDebt.toNumber().toLocaleString('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
                 })}
               </Box>
 
@@ -156,11 +126,7 @@ export function SpotMarket() {
           </Box>
           <Box flex="5" pb="32px">
             <Flex direction="column" height="100%" width="100%">
-              <AdvancedRealTimeChart
-                theme="dark"
-                autosize
-                symbol={"ETHUSD"}
-              ></AdvancedRealTimeChart>
+              <AdvancedRealTimeChart theme="dark" autosize symbol="ETHUSD"></AdvancedRealTimeChart>
             </Flex>
           </Box>
         </Flex>
