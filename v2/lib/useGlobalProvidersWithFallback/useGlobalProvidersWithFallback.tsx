53374e8ea9ea3583ca8a268f1685a87d3462e5ef
joey <joey@cc.snxdao.io>
2023-05-04 14:01:55 +1000

    Fix circular deps

--- v2/lib/useGlobalProvidersWithFallback/useGlobalProvidersWithFallback.tsx
+++ v2/lib/useGlobalProvidersWithFallback/useGlobalProvidersWithFallback.tsx
@@ -1,29 +1,18 @@
-import { NetworkIdByName } from '@snx-v2/useSynthetixContracts';
 import { providers } from 'ethers';
 import { useState } from 'react';
 
+const mainnetId = 1;
+const optimismId = 10;
 export const useGlobalProvidersWithFallback = () => {
   const [useInfura, setUseInfura] = useState(true);
 
   const globalProviders = {
     mainnet: useInfura
-      ? new providers.InfuraProvider(
-          NetworkIdByName.mainnet,
-          process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
-        )
-      : new providers.AlchemyProvider(
-          NetworkIdByName.mainnet,
-          process.env?.NEXT_PUBLIC_ALCHEMY_MAINNET_KEY
-        ),
+      ? new providers.InfuraProvider(mainnetId, process.env.NEXT_PUBLIC_INFURA_PROJECT_ID)
+      : new providers.AlchemyProvider(mainnetId, process.env?.NEXT_PUBLIC_ALCHEMY_MAINNET_KEY),
     optimism: useInfura
-      ? new providers.InfuraProvider(
-          NetworkIdByName['mainnet-ovm'],
-          process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
-        )
-      : new providers.AlchemyProvider(
-          NetworkIdByName['mainnet-ovm'],
-          process.env?.NEXT_PUBLIC_ALCHEMY_MAINNET_KEY
-        ),
+      ? new providers.InfuraProvider(optimismId, process.env.NEXT_PUBLIC_INFURA_PROJECT_ID)
+      : new providers.AlchemyProvider(optimismId, process.env?.NEXT_PUBLIC_ALCHEMY_MAINNET_KEY),
   };
 
   return {
