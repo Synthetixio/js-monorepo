5d9fc8add8e99f83bf9219c96eb977d2a927ed2d
Noisekit <nikita@cc.snxdao.io>
2023-05-08 11:26:49 +0800

    v3 staking cypress sandbox

---
+++ v3/cypress/cypress/e2e/Home - Connected.e2e.js
@@ -0,0 +1,10 @@
+it('shows homepage to a connected wallet', () => {
+  cy.connectWallet().then(({ address }) => {
+    cy.task('setEthBalance', { address, balance: 100 });
+    cy.task('getSnx', { address, amount: 20 });
+  });
+  cy.viewport(1100, 900);
+
+  cy.visit('http://localhost:3000');
+  cy.get('#app').should('contain', 'Welcome to Synthetix V3');
+});
