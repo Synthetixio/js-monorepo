88d36edbe14f1089fc05f702c84e40081844bf38
jmzwar <james@jmzwar.com>
2023-05-22 10:08:01 +1000

    markets fix + move column

--- v2/perps-v2/ui/src/components/Markets/MarketsTable.tsx
+++ v2/perps-v2/ui/src/components/Markets/MarketsTable.tsx
@@ -2,12 +2,12 @@ import { TableContainer, Table, Thead, Tr, Tbody, Flex, Text } from '@chakra-ui/
 import { MarketsTableLoading } from './MarketsTableLoading';
 import { useMarkets } from '../../hooks';
 import {
-  Currency,
+  // Currency,
+  DailyVolumeChange,
   Funding,
   Market,
   MarkPrice,
   OpenInterest,
-  PercentageChange,
   PremiumDiscount,
   Skew,
   TableHeaderCell,
@@ -16,7 +16,7 @@ import { wei } from '@synthetixio/wei';
 import { calculateSkew } from '../../utils';
 
 export const MarketsTable = () => {
-  const { data, loading } = useMarkets();
+  const { data, loading, error } = useMarkets();
 
   return (
     <>
@@ -37,7 +37,6 @@ export const MarketsTable = () => {
               <TableHeaderCell>Market</TableHeaderCell>
               <TableHeaderCell>Mark Price</TableHeaderCell>
               <TableHeaderCell>Premium/Discount</TableHeaderCell>
-              <TableHeaderCell>24H Change</TableHeaderCell>
               <TableHeaderCell>1H Funding Rate</TableHeaderCell>
               <TableHeaderCell>Open Interest</TableHeaderCell>
               <TableHeaderCell>Skew</TableHeaderCell>
@@ -71,7 +70,6 @@ export const MarketsTable = () => {
                   <Market asset={asset} leverage={null} isPosition={false} />
                   <MarkPrice markPrice={markPrice.toNumber()} indexPrice={indexPrice.toNumber()} />
                   <PremiumDiscount amount={skewPercent.toNumber()} />
-                  <PercentageChange amount={percentageDifference.toNumber()} />
                   <Funding withDollar={false} amount={100 * fundingRate.toNumber()} />
                   <OpenInterest
                     long={long.toNumber()}
@@ -79,19 +77,25 @@ export const MarketsTable = () => {
                     price={markPrice.toNumber()}
                   />
                   <Skew skew={skewValue} />
-                  <Currency amount={wei(volume, 18, true).toNumber()} decimals={2} />
+                  <DailyVolumeChange
+                    volume={wei(volume, 18, true).toNumber()}
+                    percentage={percentageDifference.toNumber()}
+                  />
+                  {/* <Currency amount={wei(volume, 18, true).toNumber()} decimals={2} /> */}
                 </Tr>
               );
             })}
           </Tbody>
         </Table>
-        {!loading && data?.length === 0 && (
-          <Flex width="100%" justifyContent="center" bg="navy.700" borderTopWidth="1px">
-            <Text fontFamily="inter" fontWeight="500" fontSize="14px" color="gray.500" m={6}>
-              No actions
-            </Text>
-          </Flex>
-        )}
+        <>
+          {((!loading && data?.length === 0) || error) && (
+            <Flex width="100%" justifyContent="center" bg="navy.700" borderTopWidth="1px">
+              <Text fontFamily="inter" fontWeight="500" fontSize="14px" color="gray.500" m={6}>
+                No market data
+              </Text>
+            </Flex>
+          )}
+        </>
       </TableContainer>
     </>
   );
