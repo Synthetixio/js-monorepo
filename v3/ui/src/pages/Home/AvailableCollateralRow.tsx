865d0fc9dc78d63c0c01481903dfef7b83c07b71
Noisekit <nikita@cc.snxdao.io>
2023-05-24 14:37:22 +0800

    Wire up collateral withdrawals and error handling

--- v3/ui/src/pages/Home/AvailableCollateralRow.tsx
+++ v3/ui/src/pages/Home/AvailableCollateralRow.tsx
@@ -1,8 +1,22 @@
 import { Amount } from '@snx-v3/Amount';
-import { Button, Flex, Td, Text, Tr } from '@chakra-ui/react';
+import { Button, Flex, Td, Text, Tr, useToast } from '@chakra-ui/react';
 import { CollateralIcon } from '@snx-v3/icons';
+import { useWithdraw } from '@snx-v3/useWithdraw';
+import { useParams } from '@snx-v3/useParams';
+import { useCollateralType } from '@snx-v3/useCollateralTypes';
+import { useContractErrorParser } from '@snx-v3/useContractErrorParser';
+import { useCoreProxy } from '@snx-v3/useCoreProxy';
+import { ContractError } from '@snx-v3/ContractError';
 
-function AvailableCollateralRowUi({ accountCollateral }: { accountCollateral: any }) {
+function AvailableCollateralRowUi({
+  accountCollateral,
+  isWithdrawLoading,
+  onWithdraw,
+}: {
+  accountCollateral: any;
+  isWithdrawLoading: boolean;
+  onWithdraw: () => void;
+}) {
   return (
     <Tr>
       <Td>
@@ -16,12 +30,52 @@ function AvailableCollateralRowUi({ accountCollateral }: { accountCollateral: an
         </Flex>
       </Td>
       <Td textAlign="end">
-        <Button isDisabled={true}>Withdraw</Button>
+        <Button isDisabled={isWithdrawLoading} onClick={onWithdraw}>
+          Withdraw
+        </Button>
       </Td>
     </Tr>
   );
 }
 
 export function AvailableCollateralRow({ accountCollateral }: { accountCollateral: any }) {
-  return <AvailableCollateralRowUi accountCollateral={accountCollateral} />;
+  const params = useParams();
+  const collateralType = useCollateralType(params.collateralSymbol);
+
+  const { data: CoreProxy } = useCoreProxy();
+  const errorParserCoreProxy = useContractErrorParser(CoreProxy);
+  const toast = useToast({ isClosable: true, duration: 9000 });
+
+  const withdraw = useWithdraw({
+    accountId: params.accountId,
+    collateralTypeAddress: collateralType?.tokenAddress,
+  });
+  const onWithdraw = async () => {
+    try {
+      await withdraw.exec();
+    } catch (error: any) {
+      const contractError = errorParserCoreProxy(error);
+      if (contractError) {
+        console.error(new Error(contractError.name), contractError);
+      }
+      toast.closeAll();
+      toast({
+        title: 'Withdraw failed',
+        description: contractError ? (
+          <ContractError contractError={contractError} />
+        ) : (
+          'Please try again.'
+        ),
+        status: 'error',
+      });
+    }
+  };
+
+  return (
+    <AvailableCollateralRowUi
+      accountCollateral={accountCollateral}
+      onWithdraw={onWithdraw}
+      isWithdrawLoading={withdraw.isLoading}
+    />
+  );
 }
