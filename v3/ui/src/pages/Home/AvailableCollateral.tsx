0880b2f4d8025e21177709e22937813ced2fb681
Noisekit <nikita@cc.snxdao.io>
2023-05-26 12:42:31 +0800

    Show green alert when you can withdraw collateral

--- v3/ui/src/pages/Home/AvailableCollateral.tsx
+++ v3/ui/src/pages/Home/AvailableCollateral.tsx
@@ -13,9 +13,8 @@ import {
   Thead,
   Tr,
 } from '@chakra-ui/react';
-import { CollateralType, useCollateralTypes } from '@snx-v3/useCollateralTypes';
 import { useParams } from '@snx-v3/useParams';
-import { useAccountCollateral } from '@snx-v3/useAccountCollateral';
+import { AccountCollateralType, useAccountCollateral } from '@snx-v3/useAccountCollateral';
 import { useAccountCollateralUnlockDate } from '@snx-v3/useAccountCollateralUnlockDate';
 import { AvailableCollateralRow } from './AvailableCollateralRow';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
@@ -23,16 +22,14 @@ import intlFormat from 'date-fns/intlFormat';
 import { BorderBox } from '@snx-v3/BorderBox';
 
 export function AvailableCollateralUi({
-  isLoading,
   accountCollaterals,
   timeToUnlock,
   unlockDate,
   AvailableCollateralRow,
 }: {
-  isLoading: boolean;
-  accountCollaterals: [];
-  timeToUnlock: string;
-  unlockDate: string;
+  accountCollaterals: AccountCollateralType[];
+  timeToUnlock?: string;
+  unlockDate?: string;
   AvailableCollateralRow: FC<{
     accountCollateral: any;
   }>;
@@ -52,11 +49,18 @@ export function AvailableCollateralUi({
           This collateral can be deposited to pools. As a security precaution, this collateral
           cannot be withdrawn until at least 1 day has elapsed since previous account activity.
         </Text>
-        <Alert ml="auto" status="error" width="540px" title={unlockDate}>
+        <Alert
+          ml="auto"
+          status={timeToUnlock ? 'error' : 'success'}
+          width="540px"
+          title={unlockDate}
+        >
           <AlertIcon />
           <Box width="100%">
-            <AlertTitle>Withdrawals available:</AlertTitle>
-            <AlertDescription display="block">{timeToUnlock}</AlertDescription>
+            <AlertTitle>Withdrawals available</AlertTitle>
+            {timeToUnlock ? (
+              <AlertDescription display="block">{timeToUnlock}</AlertDescription>
+            ) : null}
           </Box>
         </Alert>
       </Flex>
@@ -84,36 +88,46 @@ export function AvailableCollateral() {
   const accountCollaterals = useAccountCollateral({ accountId });
   const accountCollateralUnlockDate = useAccountCollateralUnlockDate({ accountId });
 
-  const formatTimeToUnlock = React.useCallback(
-    () =>
-      accountCollateralUnlockDate.data && accountCollateralUnlockDate.data.getTime() > Date.now()
-        ? formatDistanceToNow(accountCollateralUnlockDate.data, { addSuffix: true })
-        : '~',
-    [accountCollateralUnlockDate.data]
-  );
+  const formatTimeToUnlock = React.useCallback(() => {
+    if (accountCollateralUnlockDate.isLoading) {
+      return '~';
+    }
+    if (
+      !accountCollateralUnlockDate.data ||
+      accountCollateralUnlockDate.data.getTime() <= Date.now()
+    ) {
+      return undefined;
+    }
+    return formatDistanceToNow(accountCollateralUnlockDate.data, { addSuffix: true });
+  }, [accountCollateralUnlockDate.data, accountCollateralUnlockDate.isLoading]);
+
   const [timeToUnlock, setTimeToUnlock] = React.useState(formatTimeToUnlock());
   React.useEffect(() => {
     const interval = setInterval(() => setTimeToUnlock(formatTimeToUnlock()), 1_000);
     return () => clearInterval(interval);
   }, [formatTimeToUnlock]);
 
-  const unlockDate = React.useMemo(
-    () =>
-      accountCollateralUnlockDate.data
-        ? intlFormat(accountCollateralUnlockDate.data, {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-          })
-        : '~',
-    [accountCollateralUnlockDate.data]
-  );
+  const unlockDate = React.useMemo(() => {
+    if (accountCollateralUnlockDate.isLoading) {
+      return '~';
+    }
+    if (
+      !accountCollateralUnlockDate.data ||
+      accountCollateralUnlockDate.data.getTime() <= Date.now()
+    ) {
+      return undefined;
+    }
+    return intlFormat(accountCollateralUnlockDate.data, {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+  }, [accountCollateralUnlockDate.data, accountCollateralUnlockDate.isLoading]);
 
   return (
     <AvailableCollateralUi
-      isLoading={accountCollaterals.isLoading || accountCollateralUnlockDate.isLoading}
       accountCollaterals={accountCollaterals.data || []}
       timeToUnlock={timeToUnlock}
       unlockDate={unlockDate}
