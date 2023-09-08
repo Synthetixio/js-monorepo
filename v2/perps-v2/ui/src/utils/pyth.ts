682b04b682d5f030d69dbf48ebb640e50387e6b4
joey <joey@cc.snxdao.io>
2023-09-08 11:21:24 +1000

    Create getMarketsPythConfig and useMarketSummaries instead of hardcoding

--- v2/perps-v2/ui/src/utils/pyth.ts
+++ v2/perps-v2/ui/src/utils/pyth.ts
@@ -1,3 +1,52 @@
+import { z } from 'zod';
+import { isStaging } from './isStaging';
 import { EvmPriceServiceConnection } from '@pythnetwork/pyth-evm-js';
 
 export const pyth = new EvmPriceServiceConnection('https://xc-mainnet.pyth.network');
+
+const OffchainFeedSchema = z.array(
+  z.object({
+    kind: z.literal('pyth'),
+    oracle: z.string(),
+    asset: z.string(),
+    feedId: z.string(),
+  })
+);
+export type MarketsByKey = Record<
+  string,
+  {
+    key: string;
+    pythId: string;
+    asset: string;
+  }
+>;
+const formatAssetToPerpName = (x: string) => {
+  if (x === 'sETH' || x === 'sBTC') {
+    return `${x}PERP`;
+  }
+  return `s${x}PERP`;
+};
+
+export const getMarketsPythConfig = () => {
+  const network = isStaging ? 'goerli-ovm' : 'mainnet-ovm';
+  const url = `https://raw.githubusercontent.com/Synthetixio/synthetix/master/publish/deployed/${network}/offchain-feeds.json`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      const parsedJson = OffchainFeedSchema.parse(json);
+      return parsedJson
+        .map(({ feedId, asset }) => {
+          const key = formatAssetToPerpName(asset);
+          return {
+            key,
+            pythId: feedId,
+            asset,
+          };
+        })
+        .reduce((acc: MarketsByKey, val) => {
+          acc[val.key] = val;
+          return acc;
+        }, {});
+    });
+};
