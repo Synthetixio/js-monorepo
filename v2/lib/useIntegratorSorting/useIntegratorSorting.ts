de79777179fd10a5e352941961b9c466e00dff92
jz - synthetix <52280438+jmzwar@users.noreply.github.com>
2023-09-11 23:59:02 +0200

    call synthetixio directly (#956)

--- v2/lib/useIntegratorSorting/useIntegratorSorting.ts
+++ v2/lib/useIntegratorSorting/useIntegratorSorting.ts
@@ -1,17 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
-const INTEGRATORS_VOLUME_URL = 'https://api.dune.com/api/v1/query/2647536/results';
-const apiKey = process?.env?.NEXT_PUBLIC_DUNE_API_KEY || '';
+const INTEGRATORS_VOLUME_URL = 'https://synthetix.io/api/integrator';
 const ONE_DAY_MS = 1000 * 60 * 60 * 24;
 
 type IntegratorsVolumeResponse = {
   result: {
-    rows: {
-      daily_fee: number;
-      day: string;
-      integrator_cum_fee: number;
-      tracking_code: string | null;
-    }[];
+    integratorForLatestDate: string[];
   };
 };
 
@@ -19,18 +13,11 @@ export const useIntegratorSorting = () => {
   return useQuery({
     queryKey: ['useIntegratorSorting'],
     queryFn: async () => {
-      const resp = await fetch(INTEGRATORS_VOLUME_URL, {
-        headers: { 'x-dune-api-key': apiKey },
-      });
-      const json: IntegratorsVolumeResponse = await resp.json();
-      const integratorsVolume = json.result.rows.sort((a, b) => (b.day > a.day ? 1 : -1));
+      const resp: IntegratorsVolumeResponse = await (await fetch(INTEGRATORS_VOLUME_URL)).json();
 
-      // Get the integrator volume for the latest date
-      // and sort by daily fee
-      return integratorsVolume
-        .filter((item) => item.day === integratorsVolume[0].day)
-        .sort((a, b) => (a.daily_fee > b.daily_fee ? -1 : 1))
-        .map((item) => item.tracking_code?.split('\x00')[0].toLowerCase() || '');
+      return resp.result.integratorForLatestDate.map((integrator: string) =>
+        integrator.toLowerCase()
+      );
     },
     staleTime: ONE_DAY_MS,
   });
