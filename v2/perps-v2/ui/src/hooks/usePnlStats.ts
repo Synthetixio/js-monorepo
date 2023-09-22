bec7cbf63fbd58c000cbdee35efb834858940553
Steve Rogers <stefano@yomi.digital>
2023-09-22 12:27:23 +0200

    using fetch instead axios

--- v2/perps-v2/ui/src/hooks/usePnlStats.ts
+++ v2/perps-v2/ui/src/hooks/usePnlStats.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import axios, { AxiosError } from 'axios';
 
 interface ApiResponse {
   result: {
@@ -21,7 +20,7 @@ interface Row {
 
 const UsePnlStats = (DUNE_API_KEY: string, period: 'W' | 'M' | 'Y') => {
   const [data, setData] = useState<ApiResponse | null>(null);
-  const [error, setError] = useState<AxiosError | null>(null);
+  const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [lastStakers, setLastStakers] = useState<number>(0);
 
@@ -29,51 +28,60 @@ const UsePnlStats = (DUNE_API_KEY: string, period: 'W' | 'M' | 'Y') => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(
+        const response = await fetch(
           `https://api.dune.com/api/v1/query/2429954/results?api_key=${DUNE_API_KEY}`
         );
 
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+
         const sortedData: ApiResponse = {
-          ...response.data,
+          ...responseData,
           result: {
-            ...response.data.result,
-            rows: [...response.data.result.rows].sort(
+            ...responseData.result,
+            rows: [...responseData.result.rows].sort(
               (a, b) => Date.parse(a.day) - Date.parse(b.day)
             ),
           },
         };
+
         const currentDate = new Date();
-        const filteredRows = sortedData.result.rows.sort((a, b) => {
-          const aDate = new Date(a.day.replace(' ', 'T').replace(' UTC', 'Z'));
-          const bDate = new Date(b.day.replace(' ', 'T').replace(' UTC', 'Z'));
-          return aDate.getTime() - bDate.getTime();
-        }).filter((row: Row) => {
-          const rowDate = new Date(row.day.replace(' ', 'T').replace(' UTC', 'Z'));
-          switch (period) {
-            case 'W':
-              return rowDate.getTime() >= currentDate.getTime() - 7 * 24 * 60 * 60 * 1000;
-            case 'M':
-              return (
-                rowDate.getTime() >=
-                new Date(
-                  currentDate.getFullYear(),
-                  currentDate.getMonth() - 1,
-                  currentDate.getDate()
-                ).getTime()
-              );
-            case 'Y':
-              return (
-                rowDate.getTime() >=
-                new Date(
-                  currentDate.getFullYear() - 1,
-                  currentDate.getMonth(),
-                  currentDate.getDate()
-                ).getTime()
-              );
-            default:
-              return true;
-          }
-        });
+        const filteredRows = sortedData.result.rows
+          .sort((a, b) => {
+            const aDate = new Date(a.day.replace(' ', 'T').replace(' UTC', 'Z'));
+            const bDate = new Date(b.day.replace(' ', 'T').replace(' UTC', 'Z'));
+            return aDate.getTime() - bDate.getTime();
+          })
+          .filter((row: Row) => {
+            const rowDate = new Date(row.day.replace(' ', 'T').replace(' UTC', 'Z'));
+            switch (period) {
+              case 'W':
+                return rowDate.getTime() >= currentDate.getTime() - 7 * 24 * 60 * 60 * 1000;
+              case 'M':
+                return (
+                  rowDate.getTime() >=
+                  new Date(
+                    currentDate.getFullYear(),
+                    currentDate.getMonth() - 1,
+                    currentDate.getDate()
+                  ).getTime()
+                );
+              case 'Y':
+                return (
+                  rowDate.getTime() >=
+                  new Date(
+                    currentDate.getFullYear() - 1,
+                    currentDate.getMonth(),
+                    currentDate.getDate()
+                  ).getTime()
+                );
+              default:
+                return true;
+            }
+          });
         const transformedRows: Row[] = filteredRows.map((row: Row) => {
           const date = new Date(row.day);
           let formattedDate;
@@ -131,7 +139,7 @@ const UsePnlStats = (DUNE_API_KEY: string, period: 'W' | 'M' | 'Y') => {
 
         setError(null);
       } catch (error) {
-        setError(error as AxiosError);
+        setError(error as Error);
       } finally {
         setLoading(false);
       }
