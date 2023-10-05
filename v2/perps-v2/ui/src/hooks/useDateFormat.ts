ddc6d106bd75b50c380ea70e1f363c928ebc4e52
Steve Rogers <stefano@yomi.digital>
2023-10-06 01:07:47 +0200

    delete comment

--- v2/perps-v2/ui/src/hooks/useDateFormat.ts
+++ v2/perps-v2/ui/src/hooks/useDateFormat.ts
@@ -2,7 +2,6 @@ const useDateFormat = (day: string, period: 'W' | 'M' | 'Y') => {
   const isoFormattedDay = day.split(' ')[0] + 'T' + day.split(' ')[1].split('.')[0] + 'Z';
   const date = new Date(isoFormattedDay);
 
-  // Check if date is invalid and log the original string if it is
   if (isNaN(date.getTime())) {
     console.error('Invalid date derived from string: ', day);
     return '';
