c0ca3dcabf1567e2c64c7bd5a3cb62484a3fa475
DuyenTB <duyentb@decentralab.asia>
2023-11-08 12:04:00 +0700

    Fix prettier

--- v2/perps-v2/ui/src/components/Shared/Action/Action.tsx
+++ v2/perps-v2/ui/src/components/Shared/Action/Action.tsx
@@ -1,4 +1,4 @@
-import {Button, Fade, Link, SystemStyleObject, Td, Text, Tooltip} from '@chakra-ui/react';
+import { Button, Fade, Link, SystemStyleObject, Td, Text, Tooltip } from '@chakra-ui/react';
 import { formatDistance } from 'date-fns';
 import { optimisticEthercanTx } from '../../../utils';
 import { RightUpIcon } from '../../Icons';
@@ -7,11 +7,11 @@ interface ActionProps {
   label: string;
   txHash: string;
   timestamp?: number;
-  sx?: SystemStyleObject
+  sx?: SystemStyleObject;
 }
 
 export const Action = ({ label, txHash, timestamp, sx }: ActionProps) => {
-  const date = timestamp? new Date(timestamp * 1000) : undefined;
+  const date = timestamp ? new Date(timestamp * 1000) : undefined;
   return (
     <Td border="none" fontSize="14px" lineHeight="20px" fontFamily="heading" fontWeight={500}>
       <Fade in>
@@ -25,13 +25,14 @@ export const Action = ({ label, txHash, timestamp, sx }: ActionProps) => {
           fontFamily="inter"
           fontWeight="500"
           fontSize="14px"
-          sx={{...sx}}
+          sx={{ ...sx }}
         >
           {label}
           <RightUpIcon ml={1} mb={0.5} />
         </Button>
-        {date && <Text color="gray.500" fontSize="12px" lineHeight="16px">
-          <Tooltip
+        {date && (
+          <Text color="gray.500" fontSize="12px" lineHeight="16px">
+            <Tooltip
               py={2}
               px={4}
               bg="gray.900"
@@ -40,10 +41,11 @@ export const Action = ({ label, txHash, timestamp, sx }: ActionProps) => {
               fontFamily="heading"
               borderRadius="4px"
               label={date.toISOString()}
-          >
-            {formatDate(date)}
-          </Tooltip>
-        </Text>}
+            >
+              {formatDate(date)}
+            </Tooltip>
+          </Text>
+        )}
       </Fade>
     </Td>
   );
