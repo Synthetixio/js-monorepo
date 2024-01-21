458aa2537b9fd6f462c94230c0435730b7c1a670
DuyenTB <duyentb@decentralab.asia>
2024-01-21 17:10:36 +0700

    Revert

---
+++ packages/queries/src/subgraph/queryFuncs.ts
@@ -0,0 +1,85 @@
+// !!! DO NOT EDIT !!! Automatically generated file
+
+import useGetDebtDataQuery from '../queries/debt/useGetDebtDataQuery';
+import useGetDebtL1 from '../queries/debt/useGetDebtL1';
+import useGetDebtL2 from '../queries/debt/useGetDebtL2';
+import useGetDebtTimeseries from '../queries/debt/useGetDebtTimeseries';
+import useGetAuthoriserWallets from '../queries/delegate/useGetAuthoriserWallets';
+import useGetDelegateWallets from '../queries/delegate/useGetDelegateWallets';
+import useEscrowDataQuery from '../queries/escrow/useEscrowDataQuery';
+import useTokenSaleEscrowQuery from '../queries/escrow/useTokenSaleEscrowQuery';
+import useActiveProposalsQuery from '../queries/gov/useActiveProposalsQuery';
+import useGetElectionsPeriodStatus from '../queries/gov/useGetElectionsPeriodStatus';
+import useGetSpartanCouncil from '../queries/gov/useGetSpartanCouncil';
+import useProposalsQuery from '../queries/gov/useProposalsQuery';
+import useSnapshotSpaceQuery from '../queries/gov/useSnapshotSpaceQuery';
+import useGetLiquidationDataQuery from '../queries/liquidations/useGetLiquidationDataQuery';
+import useTokenListQuery from '../queries/misc/useTokenListQuery';
+import useEthGasPriceQuery from '../queries/network/useEthGasPriceQuery';
+import useIsBridgeActiveQuery from '../queries/ovm-bridge/useIsBridgeActiveQuery';
+import useCandlesticksQuery from '../queries/rates/useCandlesticksQuery';
+import useExchangeRatesQuery from '../queries/rates/useExchangeRatesQuery';
+import useSynthMarketCapQuery from '../queries/rates/useSynthMarketCapQuery';
+import useShortsQuery from '../queries/shorts/useShortsQuery';
+import useClaimableRewardsQuery from '../queries/staking/useClaimableRewardsQuery';
+import useGetFeePoolDataQuery from '../queries/staking/useGetFeePoolDataQuery';
+import useGlobalStakingInfoQuery from '../queries/staking/useGlobalStakingInfoQuery';
+import useSNXData from '../queries/staking/useSNXData';
+import useExchangeFeeRateQuery from '../queries/synths/useExchangeFeeRateQuery';
+import useFeeReclaimPeriodQuery from '../queries/synths/useFeeReclaimPeriodQuery';
+import useFeeReclaimPeriodsQuery from '../queries/synths/useFeeReclaimPeriodsQuery';
+import useGetSynthsByName from '../queries/synths/useGetSynthsByName';
+import useRedeemableDeprecatedSynthsQuery from '../queries/synths/useRedeemableDeprecatedSynthsQuery';
+import useSynthSuspensionQuery from '../queries/synths/useSynthSuspensionQuery';
+import useSynthsTotalSupplyQuery from '../queries/synths/useSynthsTotalSupplyQuery';
+import useTotalIssuedSynthsExcludeOtherCollateralQuery from '../queries/synths/useTotalIssuedSynthsExcludeOtherCollateralQuery';
+import useIsSystemOnMaintenance from '../queries/systemStatus/useIsSystemOnMaintenance';
+import useETHBalanceQuery from '../queries/walletBalances/useETHBalanceQuery';
+import useSynthsBalancesQuery from '../queries/walletBalances/useSynthsBalancesQuery';
+import useTokensBalancesQuery from '../queries/walletBalances/useTokensBalancesQuery';
+import useContractTxn from '../mutations/useContractTxn';
+import useEVMTxn from '../mutations/useEVMTxn';
+import useSynthetixTxn from '../mutations/useSynthetixTxn';
+
+export {
+  useGetDebtDataQuery,
+  useGetDebtL1,
+  useGetDebtL2,
+  useGetDebtTimeseries,
+  useGetAuthoriserWallets,
+  useGetDelegateWallets,
+  useEscrowDataQuery,
+  useTokenSaleEscrowQuery,
+  useActiveProposalsQuery,
+  useGetElectionsPeriodStatus,
+  useGetSpartanCouncil,
+  useProposalsQuery,
+  useSnapshotSpaceQuery,
+  useGetLiquidationDataQuery,
+  useTokenListQuery,
+  useEthGasPriceQuery,
+  useIsBridgeActiveQuery,
+  useCandlesticksQuery,
+  useExchangeRatesQuery,
+  useSynthMarketCapQuery,
+  useShortsQuery,
+  useClaimableRewardsQuery,
+  useGetFeePoolDataQuery,
+  useGlobalStakingInfoQuery,
+  useSNXData,
+  useExchangeFeeRateQuery,
+  useFeeReclaimPeriodQuery,
+  useFeeReclaimPeriodsQuery,
+  useGetSynthsByName,
+  useRedeemableDeprecatedSynthsQuery,
+  useSynthSuspensionQuery,
+  useSynthsTotalSupplyQuery,
+  useTotalIssuedSynthsExcludeOtherCollateralQuery,
+  useIsSystemOnMaintenance,
+  useETHBalanceQuery,
+  useSynthsBalancesQuery,
+  useTokensBalancesQuery,
+  useContractTxn,
+  useEVMTxn,
+  useSynthetixTxn,
+};
