b431e0e2282ab873883095efb1bbe5adab7c8fa7
DuyenTB <duyentb@decentralab.asia>
2024-01-22 15:44:53 +0700

    Fix testcase

--- v2/cypress/cypress/e2e/wallet.e2e.js
+++ v2/cypress/cypress/e2e/wallet.e2e.js
@@ -22,11 +22,6 @@ it('walks over wallet sub-pages', () => {
   cy.contains('a.chakra-button', 'Delegate').click().should('have.attr', 'aria-selected', 'true');
   cy.contains('[role="tab"]', 'Delegate to a new Address').should('exist');
 
-  cy.contains('a.chakra-button', 'Bridge Assets')
-    .click()
-    .should('have.attr', 'aria-selected', 'true');
-  cy.contains('h1', 'Bridge between L1 and L2').should('exist');
-
   cy.contains('a.chakra-button', 'Merge Accounts')
     .click()
     .should('have.attr', 'aria-selected', 'true');
@@ -36,4 +31,7 @@ it('walks over wallet sub-pages', () => {
     .click()
     .should('have.attr', 'aria-selected', 'true');
   cy.contains('[role="tab"]', 'Migrate Escrowed SNX').should('exist');
+
+  cy.contains('a.chakra-button', 'Bridge Assets').click();
+  cy.contains('h1', 'Bridge between L1 and L2').should('exist');
 });
