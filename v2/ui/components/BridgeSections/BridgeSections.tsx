72aa319bf4af259e76c823196b8f3f296ede8262
DuyenTB <duyentb@decentralab.asia>
2024-01-23 13:19:37 +0700

    update native bridge

--- v2/ui/components/BridgeSections/BridgeSections.tsx
+++ v2/ui/components/BridgeSections/BridgeSections.tsx
@@ -1,4 +1,4 @@
-import BridgeSectionItem from './BridgeSectionItem';
+import BridgeSectionItem, { TagItemProps } from './BridgeSectionItem';
 import { Flex } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { BridgeTypeEnum } from '../../constants/enums';
@@ -17,13 +17,22 @@ const BridgeSections = ({
     setCurrentBridge(BridgeTypeEnum.INSTANT);
   };
 
+  const nativeBridgeTags: TagItemProps[] = [
+    {
+      label: t('bridge.no-fees'),
+      bgColor: 'purple.700',
+    },
+    { label: t('bridge.no-slippage'), bgColor: 'purple.700' },
+    { label: t('bridge.delay-note'), bgColor: 'gray.900' },
+  ];
+
   return (
     <Flex flexDir={['column', 'column', 'column', 'row']} alignItems="stretch" gap="24px">
       <BridgeSectionItem
         logo="/images/synthetix-logo.svg"
         label={t('bridge.native-bridge-title')}
         description={t('bridge.native-bridge-desc')}
-        tags={[t('bridge.no-fees'), t('bridge.no-slippage')]}
+        tags={nativeBridgeTags}
         btnText={t('bridge.btn-native-bridge')}
         handleClick={handleNativeBridge}
       />
