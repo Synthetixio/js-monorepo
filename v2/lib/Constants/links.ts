06b42ba1861d95a28d19dfdd0c2b404bba967aa7
jz - synthetix <52280438+jmzwar@users.noreply.github.com>
2024-02-08 12:42:31 +1100

    discord link (#1058)
    Co-authored-by: jmzwar <james@jmzwar.com>

--- v2/lib/Constants/links.ts
+++ v2/lib/Constants/links.ts
@@ -20,7 +20,7 @@ export const EXTERNAL_LINKS = {
   Social: {
     Twitter: 'https://twitter.com/synthetix_io',
     Blog: 'https://blog.synthetix.io/',
-    Discord: 'https://discordapp.com/invite/AEdUHzt',
+    Discord: 'https://discord.com/invite/ptfNSByB8P',
     GitHub: 'https://github.com/synthetixio/staking',
   },
   TokenLists: {
