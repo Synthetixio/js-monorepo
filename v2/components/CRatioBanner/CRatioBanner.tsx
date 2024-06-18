14bc9fbc27f24d456b5f73914be33d3db4d69c4d
jz - synthetix <52280438+jmzwar@users.noreply.github.com>
2024-06-18 17:55:39 +0700

    Release (#1096)
    * update dc link (#1085)
    
    * update dc link
    
    * update eth optimism
    
    ---------
    
    Co-authored-by: jmzwar <james@jmzwar.com>
    
    * update mainnet (#1087)
    
    Co-authored-by: jmzwar <james@jmzwar.com>
    
    * l2 subgraphs (#1089)
    
    Co-authored-by: jmzwar <james@jmzwar.com>
    
    * Feature/upgrade onboard (#1090)
    
    * upgrade onboard
    
    * put coinbase first
    
    * dedupe
    
    ---------
    
    Co-authored-by: jmzwar <james@jmzwar.com>
    
    * wording (#1093)
    
    Co-authored-by: jmzwar <james@jmzwar.com>
    
    * remove reference to claims (#1095)
    
    * remove reference to claims
    
    * remove tests
    
    * tests
    
    ---------
    
    Co-authored-by: jmzwar <james@jmzwar.com>
    
    ---------
    
    Co-authored-by: jmzwar <james@jmzwar.com>

--- v2/components/CRatioBanner/CRatioBanner.tsx
+++ v2/components/CRatioBanner/CRatioBanner.tsx
@@ -40,7 +40,10 @@ export const CRatioBannerUi: FC<UiProps> = ({
   const translationKey = isFlagged ? 'error-flagged' : variant;
   const wrapperStyles = getWrapperStyles(variant);
 
-  if ((hasClaimed || nothingToClaim) && (variant === 'success' || variant === 'warning')) {
+  if (
+    ((hasClaimed || nothingToClaim) && (variant === 'success' || variant === 'warning')) ||
+    variant === 'warning'
+  ) {
     return null;
   }
 
