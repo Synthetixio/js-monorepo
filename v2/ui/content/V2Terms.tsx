02304b22ed0a159e4e2222b0fd749807f8c7656f
Noisekit <28145325+noisekit@users.noreply.github.com>
2024-11-13 17:59:21 +1100

    Apply suggestions from code review

--- v2/ui/content/V2Terms.tsx
+++ v2/ui/content/V2Terms.tsx
@@ -70,11 +70,11 @@ const V2Terms = () => {
             ), govern the User’s access to and use of https://synthetix.io/,
             https://synthetix.eth.limo/ and its sub-pages, including any content or functionality
             offered on or through the website-hosted user interface accessible via
-            https://staking.synthetix.io/ or https://staking.synthetix.io/ (the{' '}
+            https://staking.synthetix.io/ (the{' '}
             <Text as="span" fontWeight="700">
               “Interface,”{' '}
             </Text>{' '}
-            or collectively with other https://synthetix.io/ or https://synthetix.io/
+            or collectively with other https://synthetix.io/
             subpages, the{' '}
             <Text as="span" fontWeight="700">
               “Website”
