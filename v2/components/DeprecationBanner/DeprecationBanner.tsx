97ff170e2619e5aefb1581be0d161fb314cd3f32
noisekit <28145325+noisekit@users.noreply.github.com>
2025-04-07 13:21:52 +0800

    Migrated (#1130)
    * Migration notice
    
    * Migration notice

--- v2/components/DeprecationBanner/DeprecationBanner.tsx
+++ v2/components/DeprecationBanner/DeprecationBanner.tsx
@@ -5,9 +5,11 @@ export function DeprecationBanner() {
     <Alert status="warning" mb="6">
       <AlertIcon />
       <Text>
-        From March 24th 2025 the liquidation ratio is being raised on legacy positions.{' '}
-        <Link color="cyan.400" isExternal={true} href="https://420.synthetix.io">
-          Migrate to 420 Pool immediately.
+        As part of the migration to protocol managed liquidity and the deprecation of legacy
+        staking, we recently migrated all accounts from Synthetix v2x to v3. You can see more
+        information about your position on
+        <Link color="cyan.400" isExternal={true} href="https://liquidity.synthetix.io">
+          the Synthetix Liquidity website
         </Link>
       </Text>
     </Alert>
