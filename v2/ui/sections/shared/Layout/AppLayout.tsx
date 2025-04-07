c6c4417e0252a903b935c0172488e308f75ac859
Noisekit <noisekit@proton.me>
2025-04-07 12:29:55 +1000

    Migration notice

--- v2/ui/sections/shared/Layout/AppLayout.tsx
+++ v2/ui/sections/shared/Layout/AppLayout.tsx
@@ -1,18 +1,35 @@
 import { FC, ReactNode } from 'react';
 import { Header as V2Header } from '../../../v2-components/Header';
-import { Box, Container } from '@chakra-ui/react';
+import { Alert, AlertIcon, Box, Collapse, Container, Text } from '@chakra-ui/react';
 import { DeprecationBanner } from '../../../../components/DeprecationBanner/DeprecationBanner';
+import { useMigrationData } from './useMigrationData';
+import Connector from 'containers/Connector';
 
 type AppLayoutProps = {
   children: ReactNode;
 };
 
 const AppLayout: FC<AppLayoutProps> = ({ children }) => {
+  const { walletAddress } = Connector.useContainer();
+  const { data: migrationData } = useMigrationData({
+    walletAddress: walletAddress ? walletAddress : undefined,
+  });
   return (
     <>
       <V2Header />
       <Container pt={6} maxW="1200px" width="100%">
-        <DeprecationBanner />
+        <Collapse in={Boolean(migrationData?.address)} animateOpacity unmountOnExit>
+          <Alert status="error" mb="6">
+            <AlertIcon />
+            <Text>
+              We’ve recently deprecated solo staking and your account was impacted. Please create a
+              ticket in discord and the team will help recover your account
+            </Text>
+          </Alert>
+        </Collapse>
+        <Collapse in={!migrationData?.address} animateOpacity unmountOnExit>
+          <DeprecationBanner />
+        </Collapse>
       </Container>
       <Box overflow="auto" flex="1 1 auto" overflowX="hidden">
         {children}
